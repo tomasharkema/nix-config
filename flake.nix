5763c09d6b5105d70441cf23f46ea8b7f3b5fb7e
Tomas Harkema <tomas@harkema.io>
2023-12-05 18:24:43 +0100

    add overlay for desktop

--- flake.nix
+++ flake.nix
@@ -57,11 +57,7 @@
         };
         boot.isContainer = true;
 
-        networking.networkmanager.enable = true;
-        services.xserver.enable = true;
-        services.xserver.displayManager.gdm.enable = true;
-        services.xserver.desktopManager.gnome.enable = true;
-        networking.firewall.enable = false;
+        imports = [ ./overlays/desktop.nix ];
       };
 
       utm-ferdorie = { pkgs, modulesPath, ... }: {
---
+++ overlays/desktop.nix
@@ -0,0 +1,7 @@
+{
+  networking.networkmanager.enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  networking.firewall.enable = false;
+}
