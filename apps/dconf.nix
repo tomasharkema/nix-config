659f1a72d1e51f820e9a6856e80160fab46db7ba
Tomas Harkema <tomas@harkema.io>
2023-12-10 16:04:03 +0100

    derp

--- apps/dconf.nix
+++ apps/dconf.nix
@@ -5,14 +5,9 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
-    "org/gnome/Console" = {
-      last-window-size = mkTuple [ 652 480 ];
-    };
+    # "org/gnome/Console" = { last-window-size = mkTuple [ 652 480 ]; };
 
-    "org/gnome/control-center" = {
-      last-panel = "privacy";
-      window-state = mkTuple [ 980 640 false ];
-    };
+    "org/gnome/control-center" = { last-panel = "privacy"; };
 
     "org/gnome/desktop/app-folders" = {
       folder-children = [ "Utilities" "YaST" "Pardus" ];
@@ -25,68 +20,60 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/app-folders/folders/Utilities" = {
-      apps = [ "gnome-abrt.desktop" "gnome-system-log.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" "org.gnome.Connections.desktop" "org.gnome.DejaDup.desktop" "org.gnome.Dictionary.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.fonts.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.tweaks.desktop" "org.gnome.Usage.desktop" "vinagre.desktop" ];
+      apps = [
+        "gnome-abrt.desktop"
+        "gnome-system-log.desktop"
+        "nm-connection-editor.desktop"
+        "org.gnome.baobab.desktop"
+        "org.gnome.Connections.desktop"
+        "org.gnome.DejaDup.desktop"
+        "org.gnome.Dictionary.desktop"
+        "org.gnome.DiskUtility.desktop"
+        "org.gnome.Evince.desktop"
+        "org.gnome.FileRoller.desktop"
+        "org.gnome.fonts.desktop"
+        "org.gnome.Loupe.desktop"
+        "org.gnome.seahorse.Application.desktop"
+        "org.gnome.tweaks.desktop"
+        "org.gnome.Usage.desktop"
+        "vinagre.desktop"
+      ];
       categories = [ "X-GNOME-Utilities" ];
       name = "X-GNOME-Utilities.directory";
       translate = true;
     };
 
-    "org/gnome/desktop/app-folders/folders/YaST" = {
-      categories = [ "X-SuSE-YaST" ];
-      name = "suse-yast.directory";
-      translate = true;
-    };
+    # "org/gnome/desktop/app-folders/folders/YaST" = {
+    #   categories = [ "X-SuSE-YaST" ];
+    #   name = "suse-yast.directory";
+    #   translate = true;
+    # };
 
     "org/gnome/desktop/input-sources" = {
       sources = [ (mkTuple [ "xkb" "us" ]) ];
       xkb-options = [ "terminate:ctrl_alt_bksp" ];
     };
 
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
+    "org/gnome/desktop/interface" = { color-scheme = "prefer-dark"; };
 
     "org/gnome/desktop/notifications" = {
-      application-children = [ "steam" "org-gnome-console" "gnome-power-panel" "firefox" ];
+      application-children =
+        [ "steam" "org-gnome-console" "gnome-power-panel" "firefox" ];
       show-in-lock-screen = false;
     };
 
-    "org/gnome/desktop/notifications/application/firefox" = {
-      application-id = "firefox.desktop";
-    };
-
-    "org/gnome/desktop/notifications/application/gnome-power-panel" = {
-      application-id = "gnome-power-panel.desktop";
-    };
-
-    "org/gnome/desktop/notifications/application/org-gnome-console" = {
-      application-id = "org.gnome.Console.desktop";
-    };
-
-    "org/gnome/desktop/notifications/application/steam" = {
-      application-id = "steam.desktop";
-    };
-
-    "org/gnome/desktop/peripherals/keyboard" = {
-      numlock-state = true;
-    };
+    "org/gnome/desktop/peripherals/keyboard" = { numlock-state = true; };
 
     "org/gnome/desktop/privacy" = {
       old-files-age = mkUint32 30;
       recent-files-max-age = -1;
     };
 
-    "org/gnome/desktop/screensaver" = {
-      lock-enabled = false;
-    };
+    "org/gnome/desktop/screensaver" = { lock-enabled = false; };
 
-    "org/gnome/desktop/session" = {
-      idle-delay = mkUint32 0;
-    };
+    "org/gnome/desktop/session" = { idle-delay = mkUint32 0; };
 
-    "org/gnome/evolution-data-server" = {
-      migrated = true;
-    };
+    "org/gnome/evolution-data-server" = { migrated = true; };
 
     "org/gnome/nautilus/preferences" = {
       default-folder-viewer = "icon-view";
@@ -94,23 +81,89 @@ with lib.hm.gvariant;
       search-filter-time-type = "last_modified";
     };
 
-    "org/gnome/nautilus/window-state" = {
-      initial-size = mkTuple [ 890 550 ];
-    };
-
     "org/gnome/shell" = {
-      enabled-extensions = [ "gsconnect@andyholmes.github.io" "drive-menu@gnome-shell-extensions.gcampax.github.com" "appindicatorsupport@rgcjonas.gmail.com" "apps-menu@gnome-shell-extensions.gcampax.github.com" "places-menu@gnome-shell-extensions.gcampax.github.com" "dash-to-panel@jderose9.github.com" ];
+      enabled-extensions = [
+        "gsconnect@andyholmes.github.io"
+        "drive-menu@gnome-shell-extensions.gcampax.github.com"
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "apps-menu@gnome-shell-extensions.gcampax.github.com"
+        "places-menu@gnome-shell-extensions.gcampax.github.com"
+        "dash-to-panel@jderose9.github.com"
+      ];
       welcome-dialog-last-shown-version = "45.1";
     };
 
     "org/gnome/shell/extensions/dash-to-panel" = {
-      available-monitors = [ 0 ];
-      primary-monitor = 0;
+      # Even when we are not using multiple panels on multiple monitors,
+      # the extension still creates them in the config, so we set the same
+      # configuration for each (up to 2 monitors).
+      panel-positions = builtins.toJSON (lib.genAttrs [ "0" "1" ] (x: "TOP"));
+      panel-sizes = builtins.toJSON (lib.genAttrs [ "0" "1" ] (x: 32));
+      panel-element-positions = builtins.toJSON (lib.genAttrs [ "0" "1" ] (x: [
+        {
+          element = "showAppsButton";
+          visible = true;
+          position = "stackedTL";
+        }
+        {
+          element = "activitiesButton";
+          visible = false;
+          position = "stackedTL";
+        }
+        {
+          element = "dateMenu";
+          visible = true;
+          position = "stackedTL";
+        }
+        {
+          element = "leftBox";
+          visible = true;
+          position = "stackedTL";
+        }
+        {
+          element = "taskbar";
+          visible = true;
+          position = "centerMonitor";
+        }
+        {
+          element = "centerBox";
+          visible = false;
+          position = "centered";
+        }
+        {
+          element = "rightBox";
+          visible = true;
+          position = "stackedBR";
+        }
+        {
+          element = "systemMenu";
+          visible = true;
+          position = "stackedBR";
+        }
+        {
+          element = "desktopButton";
+          visible = false;
+          position = "stackedBR";
+        }
+      ]));
+      multi-monitors = false;
+      show-apps-icon-file =
+        "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake-white.svg";
+      show-apps-icon-padding = mkInt32 4;
+      focus-highlight-dominant = true;
+      dot-size = mkInt32 0;
+      appicon-padding = mkInt32 2;
+      appicon-margin = mkInt32 0;
+      trans-use-custom-opacity = true;
+      trans-panel-opacity = 0.25;
+      show-favorites = false;
+      group-apps = false;
+      isolate-workspaces = true;
+      hide-overview-on-startup = true;
+      stockgs-keep-dash = true;
     };
 
-    "org/gnome/shell/world-clocks" = {
-      locations = [];
-    };
+    "org/gnome/shell/world-clocks" = { locations = [ ]; };
 
     "org/gtk/gtk4/settings/file-chooser" = {
       date-format = "regular";
@@ -124,8 +177,7 @@ with lib.hm.gvariant;
       sort-order = "ascending";
       type-format = "category";
       view-type = "list";
-      window-size = mkTuple [ 859 366 ];
-    };
 
+    };
   };
 }
