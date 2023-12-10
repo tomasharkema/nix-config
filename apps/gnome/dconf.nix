4c7a39b0bc1e81e1405f72a33923b0e63ce5fa9d
Tomas Harkema <tomas@harkema.io>
2023-12-10 16:14:38 +0100

    derp

--- apps/gnome/dconf.nix
+++ apps/gnome/dconf.nix
@@ -1,5 +1,5 @@
 # Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 with lib.hm.gvariant;
 
@@ -95,75 +95,7 @@ with lib.hm.gvariant;
       welcome-dialog-last-shown-version = "45.1";
     };
 
-    "org/gnome/shell/extensions/dash-to-panel" = {
-      # Even when we are not using multiple panels on multiple monitors,
-      # the extension still creates them in the config, so we set the same
-      # configuration for each (up to 2 monitors).
-      panel-positions = builtins.toJSON (lib.genAttrs [ "0" "1" ] (x: "TOP"));
-      panel-sizes = builtins.toJSON (lib.genAttrs [ "0" "1" ] (x: 32));
-      panel-element-positions = builtins.toJSON (lib.genAttrs [ "0" "1" ] (x: [
-        {
-          element = "showAppsButton";
-          visible = true;
-          position = "stackedTL";
-        }
-        {
-          element = "activitiesButton";
-          visible = false;
-          position = "stackedTL";
-        }
-        {
-          element = "dateMenu";
-          visible = true;
-          position = "stackedTL";
-        }
-        {
-          element = "leftBox";
-          visible = true;
-          position = "stackedTL";
-        }
-        {
-          element = "taskbar";
-          visible = true;
-          position = "centerMonitor";
-        }
-        {
-          element = "centerBox";
-          visible = false;
-          position = "centered";
-        }
-        {
-          element = "rightBox";
-          visible = true;
-          position = "stackedBR";
-        }
-        {
-          element = "systemMenu";
-          visible = true;
-          position = "stackedBR";
-        }
-        {
-          element = "desktopButton";
-          visible = false;
-          position = "stackedBR";
-        }
-      ]));
-      multi-monitors = false;
-      show-apps-icon-file =
-        "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake-white.svg";
-      show-apps-icon-padding = mkInt32 4;
-      focus-highlight-dominant = true;
-      dot-size = mkInt32 0;
-      appicon-padding = mkInt32 2;
-      appicon-margin = mkInt32 0;
-      trans-use-custom-opacity = true;
-      trans-panel-opacity = 0.25;
-      show-favorites = false;
-      group-apps = false;
-      isolate-workspaces = true;
-      hide-overview-on-startup = true;
-      stockgs-keep-dash = true;
-    };
+    "org/gnome/shell/extensions/dash-to-panel" = { };
 
     "org/gnome/shell/world-clocks" = { locations = [ ]; };
 
