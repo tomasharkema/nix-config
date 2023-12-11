6618f16df6b9efdaa78bd8749f6ca3b36e61bf90
Tomas Harkema <tomas@harkema.io>
2023-12-11 17:30:56 +0100

    derp

--- apps/tailscale.nix
+++ apps/tailscale.nix
@@ -47,7 +47,7 @@
 
   system.activationScripts = {
     tailscale = ''
-      cd ~ && ${pkgs.tailscale} cert $(hostname).ling-lizard.ts.net
+      cd ~ && ${pkgs.tailscale}/bin/tailscale cert $(hostname).ling-lizard.ts.net
     '';
   };
 }
