206d2f6ccc45fb7c9714f576c44d6c1b15e9e54c
Tomas Harkema <tomas@harkema.io>
2023-12-12 15:55:19 +0100

    add supermicro

--- machines/supermicro/default.nix
+++ machines/supermicro/default.nix
@@ -8,10 +8,10 @@
     inputs.hardware.nixosModules.common-cpu-amd
     inputs.hardware.nixosModules.common-pc-ssd
     # (modulesPath + "/installer/scan/not-detected.nix")
-    ../../common/quiet-boot.nix
-    ../../common/game-mode.nix
+    # ../../common/quiet-boot.nix
+    # ../../common/game-mode.nix
     ../../apps/desktop.nix
-    ../../apps/steam.nix
+    # ../../apps/steam.nix
     "${
       builtins.fetchTarball {
         url = "https://github.com/nix-community/disko/archive/master.tar.gz";
@@ -38,42 +38,42 @@
   users.groups.input.members = [ "tomas" ];
 
   # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
+  # hardware.opengl = {
+  #   enable = true;
+  #   driSupport = true;
+  #   driSupport32Bit = true;
+  # };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ];
-  environment.systemPackages = with pkgs; [ nvtop ];
-  hardware.nvidia = {
+  # services.xserver.videoDrivers = [ "nvidia" ];
+  # environment.systemPackages = with pkgs; [ nvtop ];
+  # hardware.nvidia = {
 
-    # Modesetting is required.
-    modesetting.enable = true;
+  #   # Modesetting is required.
+  #   modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  #   powerManagement.enable = false;
+  #   # Fine-grained power management. Turns off GPU when not in use.
+  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  #   powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+  #   # Use the NVidia open source kernel module (not to be confused with the
+  #   # independent third-party "nouveau" open source driver).
+  #   # Support is limited to the Turing and later architectures. Full list of 
+  #   # supported GPUs is at: 
+  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+  #   # Only available from driver 515.43.04+
+  #   # Currently alpha-quality/buggy, so false is currently the recommended setting.
+  #   open = false;
 
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+  #   # Enable the Nvidia settings menu,
+  #   # accessible via `nvidia-settings`.
+  #   nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
 
   # systemd.services.netdata.path = [ pkgs.linuxPackages.nvidia_x11 ];
   # services.netdata.configDir."python.d.conf" = pkgs.writeText "python.d.conf" ''
