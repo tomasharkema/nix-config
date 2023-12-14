5ae758e5f3d28abe5487fed3ab013936c3af34a5
Tomas Harkema <tomas@harkema.io>
2023-12-14 11:08:02 +0100

    fixes

--- upload-to-cache.sh
+++ upload-to-cache.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-printenv
-
-set -u
+# set -u
 set -f # disable globbing
 export IFS=' '
 
 echo "Uploading paths $OUT_PATHS $DRV_PATH"
 
-exec nix copy --to "https://nix-cache.harke.ma/" $OUT_PATHS $DRV_PATH
+# nix store sign --key-file cache-priv-key.pem --verbose $OUT_PATHS $DRV_PATH
+
+# exec nix copy --to "https://nix-cache.harke.ma/" $OUT_PATHS $DRV_PATH
