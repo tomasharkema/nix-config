1bc3ee59d14a3f37239ccd1ecfbeddf27294805c
Tomas Harkema <tomas@harkema.io>
2023-12-14 11:29:55 +0100

    fix shell

--- upload-to-cache.sh
+++ upload-to-cache.sh
@@ -7,14 +7,20 @@ export IFS=' '
 # set -x
 
 execute() {
+
+	set -x
+
+	echo "====="
 	whoami
 
-	echo "Uploading paths $OUT_PATHS $DRV_PATH"
+	echo "Uploading paths $OUT_PATHS"
+	echo "DERV  $DRV_PATH"
 
 	nix store sign --key-file cache-priv-key.pem --verbose $OUT_PATHS $DRV_PATH
 
 	nix copy --to "https://nix-cache.harke.ma/" --verbose $OUT_PATHS $DRV_PATH
 
+	echo "done"
 }
 
 execute | tee -a /tmp/nix.log
