99f2792dbd332a6411dc27569790f443c0d6810a
Tomas Harkema <tomas@harkema.io>
2023-12-14 22:12:57 +0100

    derp

--- configurations.nix
+++ configurations.nix
@@ -215,6 +215,46 @@ in
     ];
   };
 
+
+  unraid = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = attrs;
+
+    # specialArgs = { inherit inputs outputs; };
+    modules = [
+      # ./user-defaults.nix
+      base
+      # impermanence.nixosModule
+      # disko.nixosModules.default
+      # ./machines/cfserve
+      ./secrets
+      nix-flatpak.nixosModules.nix-flatpak
+      agenix.nixosModules.default
+      ({
+
+        boot.isContainer = true;
+      })
+      # home-manager.nixosModules.home-manager
+      # {
+      #   home-manager.useGlobalPkgs = true;
+      #   home-manager.useUserPackages = true;
+      #   home-manager.extraSpecialArgs = { inherit inputs; };
+      #   home-manager.users.tomas.imports = [
+      #     nix-flatpak.homeManagerModules.nix-flatpak
+      #     agenix.homeManagerModules.default
+      #     ./home.nix
+      #   ];
+      #   home-manager.backupFileExtension = "bak";
+      # }
+      # vscode-server.nixosModules.default
+      # ({ config
+      #  , pkgs
+      #  , ...
+      #  }: { services.vscode-server.enable = true; })
+    ];
+  };
+
+
   darwinVM = nixpkgs.lib.nixosSystem {
     system = "aarch64-linux";
     specialArgs = attrs;
