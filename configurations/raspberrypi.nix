17b9a411487ca0dbcec2fc4d7e79885ea5e3ec1e
Tomas Harkema <tomas@harkema.io>
2023-12-16 19:20:07 +0100

    keys

--- configurations/raspberrypi.nix
+++ configurations/raspberrypi.nix
@@ -54,6 +54,7 @@ nixpkgs.lib.nixosSystem {
       hardware.enableRedistributableFirmware = true;
 
       users.users.root.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMQkKn73qM9vjYIaFt94Kj/syd5HCw2GdpiZ3z5+Rp/r tomas@supermicro"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILgD7me/mlDG89ZE/tLTJeNhbo3L+pi7eahB2rUneSR4 tomas"
       ];
 
