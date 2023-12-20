2219703b16559c9e310ca29026b31f8d43050f71
Tomas Harkema <tomas@harkema.io>
2023-12-20 02:52:04 +0100

    fixes

--- apps/atuin/default.nix
+++ apps/atuin/default.nix
@@ -2,18 +2,23 @@
 , pkgs
 , ...
 }: {
-  age.secrets.atuin-key = {
-    file = ../../secrets/atuin-key.age;
-    path = "/tmp/atuin.key";
-    # owner = "tomas";
-    # group = "tomas";
-  };
-  age.secrets.atuin-session = {
-    file = ../../secrets/atuin-session.age;
-    path = "/tmp/atuin.session";
-    # owner = "tomas";
-    # group = "tomas";
-  };
+  # age.secrets.atuin-key = {
+  #   file = ../../secrets/atuin-key.age;
+  #   path = "/tmp/atuin.key";
+  #   # owner = "tomas";
+  #   # group = "tomas";
+  # };
+  # age.secrets.atuin-session = {
+  #   file = ../../secrets/atuin-session.age;
+  #   path = "/tmp/atuin.session";
+  #   # owner = "tomas";
+  #   # group = "tomas";
+  # };
+
+  # programs.zsh.initExtraFirst = ''
+  #   cat ${config.age.secrets.atuin-key.path} > /tmp/agent-key-write.key
+  #   cat ${config.age.secrets.atuin-session.path} > /tmp/agent-session-write.key
+  # '';
 
   home.packages = with pkgs; [ atuin ];
   programs.atuin = {
@@ -23,8 +28,8 @@
     settings = {
       sync_frequency = "15m";
       sync_address = "https://atuin.harke.ma";
-      key_path = config.age.secrets.atuin-key.path;
-      session_path = config.age.secrets.atuin-session.path;
+      # key_path = "/tmp/agent-key-write.key";
+      # session_path = "/tmp/agent-session-write.key";
       enter_accept = false;
       workspaces = true;
     };
