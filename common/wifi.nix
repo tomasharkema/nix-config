85cbc4edc90a297136ee5d53d9a852437d179c1a
Tomas Harkema <tomas@harkema.io>
2023-12-21 20:20:18 +0100

    fixes

--- common/wifi.nix
+++ common/wifi.nix
@@ -4,12 +4,12 @@
     file = ../secrets/wireless.age;
     mode = "0664";
   };
-
+  networking.networkmanager.enable = true;
   networking.wireless = {
 
     environmentFile = config.age.secrets."wireless".path;
     networks = {
-      "Have a good day".psk = "@BRANDON_HOME@";
+      "Have a good day".psk = "0fcc36c0dd587f3d85028f427c872fead0b6bb7623099fb4678ed958f2150e23";
     };
 
     # enable = true;
