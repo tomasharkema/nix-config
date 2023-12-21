43b0d195147808dd9e4932f9f34b88728e0cbbf9
Tomas Harkema <tomas@harkema.io>
2023-12-22 00:24:00 +0100

    ojojoj

--- rundesk/imager.sh
+++ rundesk/imager.sh
@@ -1,23 +1,8 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p jq -p bash -p git
-#! nix develop
+#! /usr/bin/env sh
 
-set -x
-set -e
+cd $WORK_DIR
 
-echo @file.output-image.sha@
-
-TEMP=$(mktemp -d)
-
-echo "RUNDECK:DATA:TEMP = $TEMP"
-
-cd $TEMP
-
-git clone git@github.com:tomasharkema/nix-config.git
-
-cd $TEMP/nix-config
-
-OUT="$TEMP/@option.image@"
+OUT="$WORK_DIR/@option.image@"
 
 RES="$(nix build ".#images.x86_64-linux.@option.image@" --out-link "$OUT" --json --accept-flake-config)"
 echo $RES | jq
@@ -28,7 +13,5 @@ OUTPUT_FILE=$HOME/$FILENAME
 tar chvf - "$OUT" | pv -N in -B 100M | zstd -e - | pv -N out -B 100M >$OUTPUT_FILE
 
 # cp $OUT @file.outputfile.sha@
-
-rm -rf $TEMP
 
 echo "RUNDECK:DATA:OUTPUT_FILE = $OUTPUT_FILE"
