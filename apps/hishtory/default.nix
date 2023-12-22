8859a9b242b363669356422b6e67532b65e9df0b
Tomas Harkema <tomas@harkema.io>
2023-12-22 19:52:09 +0100

    fixes

--- apps/hishtory/default.nix
+++ apps/hishtory/default.nix
@@ -1,29 +1,56 @@
-{ lib, pkgs, ... }: rec {
-  # https://github.com/ddworken/hishtory
+# { lib, pkgs, ... }: rec {
+#   # https://github.com/ddworken/hishtory
 
-  hishtory = lib.stdenv.buildGoModule rec {
+#   hishtory = lib.stdenv.buildGoModule rec {
 
-    pname = "hishtory";
-    version = "0.263";
+#     pname = "hishtory";
+#     version = "0.263";
 
-    goPackagePath = "github.com/ddworken/hishtory";
+#     goPackagePath = "github.com/ddworken/hishtory";
 
-    allowGoReference = true;
+#     allowGoReference = true;
 
-    src = pkgs.fetchFromGitHub {
-      owner = "ddworken";
-      repo = "hishtory";
-      rev = "v${version}";
-      hash = "sha256-aVmzrYzFws6MlUnzhz4DaCp4NWGkhKeVyeezdlaBGts=";
-    };
+#     src = pkgs.fetchFromGitHub {
+#       owner = "ddworken";
+#       repo = "hishtory";
+#       rev = "v${version}";
+#       hash = "sha256-aVmzrYzFws6MlUnzhz4DaCp4NWGkhKeVyeezdlaBGts=";
+#     };
 
-    meta = with lib; {
-      description = "Simple command-line snippet manager, written in Go";
-      homepage = "https://github.com/knqyf263/pet";
-      license = licenses.mit;
-      maintainers = with maintainers; [ kalbasit ];
-    };
+#     meta = with lib; {
+#       description = "Simple command-line snippet manager, written in Go";
+#       homepage = "https://github.com/ddworken/hishtory";
+#       license = licenses.mit;
+#       maintainers = with maintainers; [ tomasharkema ];
+#     };
 
-    vendorHash = "AAAA";
+#     vendorHash = "AAAA";
+#   };
+# }
+
+{ pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [
+        (import "${fetchTree gomod2nix.locked}/overlay.nix")
+      ];
+    }
+  )
+, buildGoApplication ? pkgs.buildGoApplication
+}:
+
+buildGoApplication rec {
+  pname = "hishtory";
+  version = "0.263";
+  pwd = ./.;
+  src = pkgs.fetchFromGitHub {
+    owner = "ddworken";
+    repo = "hishtory";
+    rev = "v${version}";
+    hash = "sha256-aVmzrYzFws6MlUnzhz4DaCp4NWGkhKeVyeezdlaBGts=";
   };
+  modules = ./gomod2nix.toml;
 }
