3f5d0c54fef02373e311b23dd50fb4dc36a8a32a
Tomas Harkema <tomas@harkema.io>
2023-12-23 04:19:14 +0100

    fix

--- apps/hishtory/default.nix
+++ apps/hishtory/default.nix
@@ -12,7 +12,7 @@ let
       buildGoModule
         rec {
           pname = "hishtory";
-          version = "0.262";
+          version = "0.263";
 
           src = fetchFromGitHub {
             owner = "ddworken";
@@ -24,7 +24,7 @@ let
           vendorHash = "sha256-HzHLGrPXUSkyt2Dr7tLjfJrbg/EPBHkljoXIlPWIppU=";
           ldflags = [ " -X github.com/ddworken/hishtory/client/lib.Version=${version}" ];
 
-          excludedPackages = [ "backend/server" ];
+          excludedPackages = [ "backend/server" "client" ];
 
           postInstall = ''
             mkdir -p $out/share/hishtory
