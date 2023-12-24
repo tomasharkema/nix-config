63f4e87526e29d3bc7575d8a930b1cbc11443fc3
Tomas Harkema <tomas@harkema.io>
2023-12-24 18:27:37 +0100

    fix

--- configurations/raspberrypi.nix
+++ configurations/raspberrypi.nix
@@ -87,7 +87,7 @@ in {
         "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
         impermanence.nixosModules.impermanence
         agenix.nixosModules.default
-        disko.nixosModules.default
+        # disko.nixosModules.default
         ../secrets
         defaults
         ({
@@ -128,29 +128,29 @@ in {
           #   fsType = "vfat";
           # };
 
-          environment.persistence."/nix/persistent" = {
-            hideMounts = true;
-            directories = [
-              "/var/log"
-              "/var/lib/bluetooth"
-              "/var/lib/nixos"
-              "/var/lib/systemd/coredump"
-              "/etc/NetworkManager/system-connections"
-              {
-                directory = "/var/lib/colord";
-                user = "colord";
-                group = "colord";
-                mode = "u=rwx,g=rx,o=";
-              }
-            ];
-            files = [
-              "/etc/machine-id"
-              {
-                file = "/etc/nix/id_rsa";
-                parentDirectory = {mode = "u=rwx,g=,o=";};
-              }
-            ];
-          };
+          # environment.persistence."/nix/persistent" = {
+          #   hideMounts = true;
+          #   directories = [
+          #     "/var/log"
+          #     "/var/lib/bluetooth"
+          #     "/var/lib/nixos"
+          #     "/var/lib/systemd/coredump"
+          #     "/etc/NetworkManager/system-connections"
+          #     {
+          #       directory = "/var/lib/colord";
+          #       user = "colord";
+          #       group = "colord";
+          #       mode = "u=rwx,g=rx,o=";
+          #     }
+          #   ];
+          #   files = [
+          #     "/etc/machine-id"
+          #     {
+          #       file = "/etc/nix/id_rsa";
+          #       parentDirectory = {mode = "u=rwx,g=,o=";};
+          #     }
+          #   ];
+          # };
         })
       ]
       ++ homemanager;
