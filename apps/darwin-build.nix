3d2417af24858fb6fdb2b9ff2462b59386fda5f3
Tomas Harkema <tomas@harkema.io>
2023-12-24 18:44:36 +0100

    fix rpi

--- apps/darwin-build.nix
+++ apps/darwin-build.nix
@@ -1,7 +1,16 @@
-{ pkgs, ... }:
-let
-  darwin-build = pkgs.writeShellScriptBin "darwin-build" ''
-    darwin-rebuild switch --flake ~/Developer/nix-config
-  '';
-in
-[ darwin-build ]
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  darwin-build =
+    #lib.mkIf pkgs.stdenv.isDarwin
+    pkgs.writeShellScriptBin "darwin-build" ''
+      darwin-rebuild switch --flake ~/Developer/nix-config
+    '';
+  nixos-build =
+    #lib.mkIf pkgs.stdenv.isLinux
+    pkgs.writeShellScriptBin "nixos-build" ''
+      sudo nixos-rebuild switch --flake ~/nix-config --log-format internal-json -v |& nom --json
+    '';
+in [darwin-build nixos-build]
