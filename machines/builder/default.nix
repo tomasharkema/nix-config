6b96fe04e4606bdb4ac202f1f1b3e99578625303
Tomas Harkema <tomas@harkema.io>
2023-12-24 23:53:29 +0100

    fix apps

--- machines/builder/default.nix
+++ machines/builder/default.nix
@@ -1,14 +1,13 @@
-{ nix-darwin
-, nixpkgs
-, lib
-, ...
-}@inputs:
-let
-
+{
+  nix-darwin,
+  nixpkgs,
+  lib,
+  ...
+} @ inputs: let
   inherit (nix-darwin.lib) darwinSystem;
   system = "aarch64-darwin";
   pkgs = nixpkgs.legacyPackages."${system}";
-  linuxSystem = builtins.replaceStrings [ "darwin" ] [ "linux" ] system;
+  linuxSystem = builtins.replaceStrings ["darwin"] ["linux"] system;
 
   darwin-builder = nixpkgs.lib.nixosSystem {
     system = linuxSystem;
@@ -16,7 +15,7 @@ let
       "${nixpkgs}/nixos/modules/profiles/macos-builder.nix"
       {
         # imports = [ ../../apps/tailscale ];
-        boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
+        boot.binfmt.emulatedSystems = ["x86_64-linux"];
         virtualisation = {
           host.pkgs = pkgs;
           useNixStoreImage = true;
@@ -31,57 +30,11 @@ let
         };
 
         networking.useDHCP = true;
-        environment.systemPackages = with pkgs; [ wget curl cacert ];
-
+        environment.systemPackages = with pkgs; [wget curl cacert];
       }
     ];
   };
-  inherit (pkgs) stdenvNoCC;
-  launchcontrol = stdenvNoCC.mkDerivation
-    (finalAttrs: {
-      pname = "LaunchControl";
-      version = "2.5.1";
-
-      src = fetchTarball {
-        # name = "LaunchControl-2.5.1.tar.xz";
-        url = "https://www.soma-zone.com/download/files/LaunchControl-2.5.1.tar.xz";
-        sha256 = "sha256:0zj4inl00sqrckzdlcym5af8s0rqq8dyp7fwr4cyndgz7yvkfvz2";
-      };
-
-      dontPatch = true;
-      dontConfigure = true;
-      dontBuild = true;
-      dontFixup = true;
-
-      nativeBuildInputs = [ pkgs.xz ];
-
-      installPhase = ''
-        runHook preInstall
-
-        mkdir -p $out/Applications/LaunchControl.app
-        cp -R . $out/Applications/LaunchControl.app
-
-        runHook postInstall
-      '';
-
-      meta = with lib; {
-        description = "LaunchControl";
-        longDescription = ''
-          LaunchControl
-        '';
-        homepage = "https://v2.airbuddy.app";
-        changelog = "https://support.airbuddy.app/articles/airbuddy-2-changelog";
-        license = with licenses; [
-
-        ];
-        sourceProvenance = with sourceTypes; [ binaryNativeCode ];
-        maintainers = with maintainers; [ tomasharkema ];
-        platforms = [ "aarch64-darwin" "x86_64-darwin" ];
-      };
-    });
-in
-
-{
+in {
   nix.distributedBuilds = true;
 
   nix.buildMachines = [
@@ -89,7 +42,7 @@ in
       hostName = "builder@linux-builder";
       system = linuxSystem;
       maxJobs = 4;
-      supportedFeatures = [ "kvm" "benchmark" "big-parallel" ];
+      supportedFeatures = ["kvm" "benchmark" "big-parallel"];
       sshKey = "/var/lib/darwin-builder/keys/builder_ed25519";
       speedFactor = 30;
     }
@@ -97,7 +50,7 @@ in
       hostName = "builder@linux-builder";
       system = "x86_64-linux";
       maxJobs = 4;
-      supportedFeatures = [ "kvm" "benchmark" "big-parallel" ];
+      supportedFeatures = ["kvm" "benchmark" "big-parallel"];
       sshKey = "/var/lib/darwin-builder/keys/builder_ed25519";
       speedFactor = 30;
     }
@@ -115,8 +68,4 @@ in
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowUnfreePredicate = _: true;
-
-  environment.systemPackages = lib.mkIf stdenvNoCC.isDarwin [
-    launchcontrol
-  ];
 }
