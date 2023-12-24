2d7ded3a287d2b7770ff51d8d3e9702dd789fa14
Tomas Harkema <tomas@harkema.io>
2023-12-24 19:31:21 +0100

    update atuin version

---
+++ apps/atuin/overlay.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  pkgs,
+  requireFile,
+  fetchFromGitHub,
+  ...
+}: self: super: {
+  atuin = super.houdini.overrideAttrs (old: rec {
+    name = "atuin";
+    version = "17.10";
+
+    src = fetchFromGitHub {
+      owner = "atuinsh";
+      repo = "atuin";
+      rev = "v${version}";
+      hash = "sha256-+qXKBHtEgMIh1kTb2z9M60GWMrPS8so79+7s+6ZEgyU=";
+    };
+  });
+}
