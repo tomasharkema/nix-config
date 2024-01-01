3ec9e584dbd2de5059d4664c45ac2e46c692ed7f
Tomas Harkema <tomas@harkema.io>
2024-01-01 21:55:08 +0100

    dafuck

--- modules/nixos/traits/hardware/tpm/default.nix
+++ modules/nixos/traits/hardware/tpm/default.nix
@@ -19,7 +19,7 @@ in {
       enable = true;
       pkcs11.enable = true;
       tctiEnvironment.enable = true;
-      abrmd.enable = true;
+      # abrmd.enable = true;
     };
 
     users.users."tomas".extraGroups = ["tss"];
@@ -29,14 +29,14 @@ in {
     services.tcsd.enable = true;
 
     environment.systemPackages = with pkgs; [
-      tpm2-tss
       tpm-luks
-      tpm2-totp
       tpm-tools
-      tpmmanager
-      tpm2-tools
       tpm2-abrmd
       tpm2-pkcs11
+      tpm2-tools
+      tpm2-totp
+      tpm2-tss
+      tpmmanager
     ];
   };
 }
