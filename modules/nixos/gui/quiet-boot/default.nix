940c3d8e256798fb287c6917d95d1009f9413f22
Tomas Harkema <tomas@harkema.io>
2024-01-02 02:09:36 +0100

    fix console level

--- modules/nixos/gui/quiet-boot/default.nix
+++ modules/nixos/gui/quiet-boot/default.nix
@@ -47,8 +47,8 @@ in {
         "rd.udev.log_level=3"
         "vt.global_cursor_default=0"
       ];
-      consoleLogLevel = 0;
-      initrd.verbose = false;
+      consoleLogLevel = lib.mkDefault 0;
+      initrd.verbose = lib.mkDefault false;
     };
   };
 }
