2955c17ee83a58a6e6c30725c2d61bf8287c760b
Tomas Harkema <tomas@harkema.io>
2024-01-03 17:49:47 +0100

    fixes

--- flake.nix
+++ flake.nix
@@ -136,6 +136,7 @@
     keep-derivations = true;
 
     allowed-uris = [
+      "https://github.com/tomasharkema.keys"
       "https://git.sr.ht/~thatonelutenist/nix-cache-watcher"
       "https://api.flakehub.com/f/pinned"
       "https://github.com/NixOS/"
