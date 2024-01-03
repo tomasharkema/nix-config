33921b5854c812ca0a3feed4a1e2bf0fa3c7d068
Tomas Harkema <tomas@harkema.io>
2024-01-03 15:56:42 +0100

    maybe like this?

---
+++ .hydra/default.nix
@@ -0,0 +1,45 @@
+{pulls ? ../simple-pr-dummy.json}: let
+  pkgs = import <nixpkgs> {};
+in
+  with import ../lib.nix;
+  with pkgs.lib; let
+    defaults = {
+      enabled = 1;
+      hidden = false;
+      keepnr = 10;
+      schedulingshares = 100;
+      checkinterval = 600;
+      enableemail = false;
+      emailoverride = "";
+      nixexprinput = "nix-config";
+      nixexprpath = "default.nix";
+    };
+    primary_jobsets = {
+      nix-config-unstable =
+        defaults
+        // {
+          description = "nix-config";
+          inputs = {
+            config = mkFetchGithub "https://github.com/tomasharkema/nix-config snowfall";
+            nixpkgs = mkFetchGithub "https://github.com/nixos/nixpkgs nixos-unstable-small";
+          };
+        };
+    };
+    pr_data = builtins.fromJSON (builtins.readFile pulls);
+    makePr = num: info: {
+      name = "nix-config-${num}";
+      value =
+        defaults
+        // {
+          description = "PR ${num}: ${info.title}";
+          inputs = {
+            nix-config = mkFetchGithub "https://github.com/${info.head.repo.owner.login}/${info.head.repo.name}.git ${info.head.ref}";
+            nixpkgs = mkFetchGithub "https://github.com/nixos/nixpkgs nixos-unstable-small";
+          };
+        };
+    };
+    pull_requests = listToAttrs (mapAttrsToList makePr pr_data);
+    jobsetsAttrs = pull_requests // primary_jobsets;
+  in {
+    jobsets = makeSpec jobsetsAttrs;
+  }
