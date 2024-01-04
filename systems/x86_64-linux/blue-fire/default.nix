31e507059d7c368060b9e7163fb08333299c3adc
Tomas Harkema <tomas@harkema.io>
2024-01-04 22:13:21 +0100

    fix bootloader

--- systems/x86_64-linux/blue-fire/default.nix
+++ systems/x86_64-linux/blue-fire/default.nix
@@ -93,8 +93,9 @@ in {
     #   allowedUDPPorts = [ 2049 ];
     # };
 
-    # boot.loader.systemd-boot.enable = true;
-    # boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
     nix.buildMachines = [
       {
         hostName = "localhost";
