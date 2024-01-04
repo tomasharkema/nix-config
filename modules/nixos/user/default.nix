c6622badca468642795d11b441e17bb2164ceb78
Tomas Harkema <tomas@harkema.io>
2024-01-04 23:16:40 +0100

    fix

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -82,7 +82,6 @@ in {
       openssh.authorizedKeys.keys = keys;
     };
 
-    nix.distributedBuilds = true;
     # optional, useful when the builder has a faster internet connection than yours
     nix.extraOptions = ''
       builders-use-substitutes = true
