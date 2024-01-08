258dc55651de037bfe6b949391486381728e82b0
Tomas Harkema <tomas@harkema.io>
2024-01-08 16:02:04 +0100

    push hw

--- systems/aarch64-linux/euro-mir-vm/default.nix
+++ systems/aarch64-linux/euro-mir-vm/default.nix
@@ -6,9 +6,16 @@
 }: {
   imports = with inputs; [
     "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+    ./hardware-configuration.nix
   ];
 
   config = {
     networking.hostName = "euro-mir-vm";
+    swapDevices = [
+      {
+        device = "/swapfile";
+        size = 1024;
+      }
+    ];
   };
 }
