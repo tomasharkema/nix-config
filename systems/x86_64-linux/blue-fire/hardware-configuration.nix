434d7b05abd3c3b4a51ec5c0478a794d66b65012
Tomas Harkema <tomas@harkema.io>
2024-01-09 19:53:45 +0100

    fixes

--- systems/x86_64-linux/blue-fire/hardware-configuration.nix
+++ systems/x86_64-linux/blue-fire/hardware-configuration.nix
@@ -31,13 +31,13 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  networking.interfaces.eno2.useDHCP = lib.mkDefault true;
-  networking.interfaces.eno3.useDHCP = lib.mkDefault true;
-  networking.interfaces.eno4.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp6s0f0.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp6s0f1.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp6s0f2.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp6s0f3.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno2.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno3.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno4.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp6s0f0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp6s0f1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp6s0f2.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp6s0f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode =
