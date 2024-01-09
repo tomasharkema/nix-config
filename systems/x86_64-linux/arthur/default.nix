d9674e18222eee5fbeb8c09c63fec35728f7ae80
Tomas Harkema <tomas@harkema.io>
2024-01-09 17:39:27 +0100

    fixes?

--- systems/x86_64-linux/arthur/default.nix
+++ systems/x86_64-linux/arthur/default.nix
@@ -42,7 +42,7 @@
     disks.btrfs = {
       enable = true;
       main = "/dev/disk/by-id/ata-Samsung_SSD_850_PRO_256GB_S39KNX0J775697K";
-      mainOverride = "vdb";
+      # media = "";
     };
 
     apps.resilio.root = "/media/resilio";
