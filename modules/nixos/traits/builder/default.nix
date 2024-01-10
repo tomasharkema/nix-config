66d07c043b9b887afe37307640035de71f2067db
Tomas Harkema <tomas@harkema.io>
2024-01-10 04:01:05 +0100

    meh builder?

--- modules/nixos/traits/builder/default.nix
+++ modules/nixos/traits/builder/default.nix
@@ -70,6 +70,25 @@ in {
           supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
           maxJobs = 2;
         }
+
+        {
+          hostName = "wodan-vm";
+          systems = ["x86_64-linux" "aarch64-linux" "i686-linux"];
+          supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
+          maxJobs = 2;
+        }
+        {
+          hostName = "enzian";
+          systems = ["x86_64-linux" "aarch64-linux" "i686-linux"];
+          supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
+          maxJobs = 2;
+        }
+        {
+          hostName = "arthur";
+          systems = ["x86_64-linux" "aarch64-linux" "i686-linux"];
+          supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
+          maxJobs = 2;
+        }
       ];
 
       services.hydra = {
