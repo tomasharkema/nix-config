1ba52ed547e85c646820d190536bcbce67bcfc64
Tomas Harkema <tomas@harkema.io>
2024-01-10 04:40:04 +0100

    wsl

--- modules/nixos/traits/builder/default.nix
+++ modules/nixos/traits/builder/default.nix
@@ -59,6 +59,12 @@ in {
           maxJobs = 2;
         }
         {
+          hostName = "builder@wodan-wsl";
+          systems = ["x86_64-linux" "aarch64-linux" "i686-linux"];
+          supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
+          maxJobs = 2;
+        }
+        {
           hostName = "builder@enzian";
           systems = ["x86_64-linux" "aarch64-linux" "i686-linux"];
           supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
