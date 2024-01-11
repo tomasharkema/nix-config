32b6b06d0153747305bcdebb2eb585435ab40a38
Tomas Harkema <tomas@harkema.io>
2024-01-11 19:32:54 +0100

    add wodan

--- systems/x86_64-linux/blue-fire/default.nix
+++ systems/x86_64-linux/blue-fire/default.nix
@@ -106,6 +106,7 @@ in {
 
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.systemd-boot.configurationLimit = 10;
 
     boot = {
       binfmt.emulatedSystems = ["aarch64-linux"];
---
+++ systems/x86_64-linux/wodan/default.nix
@@ -0,0 +1,32 @@
+{lib, ...}: {
+  config = {
+    time.hardwareClockInLocalTime = true;
+
+    networking = {
+      networkmanager.enable = true;
+
+      hostName = lib.mkDefault "wodan";
+
+      firewall = {
+        enable = false;
+      };
+      useDHCP = lib.mkDefault true;
+    };
+
+    disks.btrfs = {
+      enable = true;
+      main = "/dev/disk/by-id/usb-TOSHIBA_MK3263GSXN_111122223333-0:0";
+    };
+    boot.loader = {
+      efi = {
+        # canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      grub = {
+        enable = true;
+        device = "nodev";
+        useOSProber = true;
+      };
+    };
+  };
+}
