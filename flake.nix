786e2529fe290dd932443b6e95f34e0e785c5259
Tomas Harkema <tomas@harkema.io>
2024-01-12 00:38:20 +0100

    remove zsf

--- flake.nix
+++ flake.nix
@@ -280,9 +280,11 @@
       images = with inputs; {
         baaa-express = self.nixosConfigurations.baaa-express.config.system.build.sdImage;
         pegasus = self.nixosConfigurations.pegasus.config.system.build.sdImage;
+        # arthuriso = self.nixosConfigurations.arthur.config.formats.install-iso;
 
-        arthuriso = self.nixosConfigurations.arthur.config.formats.install-iso;
-        euro-mir-vm = self.nixosConfigurations.euro-mir-vm.config.formats.install-iso;
+        euro-mir-vm = self.nixosConfigurations.euro-mir-vm.config.system.build.isoImage;
+
+        installer = self.nixosConfigurations.installer.config.system.build.isoImage;
 
         # "blue-fire" = self.nixosConfigurations."blue-fire".config.formats.install-iso;
         # "blue-fire-slim" = self.nixosConfigurations."blue-fire-slim".config.formats.install-iso;
