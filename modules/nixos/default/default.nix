7e4df8d8d5a7da1638e69a61e09b71ca6db6ac81
Tomas Harkema <tomas@harkema.io>
2024-01-14 23:08:40 +0100

    hmmm

--- modules/nixos/default/default.nix
+++ modules/nixos/default/default.nix
@@ -92,7 +92,8 @@
         url = "https://ipa.harkema.io/ipa/config/ca.crt";
         sha256 = "sha256-3XRsoBALVsBVG9HQfh9Yq/OehvPPiOuZesSgtWXh74I=";
       };
-      dyndns.enable = true; # TODO: enable this??
+      dyndns.enable = false; # TODO: enable this??
     };
+    documentation.nixos.enable = true;
   };
 }
