9ec67e946989eaaf0ea81a4ec3e01712444a6f71
Tomas Harkema <tomas@harkema.io>
2024-01-15 13:25:06 +0100

    oop

--- systems/x86_64-linux/euro-mir-2/default.nix
+++ systems/x86_64-linux/euro-mir-2/default.nix
@@ -66,7 +66,7 @@
     boot = {
       binfmt.emulatedSystems = ["aarch64-linux"];
       # kernelPackages = pkgs.linuxPackages;
-      blacklistedModules = ["i915"];
+      blacklistedKernelModules = ["i915"];
     };
     programs.mtr.enable = true;
     # programs.gnupg.agent = {
