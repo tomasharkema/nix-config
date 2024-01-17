17c23591e642bf0119c1d420ada3c43a0cda39fa
Tomas Harkema <tomas@harkema.io>
2024-01-17 15:37:55 +0100

    derp

--- modules/nixos/apps/keybase/default.nix
+++ modules/nixos/apps/keybase/default.nix
@@ -8,14 +8,14 @@
 with lib; {
   config = mkIf (!config.traits.slim.enable) {
     services.kbfs = {
-      enable = true;
+      # enable = true;
       enableRedirector = true;
     };
     services.keybase = {
       enable = true;
     };
-    security.wrappers.keybase-redirector.owner = "tomas";
-    security.wrappers.keybase-redirector.group = "tomas";
+    security.wrappers.keybase-redirector.owner = "root";
+    security.wrappers.keybase-redirector.group = "root";
     environment.systemPackages = with pkgs; mkIf (config.gui.enable && pkgs.system == "x86_64-linux") [keybase-gui];
 
     # environment.systemPackages = with pkgs; [keybase kbfs];
