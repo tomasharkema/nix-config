0a5f0a5ca2a38fe9c33f931c9ade9c3e0ffb5491
Tomas Harkema <tomas@harkema.io>
2024-01-17 13:51:50 +0100

    hmm

--- modules/nixos/apps/keybase/default.nix
+++ modules/nixos/apps/keybase/default.nix
@@ -11,14 +11,14 @@ with lib; {
     services.kbfs = {
       enable = true;
       # package = inputs.unstable.legacyPackages."${pkgs.system}".kbfs;
-      enableRedirector = true;
+      # enableRedirector = true;
     };
     services.keybase = {
       enable = true;
       # package = inputs.unstable.legacyPackages."${pkgs.system}".keybase;
     };
-    security.wrappers.keybase-redirector.owner = "tomas";
-    security.wrappers.keybase-redirector.group = "tomas";
+    # security.wrappers.keybase-redirector.owner = "tomas";
+    # security.wrappers.keybase-redirector.group = "tomas";
     environment.systemPackages = with pkgs; mkIf (config.gui.enable && pkgs.system == "x86_64-linux") [keybase-gui];
 
     # environment.systemPackages = with pkgs; [keybase kbfs];
