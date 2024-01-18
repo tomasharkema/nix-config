73669e17ada933afa8d499b7a8680264db67d99e
Tomas Harkema <tomas@harkema.io>
2024-01-18 15:29:56 +0100

    ojoo

--- modules/nixos/default/default.nix
+++ modules/nixos/default/default.nix
@@ -102,10 +102,10 @@
       };
       services.udev = {enable = lib.mkDefault true;};
 
-      environment.variables = {
-        OP_CONNECT_HOST = "http://silver-star.ling-lizard.ts.net:8080";
-        OP_CONNECT_TOKEN = "${config.age.secrets.op.path}";
-      };
+      # environment.variables = {
+      #   OP_CONNECT_HOST = "http://silver-star.ling-lizard.ts.net:8080";
+      #   OP_CONNECT_TOKEN = "${config.age.secrets.op.path}";
+      # };
 
       programs.zsh = {
         enable = true;
