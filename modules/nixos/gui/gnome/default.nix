a5e2992c6cb784f907d4b0e3988e945c48da17af
Tomas Harkema <tomas@harkema.io>
2024-01-18 02:45:54 +0100

    fixes

--- modules/nixos/gui/gnome/default.nix
+++ modules/nixos/gui/gnome/default.nix
@@ -133,22 +133,22 @@ in {
 
     environment.gnome.excludePackages =
       (with pkgs; [
-        gnome-photos
+        # gnome-photos
         gnome-tour
       ])
       ++ (with pkgs.gnome; [
         cheese # webcam tool
-        gnome-music
+        # gnome-music
         gedit # text editor
         epiphany # web browser
-        geary # email reader
+        # geary # email reader
         gnome-characters
         tali # poker game
         iagno # go game
         hitori # sudoku game
         atomix # puzzle game
         yelp # Help view
-        gnome-contacts
+        # gnome-contacts
         gnome-initial-setup
       ]);
   };
