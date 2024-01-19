f7a6cbbb048c8c3c2c92b0f15548b508337d5701
Tomas Harkema <tomas@harkema.io>
2024-01-19 20:28:14 +0100

    derp

---
+++ modules/nixos/gui/apps/spotify/default.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  cfg = config.gui;
+in {
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      spotify
+      spotifyd
+    ];
+
+    services.spotifyd = {
+      enable = true;
+      config = ''
+        [global]
+        username_cmd = "${lib.getExe pkgs._1password} item get bnzrqxggvfbfhgln4uceawfbbq --field username"
+        password_cmd = "${lib.getExe pkgs._1password} item get bnzrqxggvfbfhgln4uceawfbbq --field password"
+      '';
+    };
+    systemd.services.spotifyd = {
+      environment = {
+        OP_CONNECT_HOST = "http://silver-star.ling-lizard.ts.net:8080";
+        OP_CONNECT_TOKEN = "${config.age.secrets.op.path}";
+      };
+    };
+
+    networking.firewall = {
+      allowedUDPPorts = [33677];
+      allowedTCPPorts = [33677];
+    };
+  };
+}
--- modules/nixos/traits/hardware/remote-unlock/default.nix
+++ modules/nixos/traits/hardware/remote-unlock/default.nix
@@ -36,46 +36,65 @@ in {
     boot = {
       initrd = {
         luks.devices."crypted" = {
-          keyFile = "/key/key";
+          # keyFile = "/key/key";
+          # additionalKeyFiles = ["/key/key"];
         };
+
+        extraUtilsCommands = ''
+          copy_bin_and_libs ${pkgs.haveged}/bin/haveged
+        '';
 
         verbose = true;
 
-        systemd = {
-          enable = true;
-          users.root.shell = "/bin/systemd-tty-ask-password-agent";
+        postDeviceCommands = pkgs.lib.mkBefore ''
+          set -x
+          mkdir -m 0755 -p /key
+          sleep 2 # To make sure the usb key has been loaded
+          mount -n -t vfat -o ro /dev/disk/by-partlabel/a7098897-2784-4776-bd3d-0e217d85963d /key
+        '';
 
-          services.key-usb = {
-            description = "Rollback BTRFS root subvolume to a pristine state";
-            wantedBy = [
-              "initrd.target"
-            ];
-            after = [
-              # LUKS/TPM process
-              "systemd-cryptsetup@enc.service"
-            ];
-            before = [
-              "sysroot.mount"
-            ];
-            unitConfig.DefaultDependencies = "no";
-            serviceConfig.Type = "oneshot";
-            script = ''
-              mkdir -m 0755 -p /key
-              sleep 2 # To make sure the usb key has been loaded
-              mount -n -t vfat -o ro /dev/disk/by-partlabel/a7098897-2784-4776-bd3d-0e217d85963d /key
-            '';
-          };
-        };
+        # systemd = {
+        #   emergencyAccess = true;
+        #   enable = true;
+        #   users.root.shell = "/bin/systemd-tty-ask-password-agent";
+
+        #   services.key-usb = {
+        #     description = "Rollback BTRFS root subvolume to a pristine state";
+        #     # wantedBy = [
+        #     #   "initrd.target"
+        #     # ];
+        #     # after = [
+        #     #   # LUKS/TPM process
+        #     #   "systemd-cryptsetup@enc.service"
+        #     # ];
+        #     # before = [
+        #     #   "sysroot.mount"
+        #     # ];
+        #     wantedBy = ["sysinit.target" "initrd.target"];
+        #     before = ["cryptsetup-pre.target" "shutdown.target" "initrd-switch-root.service"];
+        #     after = ["systemd-cryptsetup@enc.service"];
+
+        #     unitConfig.DefaultDependencies = "no";
+        #     serviceConfig.Type = "oneshot";
+
+        #     script = ''
+        #       mkdir -m 0755 -p /key
+        #       sleep 2 # To make sure the usb key has been loaded
+        #       mount -n -t vfat -o ro /dev/disk/by-partlabel/a7098897-2784-4776-bd3d-0e217d85963d /key
+        #     '';
+        #   };
+        # };
         availableKernelModules = ["e1000e" "r8169"];
         kernelModules = ["uas" "usbcore" "usb_storage" "vfat" "nls_cp437" "nls_iso8859_1"];
 
         network = {
           enable = true;
           flushBeforeStage2 = true;
+          # udhcpc.enable = true;
           ssh = {
             enable = true;
             port = 22222;
-
+            shell = "/bin/cryptsetup-askpass";
             authorizedKeys = config.user.keys;
             hostKeys = [
               "/boot/secrets/ssh_host_ed25519_key"
