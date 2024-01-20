9aac56f64c2bff0ad14f564c5f59a74f1c3053db
tomas <tomas@harkema.io>
2024-01-20 16:07:58 +0100

    meh

--- packages/nixpkgs-docset/default.nix
+++ packages/nixpkgs-docset/default.nix
@@ -7,7 +7,7 @@ pkgs.stdenvNoCC.mkDerivation rec {
   name = "zeal-docset";
   version = "2024-01-19";
   src = builtins.fetchTarball {
-    url = "https://nixosbrasil.github.io/nix-docgen/nixpkgs-23.11/nixpkgs.docset.tgz";
+    url = "https://nixosbrasil.github.io/nix-docgen/nixpkgs-unstable/nixpkgs.docset.tgz";
     sha256 = "sha256:0z6iahfihj90k5y0ra2zivfq2p8qgnnifbs09y0v11x5hwjjyhy6";
   };
 
