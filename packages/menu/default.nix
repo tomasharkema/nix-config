7c51105fd5957a98760a90521a93a8833802ade2
tomas <tomas@harkema.io>
2024-01-22 00:45:14 +0100

    implement menu

--- packages/menu/default.nix
+++ packages/menu/default.nix
@@ -5,9 +5,37 @@
 }:
 pkgs.writeShellApplication {
   name = "menu";
-  runtimeInputs = with pkgs; [jq gum nix-eval-jobs nixos-rebuild];
+  
+  runtimeInputs = with pkgs; [gum nixos-rebuild nix-output-monitor zsh];
+
   text = ''
-    gum confirm "Wanna run an update?" && \
-      gum spin --spinner dot --title "Building..." -- sudo nixos-rebuild switch "github:tomasharkema/nix-config/update"
+    RUN_UPDATER="Run Updater"
+    OPEN_SHELL="Go to shell!"
+    EXIT="Exit"
+
+    update () {
+      sudo nixos-rebuild switch --flake "github:tomasharkema/nix-config/update" --log-format internal-json -v |& nom --json
+    }
+
+    CHOICE=$(gum choose "$RUN_UPDATER" "$OPEN_SHELL" "$EXIT")
+    
+    case $CHOICE in
+      "$RUN_UPDATER")
+        update
+        ;;
+  
+      "$OPEN_SHELL")
+        exec zsh
+        ;;
+
+      "$EXIT")
+        echo "Exitting..."
+        exit 0
+        ;;
+
+      *)
+        exit 1
+        ;;
+    esac
   '';
 }
