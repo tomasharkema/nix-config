ab7b5516259bc4eec2b11865ec56790a1a69d00e
Tomas Harkema <tomas@harkema.io>
2024-01-22 17:35:08 +0100

    run fast ssh

--- packages/menu/default.nix
+++ packages/menu/default.nix
@@ -5,11 +5,12 @@
 }:
 pkgs.writeShellApplication {
   name = "menu";
-  
-  runtimeInputs = with pkgs; [gum nixos-rebuild nix-output-monitor zsh];
+
+  runtimeInputs = with pkgs; [gum nixos-rebuild nix-output-monitor zsh fast-ssh];
 
   text = ''
-    RUN_UPDATER="Run Updater"
+    RUN_UPDATER="Run Updater..."
+    OPEN_SSH="SSH..."
     OPEN_SHELL="Go to shell!"
     EXIT="Exit"
 
@@ -17,13 +18,15 @@ pkgs.writeShellApplication {
       sudo nixos-rebuild switch --flake "github:tomasharkema/nix-config/update" --refresh --verbose --log-format internal-json -v |& nom --json
     }
 
-    CHOICE=$(gum choose "$RUN_UPDATER" "$OPEN_SHELL" "$EXIT")
-    
+    CHOICE=$(gum choose "$RUN_UPDATER" "$OPEN_SSH" "$OPEN_SHELL" "$EXIT")
+
     case $CHOICE in
       "$RUN_UPDATER")
         update
         ;;
-  
+      "$OPEN_SSH")
+        exec fast-ssh
+        ;;
       "$OPEN_SHELL")
         exec zsh
         ;;
