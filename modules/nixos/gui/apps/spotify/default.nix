0f42e19b93f83e10db1c8d0976a10bb40653ef14
Tomas Harkema <tomas@harkema.io>
2024-01-22 18:45:38 +0100

    fix spotifyd

--- modules/nixos/gui/apps/spotify/default.nix
+++ modules/nixos/gui/apps/spotify/default.nix
@@ -16,17 +16,17 @@ in {
 
     services.spotifyd = {
       enable = true;
-      config = ''
-        [global]
-        username_cmd = "${lib.getExe pkgs._1password} item get bnzrqxggvfbfhgln4uceawfbbq --field username"
-        password_cmd = "${lib.getExe pkgs._1password} item get bnzrqxggvfbfhgln4uceawfbbq --field password"
-      '';
+      # config = ''
+      #   [global]
+      #   username_cmd = "${lib.getExe pkgs._1password} item get bnzrqxggvfbfhgln4uceawfbbq --field username"
+      #   password_cmd = "${lib.getExe pkgs._1password} item get bnzrqxggvfbfhgln4uceawfbbq --field password"
+      # '';
     };
     systemd.services.spotifyd = {
-      environment = {
-        OP_CONNECT_HOST = "http://silver-star.ling-lizard.ts.net:7080";
-        OP_CONNECT_TOKEN = "${config.age.secrets.op.path}";
-      };
+      # environment = {
+      #   OP_CONNECT_HOST = "http://silver-star.ling-lizard.ts.net:7080";
+      #   OP_CONNECT_TOKEN = "${config.age.secrets.op.path}";
+      # };
     };
 
     networking.firewall = {
