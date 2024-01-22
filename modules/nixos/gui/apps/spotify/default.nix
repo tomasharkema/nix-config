9494d2c133f83a19b8bf1d78ffe1fd9db021c7ce
Tomas Harkema <tomas@harkema.io>
2024-01-22 19:01:47 +0100

    fix spotify

--- modules/nixos/gui/apps/spotify/default.nix
+++ modules/nixos/gui/apps/spotify/default.nix
@@ -16,18 +16,19 @@ in {
 
     services.spotifyd = {
       enable = true;
-      # config = ''
-      #   [global]
+      config = ''
+        [global]
+        use_mpris = false
+      '';
       #   username_cmd = "${lib.getExe pkgs._1password} item get bnzrqxggvfbfhgln4uceawfbbq --field username"
       #   password_cmd = "${lib.getExe pkgs._1password} item get bnzrqxggvfbfhgln4uceawfbbq --field password"
-      # '';
     };
-    systemd.services.spotifyd = {
-      # environment = {
-      #   OP_CONNECT_HOST = "http://silver-star.ling-lizard.ts.net:7080";
-      #   OP_CONNECT_TOKEN = "${config.age.secrets.op.path}";
-      # };
-    };
+    # systemd.services.spotifyd = {
+    # environment = {
+    #   OP_CONNECT_HOST = "http://silver-star.ling-lizard.ts.net:7080";
+    #   OP_CONNECT_TOKEN = "${config.age.secrets.op.path}";
+    # };
+    # };
 
     networking.firewall = {
       allowedUDPPorts = [33677];
