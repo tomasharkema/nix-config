ab9aff1cdf1e21b57415b359540bff047f9f9461
Tomas Harkema <tomas@harkema.io>
2024-01-24 00:24:35 +0100

    ojoo

--- modules/nixos/apps/tor/default.nix
+++ modules/nixos/apps/tor/default.nix
@@ -5,6 +5,23 @@
     services.tor = {
       enable = true;
       client.enable = true;
+
+      enableGeoIP = false;
+
+      relay.onionServices = {
+        ssh = {
+          version = 3;
+          map = [
+            {
+              port = 22;
+              target = {
+                addr = "127.0.0.1";
+                port = 22;
+              };
+            }
+          ];
+        };
+      };
     };
   };
 }
