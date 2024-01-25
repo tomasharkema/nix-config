13537be98b8c0bc86079f1160a5f9dcc6df2d6ae
Tomas Harkema <tomas@harkema.io>
2024-01-25 02:40:07 +0100

    fix log dashboard

--- packages/graylog-cli-dashboard/default.nix
+++ packages/graylog-cli-dashboard/default.nix
@@ -4,7 +4,7 @@
   fetchFromGitHub,
 }:
 buildNpmPackage rec {
-  pname = "cli-dashboard";
+  pname = "graylog-dashboard";
   version = "1.1.1";
 
   src = fetchFromGitHub {
@@ -14,13 +14,18 @@ buildNpmPackage rec {
     hash = "sha256-4x3gxaSxQ3eRniesHBmN5s/hDfsoXISRrEOEcxttMjA=";
   };
 
-  npmDepsHash = "sha256-tuEfyePwlOy2/mOPdXbqJskO6IowvAP4DWg8xSZwbJw=";
+  npmDepsHash = "sha256-5p7q09jQYQJw7FzgPTWyS2QpjTydBo0ImbbatVaardU=";
+
+  postPatch = ''
+    ln -s ${./package-lock.json} package-lock.json
+  '';
 
   # The prepack script runs the build script, which we'd rather do in the build phase.
   # npmPackFlags = ["--ignore-scripts"];
 
   # NODE_OPTIONS = "--openssl-legacy-provider";
-
+  dontBuild = true;
+  dontNpmBuild = true;
   meta = with lib; {
     description = "A modern web UI for various torrent clients with a Node.js backend and React frontend";
     homepage = "https://flood.js.org";
