2016093bdc4c1c41594cd29a2ab2b1d6efa2ab3c
Tomas Harkema <tomas@harkema.io>
2024-01-25 21:53:15 +0100

    fix script

--- packages/pvzstd/default.nix
+++ packages/pvzstd/default.nix
@@ -7,6 +7,6 @@ pkgs.writeShellApplication {
   name = "pvzstd";
   runtimeInputs = with pkgs; [gum pv zstd gnutar];
   text = ''
-    exec pv - -N in -B 500M -pterbT | zstd -e - -19 -T8 | pv -N out -B 500M -pterbT
+    exec pv -cN in -B 500M -pterbTC | zstd -e - -19 -T8 | pv -cN out -B 500M -pterbTC
   '';
 }
