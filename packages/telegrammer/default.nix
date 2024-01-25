14833727193f360802bca07f7b550daf99e726ff
Tomas Harkema <tomas@harkema.io>
2024-01-25 20:06:27 +0100

    fixes

--- packages/telegrammer/default.nix
+++ packages/telegrammer/default.nix
@@ -14,14 +14,17 @@ buildNpmPackage rec {
     hash = "sha256-3UVqJJqByt61uu3/7/ruoMx5KQK7G0YP7X1gVvu/3Lc=";
   };
 
-  npmDepsHash = ""; # "sha256-5p7q09jQYQJw7FzgPTWyS2QpjTydBo0ImbbatVaardU=";
+  npmDepsHash = "sha256-LV74W3T07dMOhx5envWt4vN5Sl2EmZHylsHqwDLFKfg="; # "sha256-5p7q09jQYQJw7FzgPTWyS2QpjTydBo0ImbbatVaardU=";
 
-  # postPatch = ''
-  #   ln -s ${./package-lock.json} package-lock.json
-  # '';
+  postPatch = ''
+    ln -s ${./package-lock.json} package-lock.json
+  '';
 
+  makeCacheWritable = true;
+
+  npmFlags = ["--legacy-peer-deps"];
   # The prepack script runs the build script, which we'd rather do in the build phase.
-  # npmPackFlags = ["--ignore-scripts"];
+  npmPackFlags = ["--ignore-scripts"];
 
   # NODE_OPTIONS = "--openssl-legacy-provider";
   # dontBuild = true;
