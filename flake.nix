6979b0c444880ee7135d4633ea1bda470269d4fc
Tomas Harkema <tomas@harkema.io>
2024-01-25 01:52:51 +0100

    ojoo

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
     nixpkgs.url = "nixpkgs/nixos-23.11";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
     darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
