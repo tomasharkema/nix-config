660f791a233e9dc5edd8fbaef82d239c6d692aa4
Tomas Harkema <tomas@harkema.io>
2024-01-26 15:17:56 +0100

    ojoo

--- modules/nixos/default/default.nix
+++ modules/nixos/default/default.nix
@@ -162,9 +162,14 @@
         *.*    @@nix.harke.ma:5140;RSYSLOG_SyslogProtocol23Format
       '';
     };
-    boot.loader.systemd-boot = {
-      netbootxyz.enable = true;
-      configurationLimit = 10;
+    boot.loader = {
+      kernelParams = [
+        "vt.default_red=30,243,166,249,137,245,148,186,88,243,166,249,137,245,148,166 vt.default_grn=30,139,227,226,180,194,226,194,91,139,227,226,180,194,226,173 vt.default_blu=46,168,161,175,250,231,213,222,112,168,161,175,250,231,213,200"
+      ];
+      systemd-boot = {
+        netbootxyz.enable = true;
+        configurationLimit = 10;
+      };
     };
   };
 }
