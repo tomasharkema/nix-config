0704585f7a9e0dcf6110aab5707d6253e4abc788
Tomas Harkema <tomas@harkema.io>
2024-01-27 17:03:42 +0100

    swith wodan back to default linux kernel

--- systems/x86_64-linux/wodan/default.nix
+++ systems/x86_64-linux/wodan/default.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   inputs,
   config,
@@ -11,10 +12,12 @@
   ];
 
   config = {
+    hardware.nvidia.forceFullCompositionPipeline = true;
     boot = {
-      binfmt.emulatedSystems = ["aarch64-linux"];
-      initrd.kernelModules = ["nvidia"];
+      # binfmt.emulatedSystems = ["aarch64-linux"];
+      # initrd.kernelModules = ["nvidia"];
       extraModulePackages = [config.boot.kernelPackages.nvidia_x11];
+      kernelPackages = pkgs.linuxPackages;
     };
 
     time = {
