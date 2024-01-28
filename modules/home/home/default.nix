aa9f64a1be9be0dcebd9ccdcdb74b8545aeed3a4
Tomas Harkema <tomas@harkema.io>
2024-01-28 02:55:10 +0100

    fix folders

--- modules/home/home/default.nix
+++ modules/home/home/default.nix
@@ -29,7 +29,9 @@ in
         };
         activation = {
           userSymlinks = ''
+            mkdir -p $HOME/.config/cachix
             ln -sfn /etc/cachix.dhall $HOME/.config/cachix/cachix.dhall
+            mkdir -p $HOME/.config/notify
             ln -sfn "${osConfig.age.secrets.notify.path}" $HOME/.config/notify/provider-config.yaml
           '';
         };
