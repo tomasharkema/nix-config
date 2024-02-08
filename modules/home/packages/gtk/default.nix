fa572e51544746e1506c92a4053de2d4f3d279cf
Tomas Harkema <tomas@harkema.io>
2024-02-08 19:43:24 +0100

    ojoo some optimalisations

--- modules/home/packages/gtk/default.nix
+++ modules/home/packages/gtk/default.nix
@@ -9,6 +9,28 @@
     #   enable = true;
     # };
 
+    home.file = let
+      autostartPrograms = with pkgs; [_1password-gui telegram-desktop keybase-gui openrgb];
+    in
+      builtins.listToAttrs (map
+        (pkg: {
+          name = ".config/autostart/" + pkg.pname + ".desktop";
+          value =
+            if pkg ? desktopItem
+            then {
+              # Application has a desktopItem entry.
+              # Assume that it was made with makeDesktopEntry, which exposes a
+              # text attribute with the contents of the .desktop file
+              text = pkg.desktopItem.text;
+            }
+            else {
+              # Application does *not* have a desktopItem entry. Try to find a
+              # matching .desktop name in /share/apaplications
+              source = pkg + "/share/applications/" + pkg.pname + ".desktop";
+            };
+        })
+        autostartPrograms);
+
     # services.keybase = {
     #   enable = true;
     #   #   # package = inputs.unstable.legacyPackages."${pkgs.system}".keybase;
--- modules/nixos/gui/gnome/default.nix
+++ modules/nixos/gui/gnome/default.nix
@@ -28,7 +28,9 @@ in {
 
       udev.packages = with pkgs; [gnome.gnome-settings-daemon];
     };
-
+    xdg.autostart = {
+      enable = true;
+    };
     environment.systemPackages = with pkgs; [
       # gnome-extension-manager
 
@@ -124,14 +126,14 @@ in {
     #   serverAddress = "0.0.0.0";
     # };
 
-    programs.hyprland = {
-      enable = true;
-      enableNvidiaPatches = true;
-    };
-    programs = {
-      sway.enable = true;
-      dconf.enable = true;
-    };
+    # programs.hyprland = {
+    #   enable = true;
+    #   enableNvidiaPatches = true;
+    # };
+    # programs = {
+    #   sway.enable = true;
+    #   dconf.enable = true;
+    # };
 
     # trace: warning: The option `fonts.fonts' defined in `/nix/store/z1gqs0dm5j9g1qy5j9m7m85al7lhjpim-aca1xyh73qrpxrv4yh6lnavs59q875xf-source/modules/nixos/gui/gnome/default.nix' has been renamed to `fonts.packages'.
     # trace: warning: The option `fonts.enableDefaultFonts' defined in `/nix/store/z1gqs0dm5j9g1qy5j9m7m85al7lhjpim-aca1xyh73qrpxrv4yh6lnavs59q875xf-source/modules/nixos/gui/gnome/default.nix' has been renamed to `fonts.enableDefaultPackages'.
