01ae4a8af7594d286ea2eaad20d46f16ebae1a3e
Tomas Harkema <tomas@harkema.io>
2024-02-12 14:48:59 +0100

    fix timeline settings

--- modules/nixos/disks/btrfs/default.nix
+++ modules/nixos/disks/btrfs/default.nix
@@ -116,30 +116,32 @@ in
         };
         snapper = {
           snapshotRootOnBoot = true;
-          # snapshotInterval = "hourly";
-          # cleanupInterval = "weekly";
+          snapshotInterval = "hourly";
+          cleanupInterval = "1d";
 
-          configs."home" = {
-            SUBVOLUME = "/home";
-            ALLOW_USERS = ["tomas" "root"];
-            TIMELINE_CREATE = true;
-            TIMELINE_CLEANUP = true;
-            TIMELINE_LIMIT_HOURLY = 4;
-            TIMELINE_LIMIT_DAILY = 6;
-            TIMELINE_LIMIT_WEEKLY = 2;
-            TIMELINE_LIMIT_MONTHLY = 2;
-            TIMELINE_LIMIT_YEARLY = 0;
-          };
-          configs."root" = {
-            SUBVOLUME = "/";
-            ALLOW_USERS = ["tomas" "root"];
-            TIMELINE_CREATE = true;
-            TIMELINE_CLEANUP = true;
-            TIMELINE_LIMIT_HOURLY = 4;
-            TIMELINE_LIMIT_DAILY = 6;
-            TIMELINE_LIMIT_WEEKLY = 2;
-            TIMELINE_LIMIT_MONTHLY = 2;
-            TIMELINE_LIMIT_YEARLY = 0;
+          configs = {
+            "home" = {
+              SUBVOLUME = "/home";
+              ALLOW_USERS = ["tomas" "root"];
+              TIMELINE_CREATE = true;
+              TIMELINE_CLEANUP = true;
+              TIMELINE_LIMIT_HOURLY = 5;
+              TIMELINE_LIMIT_DAILY = 7;
+              TIMELINE_LIMIT_WEEKLY = 2;
+              TIMELINE_LIMIT_MONTHLY = 0;
+              TIMELINE_LIMIT_YEARLY = 0;
+            };
+            "root" = {
+              SUBVOLUME = "/";
+              ALLOW_USERS = ["tomas" "root"];
+              TIMELINE_CREATE = true;
+              TIMELINE_CLEANUP = true;
+              TIMELINE_LIMIT_HOURLY = 5;
+              TIMELINE_LIMIT_DAILY = 7;
+              TIMELINE_LIMIT_WEEKLY = 2;
+              TIMELINE_LIMIT_MONTHLY = 0;
+              TIMELINE_LIMIT_YEARLY = 0;
+            };
           };
         };
         # beesd = {
