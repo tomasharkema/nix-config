c1b68ab515a9352d3bbf92ef6f36cfd974dcc26e
tomas <tomas@harkema.io>
2024-02-15 00:44:47 +0100

    add custom sssd

--- overlays/unstable/default.nix
+++ overlays/unstable/default.nix
@@ -1,6 +1,8 @@
-{channels, ...}: final: prev: {
+{channels, self,...}: final: prev: {
   runitor = channels.unstable.runitor;
 
   vscode = channels.unstable.vscode;
   android-tools = channels.unstable.android-tools;
+  freeipa = channels.unstable.freeipa;
+  sssd = self.packages."${prev.system}".sssd;
 }
