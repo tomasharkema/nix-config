7f56d3c89f9d4a495872944708bcf2a01abfe821
Tomas Harkema <tomas@harkema.io>
2024-02-14 17:47:20 +0100

    fix several things

--- modules/nixos/default/default.nix
+++ modules/nixos/default/default.nix
@@ -243,7 +243,7 @@
       enableIPv6 = false;
     };
 
-    # powerManagement.powertop.enable = true;
+    powerManagement.powertop.enable = true;
 
     security = {
       pki.certificateFiles = [./ca.crt];
