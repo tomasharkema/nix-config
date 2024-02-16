3db22c7400ea3ab3bd73ef358ededeed55a7b202
Tomas Harkema <tomas@harkema.io>
2024-02-16 13:52:20 +0100

    just add newer freeipa

--- overlays/unstable/default.nix
+++ overlays/unstable/default.nix
@@ -7,8 +7,8 @@
 
   vscode = channels.unstable.vscode;
   android-tools = channels.unstable.android-tools;
-  # freeipa = self.packages."${prev.system}".freeipa;
-  # sssd = self.packages."${prev.system}".sssd;
-  freeipa = channels.unstable.freeipa;
-  sssd = channels.unstable.sssd;
+  freeipa = self.packages."${prev.system}".freeipa;
+  sssd = self.packages."${prev.system}".sssd;
+  # freeipa = channels.unstable.freeipa;
+  # sssd = channels.unstable.sssd;
 }
