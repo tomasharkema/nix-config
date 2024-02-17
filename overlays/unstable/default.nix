6ca08460de5561a2f723f866d43f97450edfaf24
Tomas Harkema <tomas@harkema.io>
2024-02-17 02:00:55 +0100

    fixes

--- overlays/unstable/default.nix
+++ overlays/unstable/default.nix
@@ -7,6 +7,7 @@
 
   vscode = channels.unstable.vscode;
   android-tools = channels.unstable.android-tools;
+
   # freeipa = self.packages."${prev.system}".freeipa;
   # sssd = self.packages."${prev.system}".sssd;
   freeipa = channels.unstable.freeipa;
