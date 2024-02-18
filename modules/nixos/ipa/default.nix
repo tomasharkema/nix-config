46dcee7e97b6ca43a458a4088a8ae855159f0891
Tomas Harkema <tomas@harkema.io>
2024-02-18 21:32:58 +0100

    add ipa to different server

---
+++ modules/nixos/ipa/default.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  config = {
+    system.activationScripts = {
+      default_ccache_name = ''
+        if [ ! -d "/var/cache/krb5" ]; then
+          mkdir /var/cache/krb5
+          chmod 777 /var/cache/krb5
+        fi
+      '';
+    };
+
+    environment.etc."krb5.conf".text = ''
+      [libdefaults]
+      default_ccache_name = FILE:/var/cache/krb5/krb5cc_%{uid}
+    '';
+
+    services.sssd = {
+      enable = true;
+      kcm = true;
+      # [sssd]
+      # krb5_rcache_dir = /var/cache/krb5
+
+      config = ''
+        [pam]
+        pam_passkey_auth = True
+      '';
+    };
+    networking.extraHosts = ''
+      100.96.240.97 ipa.harkema.intra
+    '';
+    security = {
+      pki.certificateFiles = [./ca.crt];
+      ipa = {
+        enable = true;
+        server = "ipa.harkema.intra";
+        domain = "harkema.intra";
+        realm = "HARKEMA.INTRA";
+        basedn = "dc=harkema,dc=intra";
+        certificate = pkgs.fetchurl {
+          url = "https://ipa.harkema.intra/ipa/config/ca.crt";
+          sha256 = "1479i13wzznz7986sqlpmx6r108d24kbn84yp5n3s50q7wpgdfxz";
+        };
+        dyndns.enable = true;
+        ifpAllowedUids = ["root" "tomas"];
+      };
+    };
+  };
+}
