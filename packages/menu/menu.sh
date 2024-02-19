f624208801ed705e4df000e4b5c1992f87a0997b
Tomas Harkema <tomas@harkema.io>
2024-02-19 15:42:06 +0100

    fix menu

--- packages/menu/menu.sh
+++ packages/menu/menu.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 RUN_UPDATER="Run Updater..."
 ATTACH_SESSION="Attach to session..."
@@ -9,63 +9,64 @@ OPEN_SHELL="Go to shell!"
 CLEAN_RAM="Clean ram..."
 EXIT="Exit"
 
-update () {
-    exec sudo nixos-rebuild switch --flake "github:tomasharkema/nix-config" --refresh --verbose --log-format internal-json -v |& nom --json
+update() {
+    sudo nixos-rebuild switch --flake "github:tomasharkema/nix-config" --refresh --verbose --log-format internal-json -v 2>&1 | nom --json
 }
 
-clear-cache () {
+clearcache() {
     sudo nix-collect-garbage --delete-older-than '1d'
     nix-collect-garbage --delete-older-than '1d'
     echo "-- nix store optimise --"
     nix store optimise
 }
 
-attach-session () {
+attachsession() {
     SESSION=$(tmux list-sessions -F \#S | gum filter --placeholder "Pick session...")
     tmux switch-client -t "$SESSION" || tmux attach -t "$SESSION"
 }
 
-clean-ram () {
-    sudo sync; echo 3 > /proc/sys/vm/drop_caches
+cleanram() {
+    sudo sync
+    echo 3 >/proc/sys/vm/drop_caches
 }
 
 CHOICE=$(gum choose "$RUN_UPDATER" "$ATTACH_SESSION" "$OPEN_BLUE_FIRE" "$OPEN_SSH" "$CLEAR_CACHE" "$OPEN_SHELL" "$CLEAN_RAM" "$EXIT")
 
 case $CHOICE in
-    "$RUN_UPDATER")
+"$RUN_UPDATER")
     update
     ;;
 
-    "$OPEN_BLUE_FIRE")
+"$OPEN_BLUE_FIRE")
     exec ssh blue-fire-menu
     ;;
 
-    "$OPEN_SSH")
+"$OPEN_SSH")
     exec fast-ssh
     ;;
 
-    "$OPEN_SHELL")
+"$OPEN_SHELL")
     exec zsh
     ;;
 
-    "$CLEAR_CACHE")
-    clear-cache
+"$CLEAR_CACHE")
+    clearcache
     ;;
 
-    "$ATTACH_SESSION")
-    attach-session
+"$ATTACH_SESSION")
+    attachsession
     ;;
 
-    "$CLEAN_RAM")
-    clean-ram
+"$CLEAN_RAM")
+    cleanram
     ;;
 
-    "$EXIT")
+"$EXIT")
     echo "Exitting..."
     exit 0
     ;;
 
-    *)
+*)
     exit 1
     ;;
 esac
