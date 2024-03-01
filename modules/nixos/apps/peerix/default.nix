ef574e9f3cbb2994ec70a8b26c770df981ac3181
tomas <tomas@harkema.io>
2024-03-01 03:13:02 +0100

    add key

--- modules/nixos/apps/peerix/default.nix
+++ modules/nixos/apps/peerix/default.nix
@@ -4,10 +4,13 @@
   ...
 }: {
   config = {
+    # nix-store --generate-binary-cache-key tomas-peerix /home/tomas/tomas-peerix-secret.key /home/tomas/tomas-peerix-public.key
     services.peerix = {
       enable = true;
       user = "tomas";
       group = "tomas";
+      publicKeyFile = "/home/tomas/tomas-peerix-public.key";
+      privateKeyFile = "/home/tomas/tomas-peerix-secret.key";
     };
   };
 }
