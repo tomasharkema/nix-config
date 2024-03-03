b639bf75d3bcf128a01e8f4758c73ca3052b2bf5
Tomas Harkema <tomas@harkema.io>
2024-03-03 21:00:26 +0100

    Enzian (#9)
    * enzian
    
    * add snapshots
    
    * more fixes
    
    * readd ipa
    
    * fixes
    
    * ojoo
    
    * more fixes
    
    * more fixes
    
    * ojo
    
    * fix
    
    * hmm
    
    * ojoo
    
    * fixes
    
    * meg
    
    * ojoo
    
    * fixes
    
    * hmmm
    
    * meh
    
    * reencrypt
    
    * hmm
    
    * fixes
    
    * fix
    
    * ojoo
    
    * reencrypt
    
    * netbox
    
    * ojo
    
    * fixes
    
    * hmm
    
    * updates
    
    * updates
    
    * add tailsacle tui
    
    * add tui
    
    * ojoo
    
    * hmm
    
    * updates
    
    * hmm
    
    * add agent
    
    * fixes
    
    * fixes
    
    * more fixes
    
    * fix vm

---
+++ packages/netbrowse/default.nix
@@ -0,0 +1,34 @@
+{
+  rustPlatform,
+  fetchCrate,
+  xorg,
+  libGL,
+  makeWrapper,
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "netbrowse";
+  version = "0.1.0";
+
+  src = fetchCrate {
+    inherit pname version;
+    hash = "sha256-dA8oDeQSPDPXPAgX6jPLLPWXxje+1L80zC8SVaQ2WTs=";
+  };
+
+  cargoSha256 = "sha256-FzKDKm3TJNelDRUL3BJp8UJnjrCb++g1osXOT6X55ho=";
+
+  buildInputs = [
+    xorg.libxcb
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    libGL
+    makeWrapper
+  ];
+
+  postInstall = ''
+    mkdir -p $out/share/applications
+    cp ${./netbrowse.desktop} $out/share/applications/netbrowse.desktop
+    wrapProgram $out/bin/netbrowse --prefix LD_LIBRARY_PATH : "${xorg.libX11.out}/lib:${xorg.libXcursor.out}/lib:${xorg.libXrandr.out}/lib:${xorg.libXi.out}/lib:${libGL}/lib"
+  '';
+}
