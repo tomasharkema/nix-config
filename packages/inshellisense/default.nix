a71f1aa703d1ad6d54c2cfb33547c13ff74f5ba1
Tomas Harkema <tomas@harkema.io>
2024-03-05 02:22:38 +0100

    tryout inshellisense

---
+++ packages/inshellisense/default.nix
@@ -0,0 +1,16 @@
+{
+  buildNpmPackage,
+  fetchFromGitHub,
+}:
+buildNpmPackage rec {
+  pname = "inshellisense";
+  version = "0.0.1-rc.7";
+
+  src = fetchFromGitHub {
+    owner = "microsoft";
+    repo = pname;
+    rev = "${version}";
+    hash = "sha256-cWSynWQaCK2Ru+s+MpKbd3SX3gWaEWgOzIACy2yHk9c=";
+  };
+  npmDepsHash = "sha256-sGGBKyyoLdK49ml1VACi8h9OMgLSE20w34n2B08OGLk=";
+}
