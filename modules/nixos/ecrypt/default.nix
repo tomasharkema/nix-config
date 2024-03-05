be3bc61748fe8e8240f12e608b6c9a204f18fc94
Tomas Harkema <tomas@harkema.io>
2024-03-05 16:12:44 +0100

    remove ecryptfs

--- modules/nixos/ecrypt/default.nix
+++ modules/nixos/ecrypt/default.nix
@@ -4,7 +4,7 @@
   config,
   ...
 }: {
-  config = {
+  config = lib.mkIf false {
     security.pam.enableEcryptfs = true;
     boot.kernelModules = ["ecryptfs"];
     environment.systemPackages = with pkgs; [ecryptfs];
