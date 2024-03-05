25d851e37eecc6416fcb9c7c43fec7bac2556cfa
Tomas Harkema <tomas@harkema.io>
2024-03-05 01:42:03 +0100

    add gpustat

---
+++ packages/gpustat/default.nix
@@ -0,0 +1,24 @@
+# https://github.com/wookayin/gpustat
+{python3Packages}:
+with python3Packages;
+  buildPythonPackage rec {
+    pname = "gpustat";
+    version = "1.1.1";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-wY0+1VGPwWMAxC1pTevHCuuzvlXK6R8dtk1jtfqK+dg="; # TODO
+    };
+
+    doCheck = false;
+
+    propagatedBuildInputs = [
+      # ...
+      setuptools
+      setuptools_scm
+      pytest
+      psutil
+      blessed
+      pynvml
+    ];
+  }
