f3fc99b1c210f6df7c71314dc9b754ec20d132cc
Tomas Harkema <tomas@harkema.io>
2024-03-07 00:39:00 +0100

    ojoo

--- modules/nixos/apps/monit/default.nix
+++ modules/nixos/apps/monit/default.nix
@@ -1,8 +1,17 @@
 {
   config,
   pkgs,
+  lib,
   ...
-}: {
+}:
+with lib; {
+  options.apps.monit = {
+    port = mkOption {
+      type = types.str;
+      default = "2812";
+    };
+  };
+
   config = {
     services.monit = {
       enable = true;
@@ -10,11 +19,17 @@
         set daemon  30
         set logfile syslog
 
-        set httpd port 2812 and
+        set httpd port ${config.apps.monit.port} and
           use address localhost
           allow localhost
           allow admin:monit
       '';
+    };
+
+    proxy-services.services = {
+      "/monit/" = {
+        proxyPass = "http://localhost:${config.apps.monit.port}/";
+      };
     };
   };
 }
