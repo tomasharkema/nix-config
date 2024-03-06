6e48a51ce3326a34ec0c0cf8e75f88a5eadb492c
Tomas Harkema <tomas@harkema.io>
2024-03-06 19:21:15 +0100

    add monit

--- modules/nixos/apps/monit/default.nix
+++ modules/nixos/apps/monit/default.nix
@@ -6,6 +6,15 @@
   config = {
     services.monit = {
       enable = true;
+      config = ''
+        set daemon  30
+        set logfile syslog
+
+        set httpd port 2812 and
+          use address localhost
+          allow localhost
+          allow admin:monit
+      '';
     };
   };
 }
