da3babdc6a22816f6471262fd585e97cb3113e1f
Tomas Harkema <tomas@harkema.io>
2024-03-09 00:06:41 +0100

    fixes

---
+++ modules/nixos/services/mail/sendtelegram/default.nix
@@ -0,0 +1,40 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  config = {
+    services.postfix = {
+      enable = true;
+
+      # config.public-inbox_destination_recipient_limit = lib.mkForce null;
+
+      extraAliases = ''
+        tomas@harkema.intra: tomas
+        tomas@harkema.io: tomas
+      '';
+
+      transport = ''
+        tomas sendtelegram:tomas
+      '';
+
+      # The public-inbox transport
+      masterConfig.sendtelegram = let
+        sendtelegram-script = pkgs.writeShellScript "postix-sendtelegram" ''
+          set -x
+          SENDTELEGRAM_API_TOKEN="$(${pkgs.coreutils}/bin/cat /run/agenix/notify | ${pkgs.yq}/bin/yq '.telegram[0].telegram_api_key' -r)"
+          SENDTELEGRAM_CHAT_ID="$(${pkgs.coreutils}/bin/cat /run/agenix/notify | ${pkgs.yq}/bin/yq '.telegram[0].telegram_chat_id' -r)"
+          ${pkgs.custom.sendtelegram}/bin/sendtelegram
+        '';
+      in {
+        type = "unix";
+        privileged = true;
+        command = "pipe";
+        args = [
+          "user=tomas:tomas"
+          "argv=${sendtelegram-script}"
+        ];
+      };
+    };
+  };
+}
--- systems/x86_64-linux/euro-mir-2/default.nix
+++ systems/x86_64-linux/euro-mir-2/default.nix
@@ -68,15 +68,38 @@ with lib; {
         reflector = mkForce false;
       };
 
-      fprintd = {
-        enable = true;
-        package = pkgs.fprintd-tod;
-        tod = {
-          enable = true;
-          driver = pkgs.custom.libfprint-2-tod1-goodix; #pkgs.libfprint-2-tod1-goodix;
-        };
-      };
+      # fprintd = {
+      #   enable = true;
+      #   package = pkgs.fprintd-tod;
+      #   tod = {
+      #     enable = true;
+      #     driver = pkgs.custom.libfprint-2-tod1-goodix; #pkgs.libfprint-2-tod1-goodix;
+      #   };
+      # };
     };
+
+    # services.fprintd = let
+    #   libfprint-tod = pkgs.libfprint.overrideAttrs (_: rec {
+    #     pname = "libfprint-tod";
+    #     version = "1.94.7+tod1";
+    #     src = pkgs.fetchFromGitLab {
+    #       domain = "gitlab.freedesktop.org";
+    #       owner = "3v1n0";
+    #       repo = "libfprint";
+    #       rev = "v${version}";
+    #       sha256 = "sha256-q6m/J5GH86/z/mKnrYoanhKWR7+reKIRHqhDOUkknFA=";
+    #     };
+    #     doCheck = false;
+    #   });
+    # in {
+    #   enable = true;
+    #   package = pkgs.fprintd.override {libfprint = libfprint-tod;};
+    #   tod = {
+    #     enable = true;
+    #     driver = pkgs.libfprint-2-tod1-goodix.override {libfprint-tod = libfprint-tod;};
+    #   };
+    # };
+    # systemd.services."fprintd".environment."G_MESSAGES_DEBUG" = "all"; # for good measure
 
     boot = {
       binfmt.emulatedSystems = ["aarch64-linux"];
