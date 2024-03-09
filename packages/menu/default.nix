8cf9284559bfe3bdf45eb8d193fa0b83a869ce37
tomas <tomas@harkema.io>
2024-03-09 23:01:07 +0100

    fixes

--- packages/menu/default.nix
+++ packages/menu/default.nix
@@ -6,7 +6,18 @@
 pkgs.writeShellApplication {
   name = "menu";
 
-  runtimeInputs = with pkgs; [gum nixos-rebuild nix-output-monitor zsh fast-ssh dialog manix];
+  runtimeInputs = with pkgs; [
+    gum
+    nixos-rebuild
+    nix-output-monitor
+    zsh
+    fast-ssh
+    dialog
+    manix
+    nix-search-cli
+
+    nix-du
+  ];
 
   text = builtins.readFile ./menu.sh;
 }
