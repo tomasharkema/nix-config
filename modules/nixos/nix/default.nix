d640888e1a0c0c37760c43be0de03e69d830034b
tomas <tomas@harkema.io>
2024-03-11 14:53:45 +0100

    fix packages

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -43,62 +43,59 @@ in {
       # (nixos-hosts.override {
       #   hosts = inputs.self.nixosConfigurations;
       # })
-      # inputs.deploy-rs.packages.${system}.deploy-rs
-      # nixfmt
-      # nix-prefetch-git
-      # nix-output-monitor
+      inputs.deploy-rs.packages."${system}".deploy-rs
+      nixfmt
+      nix-prefetch-git
+      nix-output-monitor
       # flake-checker
+      inputs.flake-checker.packages."${system}".default
     ];
 
-    nix = {
-      settings = {
-        use-cgroups = true;
-        experimental-features = "nix-command flakes cgroups";
+    nix = let
+      users =
+        ["root" config.user.name]
+        ++ optional config.services.hydra.enable "hydra";
+    in {
+      # package = cfg.package;
+
+      settings =
+        {
+          # permittedInsecurePackages = [
+          #   "nix-2.17.1"
+          # ];
+          use-cgroups = true;
+          experimental-features = "nix-command flakes cgroups";
+          http-connections = 50;
+          warn-dirty = false;
+          log-lines = 50;
+          sandbox = false;
+          auto-optimise-store = true;
+          trusted-users = users ++ ["tomas" "root"]; # "builder"];
+          allowed-users = users ++ ["tomas" "root" "builder"];
+          netrc-file = "/etc/nix/netrc";
+          substituters =
+            [cfg.default-substituter.url]
+            ++ (mapAttrsToList (name: value: name) cfg.extra-substituters);
+          trusted-public-keys =
+            [cfg.default-substituter.key]
+            ++ (mapAttrsToList (name: value: value.key) cfg.extra-substituters);
+        }
+        // (lib.optionalAttrs true {
+          # config.custom.tools.direnv.enable {
+          keep-outputs = true;
+          keep-derivations = true;
+        });
+
+      gc = {
+        automatic = true;
+        dates = "daily";
+        options = "--delete-older-than 7d";
       };
+
+      # flake-utils-plus
+      generateRegistryFromInputs = true;
+      generateNixPathFromInputs = true;
+      linkInputs = true;
     };
-
-    # nix = let
-    #   users =
-    #     ["root" config.user.name]
-    #     ++ optional config.services.hydra.enable "hydra";
-    # in {
-    #   # package = cfg.package;
-
-    #   settings =
-    #     {
-    #       use-cgroups = true;
-    #       experimental-features = "nix-command flakes cgroups";
-    #       http-connections = 50;
-    #       warn-dirty = false;
-    #       log-lines = 50;
-    #       sandbox = false;
-    #       auto-optimise-store = true;
-    #       trusted-users = users ++ ["tomas" "root"]; # "builder"];
-    #       allowed-users = users ++ ["tomas" "root" "builder"];
-    #       netrc-file = "/etc/nix/netrc";
-    #       substituters =
-    #         [cfg.default-substituter.url]
-    #         ++ (mapAttrsToList (name: value: name) cfg.extra-substituters);
-    #       trusted-public-keys =
-    #         [cfg.default-substituter.key]
-    #         ++ (mapAttrsToList (name: value: value.key) cfg.extra-substituters);
-    #     }
-    #     // (lib.optionalAttrs true {
-    #       # config.custom.tools.direnv.enable {
-    #       keep-outputs = true;
-    #       keep-derivations = true;
-    #     });
-
-    #   gc = {
-    #     automatic = true;
-    #     dates = "daily";
-    #     options = "--delete-older-than 7d";
-    #   };
-
-    #   # flake-utils-plus
-    #   generateRegistryFromInputs = true;
-    #   generateNixPathFromInputs = true;
-    #   linkInputs = true;
-    # };
   };
 }
