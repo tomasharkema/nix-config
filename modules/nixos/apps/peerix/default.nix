ea895842884e42ee9366e2b88641d0deb9b9406a
Tomas Harkema <tomas@harkema.io>
2024-03-12 22:17:23 +0100

    fix peerix

--- modules/nixos/apps/peerix/default.nix
+++ modules/nixos/apps/peerix/default.nix
@@ -5,25 +5,25 @@
   ...
 }: {
   config = {
-    age.secrets = {
-      "peerix-public" = {
-        file = ../../../../secrets/peerix.public.age;
-        mode = "644";
-      };
+    # age.secrets = {
+    #   "peerix-public" = {
+    #     file = ../../../../secrets/peerix.public.age;
+    #     mode = "644";
+    #   };
 
-      "peerix-private" = {
-        file = ../../../../secrets/peerix.private.age;
-        mode = "644";
-      };
-    };
+    #   "peerix-private" = {
+    #     file = ../../../../secrets/peerix.private.age;
+    #     mode = "644";
+    #   };
+    # };
 
-    services.peerix = {
-      enable = true;
-      user = "tomas";
-      group = "tomas";
-      publicKey = "peerix-tomas-1:OBFTUNI1LIezxoFStcRyCHKi2PHExoIcZA0Mfq/4uJA=";
-      privateKeyFile = config.age.secrets."peerix-private".path;
-      openFirewall = true;
-    };
+    # services.peerix = {
+    #   enable = true;
+    #   user = "tomas";
+    #   group = "tomas";
+    #   publicKey = "peerix-tomas-1:OBFTUNI1LIezxoFStcRyCHKi2PHExoIcZA0Mfq/4uJA=";
+    #   privateKeyFile = config.age.secrets."peerix-private".path;
+    #   openFirewall = true;
+    # };
   };
 }
