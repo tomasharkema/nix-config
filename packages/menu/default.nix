4966ecf2961c2696abad4b88081bc6acc74c5fca
Tomas Harkema <tomas@harkema.io>
2024-03-12 19:05:23 +0100

    fix nix-du

--- packages/menu/default.nix
+++ packages/menu/default.nix
@@ -6,18 +6,18 @@
 pkgs.writeShellApplication {
   name = "menu";
 
-  runtimeInputs = with pkgs; [
-    gum
-    nixos-rebuild
-    nix-output-monitor
-    zsh
-    fast-ssh
-    dialog
-    manix
-    nix-search-cli
-
-    nix-du
-  ];
+  runtimeInputs = with pkgs;
+    [
+      gum
+      nixos-rebuild
+      nix-output-monitor
+      zsh
+      fast-ssh
+      dialog
+      manix
+      nix-search-cli
+    ]
+    ++ (lib.optional pkgs.stdenv.isLinux nix-du);
 
   text = builtins.readFile ./menu.sh;
 }
