6904f6cc6ac0c33389269ffc814a7bdd4f1e0bd9
Tomas Harkema <tomas@harkema.io>
2024-03-14 16:41:31 +0100

    fix raspberryshit

--- systems/aarch64-linux/baaa-express/default.nix
+++ systems/aarch64-linux/baaa-express/default.nix
@@ -3,7 +3,8 @@
   inputs,
   lib,
   ...
-}: {
+}:
+with lib; {
   config = {
     networking.hostName = "baaa-express";
 
@@ -13,27 +14,36 @@
       libraspberrypi
       raspberrypi-eeprom
     ];
-    # system.stateVersion = "23.11";
 
-    # boot.kernelParams = [
-    #   "console=ttyS1,115200n8"
-    #   "cma=320M"
-    # ];
+    services = {
+      netdata.enable = mkForce false;
+    };
+    # system.stateVersion = "23.11";
 
     # fileSystems."/".fsType = lib.mkForce "tmpfs";
     # fileSystems."/".device = lib.mkForce "none";
 
-    boot.initrd.kernelModules = ["vc4" "bcm2835_dma" "i2c_bcm2835"];
+    boot = {
+      initrd.availableKernelModules = ["vc4" "bcm2835_dma" "i2c_bcm2835"];
+      initrd.kernelModules = ["dwc2" "g_serial"];
 
-    boot.loader.raspberryPi = {
-      enable = true;
-      version = 3;
-      #   firmwareConfig = ''
-      #     core_freq=250
-      #   '';
-      uboot.enable = true;
+      kernelPackages = pkgs.linuxPackages_rpi4;
+
+      kernelParams = [
+        "console=ttyS1,115200n8"
+        "cma=320M"
+      ];
+
+      loader.raspberryPi = {
+        enable = true;
+        version = 3;
+        #     core_freq=250
+        firmwareConfig = ''
+          dtoverlay=dwc2
+        '';
+        # uboot.enable = true;
+      };
     };
-
     hardware = {
       i2c.enable = true;
 
