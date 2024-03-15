e7a7945573b1faa66aa43e3e0f6ac6ceee43b91f
Tomas Harkema <tomas@harkema.io>
2024-03-15 14:16:44 +0100

    fixes

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -125,6 +125,8 @@ in {
         "https://nix-cache.harke.ma/tomas"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org/"
+
+        "http://blue-fire.harkema.intra:5000"
       ];
 
       trusted-public-keys = [
@@ -132,6 +134,7 @@ in {
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "tomasharkema.cachix.org-1:LOeGvH7jlA3vZmW9+gHyw0BDd1C8a0xrQSl9WHHTRuA="
+        "peerix-tomas-1:OBFTUNI1LIezxoFStcRyCHKi2PHExoIcZA0Mfq/4uJA="
       ];
     };
   };
