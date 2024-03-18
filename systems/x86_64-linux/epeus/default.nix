bef09f5bf8129f0848504aecf75e9a5d4e932655
Tomas Harkema <tomas@harkema.io>
2024-03-18 13:58:31 +0100

    fixes

--- systems/x86_64-linux/epeus/default.nix
+++ systems/x86_64-linux/epeus/default.nix
@@ -32,11 +32,6 @@
 
     # Select internationalisation properties.
     i18n.defaultLocale = "en_US.UTF-8";
-    console = {
-      font = "Lat2-Terminus16";
-      #    keyMap = "us";
-      useXkbConfig = true; # use xkb.options in tty.
-    };
 
     # Enable the X11 windowing system.
     # services.xserver.enable = true;
