2708aea457452bcd34f1a91c8c3c10b801e5141d
tomas <tomas@harkema.io>
2024-03-20 04:05:03 +0100

    fix all the serve shizzle

--- modules/nixos/apps/attic/server/default.nix
+++ modules/nixos/apps/attic/server/default.nix
@@ -12,9 +12,39 @@ in {
   };
 
   config = mkIf cfg.enable {
-    # services.atticd = {
-    #   enable = true;
-    #   credentialsFile = config.age.secrets."peerix-private".path;
-    # };
+    proxy-services.services = {
+      "/attic" = {
+        proxyPass = "http://localhost:8080/";
+        extraConfig = ''
+          rewrite /attic(.*) $1 break;
+        '';
+      };
+    };
+
+    services.atticd = {
+      enable = true;
+
+      credentialsFile = "/etc/atticd.env";
+
+      settings = {
+        listen = "0.0.0.0:8080";
+        chunking = {
+          # The minimum NAR size to trigger chunking
+          #
+          # If 0, chunking is disabled entirely for newly-uploaded NARs.
+          # If 1, all NARs are chunked.
+          nar-size-threshold = 64 * 1024; # 64 KiB
+
+          # The preferred minimum size of a chunk, in bytes
+          min-size = 16 * 1024; # 16 KiB
+
+          # The preferred average size of a chunk, in bytes
+          avg-size = 64 * 1024; # 64 KiB
+
+          # The preferred maximum size of a chunk, in bytes
+          max-size = 256 * 1024; # 256 KiB
+        };
+      };
+    };
   };
 }
