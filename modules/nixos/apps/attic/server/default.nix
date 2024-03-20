1012e3671fe37717283f3d74444bfd5c58d165be
Tomas Harkema <tomas@harkema.io>
2024-03-20 15:33:07 +0100

    fix attic things

--- modules/nixos/apps/attic/server/default.nix
+++ modules/nixos/apps/attic/server/default.nix
@@ -26,8 +26,41 @@ in {
 
       credentialsFile = "/etc/atticd.env";
 
+      storage = {
+        # Storage type
+        #
+        # Can be "local" or "s3".
+        type = "s3";
+
+        # ## Local storage
+
+        # The directory to store all files under
+        # path = "/data"
+
+        # ## S3 Storage (set type to "s3" and uncomment below)
+
+        # The AWS region
+        #region = "us-east-1"
+
+        # The name of the bucket
+        bucket = "attic";
+
+        # Custom S3 endpoint
+        #
+        # Set this if you are using an S3-compatible object storage (e.g., Minio).
+        endpoint = "https://cce1d3af687f672684f23cf7aa7731f7.r2.cloudflarestorage.com/";
+      };
+
       settings = {
         listen = "0.0.0.0:8080";
+        api-endpoint = "https://blue-fire.ling-lizard.ts.net/attic/";
+        compression = {
+          # Can be "none", "brotli", "zstd", or "xz"
+          type = "zstd";
+
+          # Compression level
+          level = 8;
+        };
         chunking = {
           # The minimum NAR size to trigger chunking
           #
@@ -43,6 +76,22 @@ in {
 
           # The preferred maximum size of a chunk, in bytes
           max-size = 256 * 1024; # 256 KiB
+        };
+        garbage-collecting = {
+          # The frequency to run garbage collection at
+          #
+          # By default it's 12 hours. You can use natural language
+          # to specify the interval, like "1 day".
+          #
+          # If zero, automatic garbage collection is disabled, but
+          # it can still be run manually with `atticd --mode garbage-collector-once`.
+          interval = "1 day";
+
+          # Default retention period
+          #
+          # Zero (default) means time-based garbage-collection is
+          # disabled by default. You can enable it on a per-cache basis.
+          default-retention-period = "14 days";
         };
       };
     };
