a8cf83223539ac7c6e7797f827c2c15e48d30ad9
tomas <tomas@harkema.io>
2024-03-20 04:05:03 +0100

    fix all the serve shizzle

--- config.nix
+++ config.nix
@@ -7,17 +7,15 @@
     trusted-users = ["root" "tomas"];
 
     substituters = [
-      "https://nix-cache.harke.ma/tomas"
       "https://nix-community.cachix.org"
       "https://cache.nixos.org/"
-      "http://blue-fire.harkema.intra:5000"
+      "http://blue-fire.harkema.intra:8080"
     ];
 
     trusted-public-keys = [
-      "tomas:/cvjdgRjoTx9xPqCkeMWkf9csRSAmnqLgN3Oqkpx2Tg="
+      "tomas:eOMnjVopwTC6220eUnejgPyFkTUEyVj2EB8eWdJIR4s="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "peerix-tomas-1:OBFTUNI1LIezxoFStcRyCHKi2PHExoIcZA0Mfq/4uJA="
     ];
   };
 }
