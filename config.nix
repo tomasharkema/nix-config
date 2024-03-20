0cb84a483a35fd2304aa2b094c3fc82e89ffe502
tomas <tomas@harkema.io>
2024-03-20 20:19:35 +0100

    add public key

--- config.nix
+++ config.nix
@@ -16,6 +16,7 @@
       "tomas:eOMnjVopwTC6220eUnejgPyFkTUEyVj2EB8eWdJIR4s="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "tomas:qzaaV24nfgwcarekICaYr2c9ZBFDQnvvydOywbwAeys="
     ];
   };
 }
