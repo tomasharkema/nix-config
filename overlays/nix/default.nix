5496d615f85c03362535b80754fa00e7ebe1a571
tomas <tomas@harkema.io>
2024-03-21 21:55:29 +0100

    fix

---
+++ overlays/nix/default.nix
@@ -0,0 +1,18 @@
+{
+  nil,
+  devenv,
+  deploy-rs,
+  attic,
+  agenix,
+  hydra-check,
+  flake-checker,
+  ...
+}: final: prev: {
+  nil = nil.packages.${prev.system}.default;
+  devenv = devenv.packages.${prev.system}.default;
+  deploy-rs = deploy-rs.packages.${prev.system}.default;
+  attic = attic.packages.${prev.system}.default;
+  agenix = agenix.packages.${prev.system}.default;
+  hydra-check = hydra-check.packages.${prev.system}.default;
+  flake-checker = flake-checker.packages."${prev.system}".default;
+}
