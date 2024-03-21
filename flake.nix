c5fd69e7427b09ef7027fd3b3168f3e63d2de240
tomas <tomas@harkema.io>
2024-03-21 10:58:18 +0100

    add devenv cache

--- flake.nix
+++ flake.nix
@@ -380,6 +380,7 @@
       "https://tomasharkema.cachix.org"
       "https://cache.nixos.org/"
       "https://blue-fire.ling-lizard.ts.net/attic/tomas/"
+      "https://devenv.cachix.org"
     ];
 
     trusted-public-keys = [
@@ -390,6 +391,7 @@
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "peerix-tomas-1:OBFTUNI1LIezxoFStcRyCHKi2PHExoIcZA0Mfq/4uJA="
       "tomas:qzaaV24nfgwcarekICaYr2c9ZBFDQnvvydOywbwAeys="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     ];
 
     # extra-trusted-public-keys = [
