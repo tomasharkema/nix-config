5e00168145e388ed87dedb1c524bbb18a45551ec
Tomas Harkema <tomas@harkema.io>
2024-03-21 22:35:14 +0100

    derp

--- overlays/nix/default.nix
+++ overlays/nix/default.nix
@@ -9,12 +9,12 @@
   channels,
   ...
 }: final: prev: {
-  nil = nil.packages.${prev.system}.default;
-  devenv = devenv.packages.${prev.system}.default;
-  deploy-rs = deploy-rs.packages.${prev.system}.default;
-  attic = attic.packages.${prev.system}.default;
-  agenix = agenix.packages.${prev.system}.default;
-  hydra-check = hydra-check.packages.${prev.system}.default;
+  nil = nil.packages."${prev.system}".default;
+  devenv = devenv.packages."${prev.system}".default;
+  deploy-rs = deploy-rs.packages."${prev.system}".default;
+  attic = attic.packages."${prev.system}".default;
+  agenix = agenix.packages."${prev.system}".default;
+  hydra-check = hydra-check.packages."${prev.system}".default;
   flake-checker = flake-checker.packages."${prev.system}".default;
-  nixUnstable = channels.unstable.nixVersions.nix_2_21;
+  nixUnstable = channels.unstable.nixVersions.nix_2_20;
 }
