18d6bf7687ed68075c3a3b2fa0f2badf68ea8e64
Tomas Harkema <tomas@harkema.io>
2024-03-22 15:33:40 +0100

    add motd things

--- systems/x86_64-linux/enzian/samba.nix
+++ systems/x86_64-linux/enzian/samba.nix
@@ -17,7 +17,7 @@
         #use sendfile = yes
         #max protocol = smb2
         # note: localhost is the ipv6 localhost ::1
-        hosts allow = 192.168.178. 127.0.0.1 localhost
+        hosts allow = 192.168.178. 172. 100. 127.0.0.1 localhost
         hosts deny = 0.0.0.0/0
         guest account = nobody
         map to guest = bad user
