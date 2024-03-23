e59d1744a047a00744112234e937ed39e535ad30
Tomas Harkema <tomas@harkema.io>
2024-03-23 21:35:32 +0100

    hmm

--- modules/nixos/gui/game-mode/default.nix
+++ modules/nixos/gui/game-mode/default.nix
@@ -15,7 +15,16 @@ in {
       enable = true;
 
       enableRenice = true;
-
+      settings = {
+        general = {
+          softrealtime = "auto";
+          renice = 10;
+        };
+        custom = {
+          start = "notify-send -a 'Gamemode' 'Optimizations activated'";
+          end = "notify-send -a 'Gamemode' 'Optimizations deactivated'";
+        };
+      };
       # settings = {
       #   general = {
       #     softrealtime = "on";
