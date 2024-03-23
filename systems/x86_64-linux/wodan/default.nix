b842c1ce7979d36bf48cca2a6930d19ddb42761a
Tomas Harkema <tomas@harkema.io>
2024-03-24 00:55:27 +0100

    hmmmz

--- systems/x86_64-linux/wodan/default.nix
+++ systems/x86_64-linux/wodan/default.nix
@@ -95,22 +95,22 @@
       #   device = "192.168.0.11:/exports/media";
       #   fsType = "nfs";
       # };
-      "/mnt/unraid/appdata" = {
-        device = "192.168.0.100:/mnt/user/appdata";
-        fsType = "nfs";
-      };
-      "/mnt/unraid/appdata_ssd" = {
-        device = "192.168.0.100:/mnt/user/appdata_ssd";
-        fsType = "nfs";
-      };
-      "/mnt/unraid/appdata_disk" = {
-        device = "192.168.0.100:/mnt/user/appdata_disk";
-        fsType = "nfs";
-      };
-      "/mnt/unraid/data" = {
-        device = "192.168.0.100:/mnt/user/data";
-        fsType = "nfs";
-      };
+      # "/mnt/unraid/appdata" = {
+      #   device = "192.168.0.100:/mnt/user/appdata";
+      #   fsType = "nfs";
+      # };
+      # "/mnt/unraid/appdata_ssd" = {
+      #   device = "192.168.0.100:/mnt/user/appdata_ssd";
+      #   fsType = "nfs";
+      # };
+      # "/mnt/unraid/appdata_disk" = {
+      #   device = "192.168.0.100:/mnt/user/appdata_disk";
+      #   fsType = "nfs";
+      # };
+      # "/mnt/unraid/data" = {
+      #   device = "192.168.0.100:/mnt/user/data";
+      #   fsType = "nfs";
+      # };
       #   "/mnt/dione" = {
       #     device = "192.168.178.3:/volume1/homes";
       #     fsType = "nfs";
