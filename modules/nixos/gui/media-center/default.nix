6eb9688f67a30b4619c039e17d40be1f4fc9b6e1
Tomas Harkema <tomas@harkema.io>
2024-03-23 01:44:07 +0100

    add meda to rasp

--- modules/nixos/gui/media-center/default.nix
+++ modules/nixos/gui/media-center/default.nix
@@ -57,7 +57,7 @@ in {
             plex-media-player
             ratpoison
             pavucontrol
-            syncplay
+            # syncplay
             mpv
             # teamspeak_client
           ];
