cca2539151bb362c266bf1918db3fecb3268220f
Tomas Harkema <tomas@harkema.io>
2024-03-23 01:31:00 +0100

    downgrade

--- overlays/nix/default.nix
+++ overlays/nix/default.nix
@@ -22,5 +22,5 @@
   nixos-anywhere = nixos-anywhere.packages.${prev.system}.nixos-anywhere;
   nixos-conf-editor = nixos-conf-editor.packages.${prev.system}.nixos-conf-editor;
   nix-software-center = nix-software-center.packages.${prev.system}.nix-software-center;
-  nixUnstable = channels.unstable.nixVersions.nix_2_21;
+  nixUnstable = channels.unstable.nixVersions.nix_2_20;
 }
