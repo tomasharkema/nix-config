c7063d10d15e1fc20b4db842a6703b335a8ad2f7
Tomas Harkema <tomas@harkema.io>
2024-03-25 14:02:24 +0100

    ✨ Add Spotifyd service configuration
    Configure the Spotifyd service to enable use with updated dependencies and firewall settings. Enable the Spotifyd app in the NixOS configuration, allowing for media center integration.

---
+++ modules/nixos/apps/spotifyd/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.apps.spotifyd;
+in {
+  options.apps.spotifyd = {
+    enable = mkEnableOption "spotifyd";
+  };
+
+  config = mkIf cfg.enable {
+    #   environment.systemPackages = with pkgs; [
+    #     # spotify
+    #     spotifyd
+    #   ];
+
+    services.spotifyd = {
+      enable = true;
+      config = ''
+        [global]
+        use_mpris = false
+      '';
+      #   username_cmd = "${lib.getExe pkgs._1password} item get bnzrqxggvfbfhgln4uceawfbbq --field username"
+      #   password_cmd = "${lib.getExe pkgs._1password} item get bnzrqxggvfbfhgln4uceawfbbq --field password"
+    };
+    #   # systemd.services.spotifyd = {
+    #   # environment = {
+    #   #   OP_CONNECT_HOST = "http://silver-star.ling-lizard.ts.net:7080";
+    #   #   OP_CONNECT_TOKEN = "${config.age.secrets.op.path}";
+    #   # };
+    #   # };
+
+    networking.firewall = {
+      allowedUDPPorts = [33677];
+      allowedTCPPorts = [33677];
+    };
+    # };
+  };
+}
