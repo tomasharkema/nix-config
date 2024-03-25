1b36b689469c9c760e4cffc1ad7a83c5becb5f9d
Tomas Harkema <tomas@harkema.io>
2024-03-25 01:26:54 +0100

    🚀 Update: Refactored git-butler package, added custom dependencies
    - Updated git-butler package to version 0.10.27-746
    - Refactored package installation scripts to handle appImage extraction
    - Added libthai as a custom dependency
    - Renamed gitbutler to git-butler for consistency
    - Included additional systemPackages dependencies like caffeine-ng and qjournalctl

---
+++ packages/git-butler/default.nix
@@ -0,0 +1,27 @@
+{
+  appimageTools,
+  fetchzip,
+  fetchurl,
+  lib,
+}:
+appimageTools.wrapType2 rec {
+  pname = "git-butler";
+  version = "0.10.27-746";
+
+  src = fetchurl {
+    url = "https://releases.gitbutler.com/releases/release/${version}-746/linux/x86_64/git-butler_${(builtins.head (lib.strings.splitString "-" version))}_amd64.AppImage";
+    hash = "sha256-J7y/KfT5EfxWks2OXYGJIkPex8yh4jdFBhyGKXTXKFA=";
+  };
+  extraPkgs = pkgs: with pkgs; [libthai];
+
+  extraInstallCommands = let
+    contents = appimageTools.extractType2 {inherit pname version src;};
+  in ''
+    mkdir -p "$out/share/applications"
+    # cp "${contents}/${pname}.desktop" "$out/share/applications/"
+    cp -r ${contents}/usr/share/* "$out/share"
+    ln -fns "$out/bin/${pname}-${version}" "$out/bin/${pname}"
+
+    # substituteInPlace $out/share/applications/${pname}.desktop --replace 'Exec=AppRun' 'Exec=${pname}'
+  '';
+}
