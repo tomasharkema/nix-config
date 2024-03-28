dd2f348d8efb1a9e3a923e262573ae4b8d76a67e
Tomas Harkema <tomas@harkema.io>
2024-03-28 13:11:02 +0100

    🔧 Update Nix configurations: Remove tomas substituters and keys

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -118,19 +118,17 @@ in {
     '';
 
     nix.settings = {
-      extra-experimental-features = "nix-command flakes";
+      extra-experimental-features = "nix-command flakes cgroups";
       trusted-users = ["root" "tomas" "builder"];
 
       substituters = [
         "https://nix-community.cachix.org"
-        "https://tomasharkema.cachix.org"
         "https://cache.nixos.org/"
         "https://blue-fire.ling-lizard.ts.net/attic/tomas/"
         "https://devenv.cachix.org"
       ];
 
       trusted-public-keys = [
-        "tomasharkema.cachix.org-1:LOeGvH7jlA3vZmW9+gHyw0BDd1C8a0xrQSl9WHHTRuA="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
