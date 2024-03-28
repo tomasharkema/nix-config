10e6ceea0cdbddd94bdb5a9d8d49fda31640a5de
Tomas Harkema <tomas@harkema.io>
2024-03-28 23:25:02 +0100

    fix keys

--- packages/authorized-keys/default.nix
+++ packages/authorized-keys/default.nix
@@ -1,9 +1,20 @@
 {
   fetchurl,
   lib,
-}: {
+  stdenv,
+}:
+stdenv.mkDerivation rec {
+  pname = "authorized_keys";
+  version = "0.0.1";
+
   src = fetchurl {
     url = "https://github.com/tomasharkema.keys";
     sha256 = "sha256-YJLSHQN5TotsCLHRX35VzemFZ1kYMAfJkfH89EZyptU=";
   };
+
+  dontUnpack = true;
+
+  installPhase = ''
+    cp ${src} $out
+  '';
 }
