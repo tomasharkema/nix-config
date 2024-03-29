80b88a3be425fd5935a2060b8637e57d807be967
Tomas Harkema <tomas@harkema.io>
2024-03-29 23:05:43 +0100

    feat(services): Add and configure polybar, notify-osd, mpd, mbsync, and mopidy services

--- modules/home/packages/services/default.nix
+++ modules/home/packages/services/default.nix
@@ -12,8 +12,27 @@ with lib; {
       status-notifier-watcher.enable = true;
       poweralertd.enable = true;
       pueue.enable = true;
-      polybar.enable = true;
+      polybar = {
+        enable = true;
+        script = "polybar bar &";
+        extraConfig = ''
+          [module/date]
+          type = internal/date
+          interval = 5
+          date = "%d.%m.%y"
+          time = %H:%M
+          format-prefix-foreground = \''${colors.foreground-alt}
+          label = %time%  %date%
+        '';
+      };
       playerctld.enable = true;
+      notify-osd.enable = true;
+      mpd = {
+        enable = true;
+        musicDirectory = "/home/tomas/Music";
+      };
+      mbsync.enable = true;
+      mopidy.enable = true;
     };
   };
 }
