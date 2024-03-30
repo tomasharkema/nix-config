ee1d4cda4ed3bffefd5c676ece02c81a9af590b6
Tomas Harkema <tomas@harkema.io>
2024-03-30 22:10:25 +0100

    try to fix yubikey

--- modules/nixos/traits/developer/default.nix
+++ modules/nixos/traits/developer/default.nix
@@ -12,23 +12,50 @@ in {
   };
   config = mkIf cfg.enable {
     system.nixos.tags = ["developer"];
-    environment.systemPackages = with pkgs; [
-      devenv
-      deploy-rs
-      nixfmt
-      nix-prefetch-git
-      nix-output-monitor
-      flake-checker
-      flake-checker
-      go
-      go-outline
-      gocode
-      gocode-gomod
-      godef
-      golint
-      gopkgs
-      gopls
-      gotools
-    ];
+
+    environment.systemPackages = with pkgs;
+    # nix
+      [
+        # # snowfallorg.flake
+        # cachix
+        # nix-init
+        # nixci
+        agenix
+        alejandra
+        deadnix
+        deploy-rs
+        devenv
+        fh
+        flake-checker
+        hydra-cli
+        manix
+        nil
+        nixd
+        nix-eval-jobs
+        # nix-fast-build
+        nix-output-monitor
+        nix-prefetch-git
+        nix-prefetch-scripts
+        nix-serve
+        nix-tree
+        nixfmt
+        nixos-shell
+        nixpkgs-fmt
+        nixpkgs-lint
+        nurl
+        statix
+      ]
+      # go
+      ++ [
+        go
+        go-outline
+        gocode
+        gocode-gomod
+        godef
+        golint
+        gopkgs
+        gopls
+        gotools
+      ];
   };
 }
