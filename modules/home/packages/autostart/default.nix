7567e57d73174375f62b73c55a2906232fd64fdf
Tomas Harkema <tomas@harkema.io>
2024-03-30 15:21:31 +0100

    try to fix yubikey

--- modules/home/packages/autostart/default.nix
+++ modules/home/packages/autostart/default.nix
@@ -10,24 +10,24 @@ with lib.custom; {
     programs = mkOpt (types.listOf types.package) [] "Autostart programs";
   };
 
-  #   config = {
-  #     home.file = builtins.listToAttrs (map
-  #       (pkg: {
-  #         name = ".config/autostart/" + pkg.pname + ".desktop";
-  #         value =
-  #           if pkg ? desktopItem
-  #           then {
-  #             # Application has a desktopItem entry.
-  #             # Assume that it was made with makeDesktopEntry, which exposes a
-  #             # text attribute with the contents of the .desktop file
-  #             text = pkg.desktopItem.text;
-  #           }
-  #           else {
-  #             # Application does *not* have a desktopItem entry. Try to find a
-  #             # matching .desktop name in /share/apaplications
-  #             source = pkg + "/share/applications/" + pkg.pname + ".desktop";
-  #           };
-  #       })
-  #       config.autostart.programs);
-  #   };
+  config = {
+    home.file = builtins.listToAttrs (map
+      (pkg: {
+        name = ".config/autostart/" + pkg.pname + ".desktop";
+        value =
+          if pkg ? desktopItem
+          then {
+            # Application has a desktopItem entry.
+            # Assume that it was made with makeDesktopEntry, which exposes a
+            # text attribute with the contents of the .desktop file
+            text = pkg.desktopItem.text;
+          }
+          else {
+            # Application does *not* have a desktopItem entry. Try to find a
+            # matching .desktop name in /share/apaplications
+            source = pkg + "/share/applications/" + pkg.pname + ".desktop";
+          };
+      })
+      config.autostart.programs);
+  };
 }
