f779a77f78892f382feebbf5d91a4816439a6eb9
Tomas Harkema <tomas@harkema.io>
2024-04-02 20:40:12 +0200

    fixes

--- systems/x86_64-linux/arthur/default.nix
+++ systems/x86_64-linux/arthur/default.nix
@@ -48,18 +48,6 @@
 
     boot = {
       binfmt.emulatedSystems = ["aarch64-linux"];
-      loader = {
-        efi = {
-          canTouchEfiVariables = true;
-          efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
-        };
-        grub = {
-          enable = true;
-          efiSupport = true;
-          #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-          device = "nodev";
-        };
-      };
     };
 
     networking = {
