8e3d0fb7633f17b25aa8580e400432ea1db61c5f
tomasharkema <tomas@harkema.io>
2024-04-04 18:58:34 +0200

    add nfs

--- modules/nixos/traits/hardware/nfs/default.nix
+++ modules/nixos/traits/hardware/nfs/default.nix
@@ -3,13 +3,63 @@
   config,
   ...
 }:
-with lib;
-with lib.custom; let
-  cfg = config.traits.hardware.nvidia;
+with lib; let
+  cfg = config.traits.hardware.nfs;
 in {
   options.traits = {
     hardware.nfs = {
-      enable = mkBoolOpt false "nfs";
+      enable = mkEnableOption "nfs";
+
+      machines = {
+        silver-star.enable = mkEnableOption "nfs silver-star";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    assertions = [];
+
+    services.rpcbind.enable = true;
+
+    systemd = let
+      mounts = optionals cfg.machines.silver-star.enable [
+        {
+          what = "192.168.0.100:/mnt/user/domains";
+          where = "/mnt/unraid/domains";
+        }
+        {
+          what = "192.168.0.100:/mnt/user/appdata";
+          where = "/mnt/unraid/appdata";
+        }
+        {
+          what = "192.168.0.100:/mnt/user/appdata_ssd";
+          where = "/mnt/unraid/appdata_ssd";
+        }
+        {
+          what = "192.168.0.100:/mnt/user/appdata_disk";
+          where = "/mnt/unraid/appdata_disk";
+        }
+        {
+          what = "192.168.0.100:/mnt/user/data";
+          where = "/mnt/unraid/data";
+        }
+      ];
+    in {
+      mounts = lists.forEach mounts (mnt:
+        mnt
+        // {
+          type = "nfs";
+          mountConfig = {
+            Options = "noatime";
+          };
+        });
+      automounts = lists.forEach mounts (mnt: {
+        wantedBy = ["multi-user.target"];
+        automountConfig = {
+          TimeoutIdleSec = "600";
+        };
+        where = mnt.where;
+      });
     };
   };
 }
