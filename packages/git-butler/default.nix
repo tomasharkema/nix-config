0dffa788cedf6b0db2fa048c333d0276b400a356
Tomas Harkema <tomas@harkema.io>
2024-04-19 13:13:58 +0200

    ✨ Refactor: Update settings for Nix Language Server in VS Code
    🔧 Changed nix.serverPath to "nil" instead of "nixd" for language server settings.

--- packages/git-butler/default.nix
+++ packages/git-butler/default.nix
@@ -28,8 +28,6 @@ in
       hash = "sha256-GwyW5t2LslC3KoAvcngrec3qscsZaLWH5CyjI9nrZoo=";
     };
 
-    # https://releases.gitbutler.com/releases/release/0.11.3-838/linux/x86_64/git-butler_0.11.3_amd64.deb
-
     nativeBuildInputs = [
       autoPatchelfHook
       dpkg
@@ -62,39 +60,3 @@ in
       runHook postInstall
     '';
   }
-# {
-#   appimageTools,
-#   fetchzip,
-#   fetchurl,
-#   lib,
-#   stdenv,
-# }: let
-#   shortVersion = "0.11.1";
-#   build = "814";
-#   fullVersion = "${shortVersion}-${build}";
-#   fullArch =
-#     if stdenv.isx86_64
-#     then "x86_64"
-#     else "aarch64";
-#   arch =
-#     if stdenv.isx86_64
-#     then "amd64"
-#     else "aarch64";
-# in
-#   appimageTools.wrapType2 rec {
-#     pname = "git-butler";
-#     version = fullVersion;
-#     src = fetchurl {
-#       url = "https://releases.gitbutler.com/releases/release/${fullVersion}/linux/${fullArch}/git-butler_${shortVersion}_${arch}.AppImage";
-#       hash = "sha256-N/6aaB7RWCjF0jIMKR6ymvm6Md9o+sRB8cetPxEBUAo=";
-#     };
-#     extraPkgs = pkgs: with pkgs; [libthai];
-#     extraInstallCommands = let
-#       contents = appimageTools.extractType2 {inherit pname version src;};
-#     in ''
-#       mkdir -p "$out/share/applications"
-#       cp -r ${contents}/usr/share/* "$out/share"
-#       ln -fns "$out/bin/${pname}-${version}" "$out/bin/${pname}"
-#     '';
-#   }
-
