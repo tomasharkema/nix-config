4eb6a22181418f658af48c03e645fd3b2455a624
tomasharkema <tomas@harkema.io>
2024-04-19 16:57:30 +0200

    update

--- config.nix
+++ config.nix
@@ -9,6 +9,7 @@
     # netrc-file = "/etc/nix/netrc";
 
     substituters = [
+      "https://nix-gaming.cachix.org"
       "https://nix-community.cachix.org"
       "https://cache.nixos.org/"
       "https://blue-fire.ling-lizard.ts.net/attic/tomas/"
