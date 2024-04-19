cf0707886e3bdf8f8ec0d1192559234833338d9e
Tomas Harkema <tomas@harkema.io>
2024-04-20 00:23:48 +0200

    try to fix rdp

---
+++ modules/nixos/gui/xrdp/default.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib; {
+  config = mkIf config.gui.enable {
+    services.xrdp = {
+      enable = true;
+
+      confDir = "/etc/xrdp";
+
+      package = pkgs.xrdp.overrideAttrs (oldAttrs: {
+        configureFlags = oldAttrs.configureFlags ++ ["--enable-vsock"];
+
+        postInstall =
+          oldAttrs.postInstall
+          + ''
+            substituteInPlace $out/etc/xrdp/xrdp.ini \
+              --replace "port=3389" "port=tcp://0.0.0.0:3389" \
+              --replace "security_layer=negotiate" "security_layer=rdp" \
+              --replace "crypt_level=high" "crypt_level=none" \
+              --replace "bitmap_compression=true" "bitmap_compression=false"
+            substituteInPlace $out/etc/xrdp/sesman.ini \
+              --replace "X11DisplayOffset=10" "X11DisplayOffset=0" \
+          '';
+      });
+    };
+
+    systemd = {
+      services.xrdp = {
+        serviceConfig = {
+          ExecStart = lib.mkForce "${pkgs.xrdp}/bin/xrdp --nodaemon --config /etc/xrdp/xrdp.ini";
+          # https://github.com/NixOS/nixpkgs/blob/nixos-23.11/nixos/modules/services/networking/xrdp.nix#L158
+          # seems the integer port results in ipv6 only
+        };
+      };
+    };
+  };
+}
