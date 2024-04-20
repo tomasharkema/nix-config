4a5134802f427b64b187c24193f64b48b1d3a0f6
tomasharkema <tomas@harkema.io>
2024-04-20 13:54:45 +0200

    fix rdp

--- modules/nixos/gui/xrdp/default.nix
+++ modules/nixos/gui/xrdp/default.nix
@@ -18,13 +18,13 @@ with lib; {
           oldAttrs.postInstall
           + ''
             substituteInPlace $out/etc/xrdp/xrdp.ini \
-              --replace "port=3389" "port=tcp://0.0.0.0:3389" \
-              --replace "security_layer=negotiate" "security_layer=rdp" \
-              --replace "crypt_level=high" "crypt_level=none" \
-              --replace "bitmap_compression=true" "bitmap_compression=false"
+              --replace "port=3389" "port=tcp://0.0.0.0:3389" #\
+              # --replace "security_layer=negotiate" "security_layer=rdp" \
+            #   --replace "crypt_level=high" "crypt_level=none" \
+            #   --replace "bitmap_compression=true" "bitmap_compression=false"
 
-            substituteInPlace $out/etc/xrdp/sesman.ini \
-              --replace "X11DisplayOffset=10" "X11DisplayOffset=0" \
+            # substituteInPlace $out/etc/xrdp/sesman.ini \
+            #   --replace "X11DisplayOffset=10" "X11DisplayOffset=0" \
           '';
       });
     };
