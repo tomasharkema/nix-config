0d7792e012acb65110284b782a166ab053075864
Tomas Harkema <tomas@harkema.io>
2024-04-23 14:07:50 +0200

    fix timer

--- modules/nixos/apps/btrbk/default.nix
+++ modules/nixos/apps/btrbk/default.nix
@@ -8,6 +8,8 @@
     services.btrbk = mkIf config.disks.btrfs.enable {
       extraPackages = with pkgs; [zstd mbuffer];
       instances."${config.networking.hostName}-btrbk" = {
+        onCalendar = "hourly";
+
         settings = {
           snapshot_preserve = "14d";
           snapshot_preserve_min = "2d";
