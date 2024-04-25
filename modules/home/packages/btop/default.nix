9874b91bc4b4fd0493f61d1bad3f07849b3b066b
Tomas Harkema <tomas@harkema.io>
2024-04-25 14:03:07 +0200

    fix nvidia things

--- modules/home/packages/btop/default.nix
+++ modules/home/packages/btop/default.nix
@@ -9,14 +9,14 @@
     };
 
     home.file = {
-      ".config/btop/themes" = {
-        source = pkgs.fetchFromGitHub {
-          owner = "catppuccin";
-          repo = "btop";
-          rev = "c6469190f2ecf25f017d6120bf4e050e6b1d17af";
-          hash = "sha256-jodJl4f2T9ViNqsY9fk8IV62CrpC5hy7WK3aRpu70Cs=";
-        };
-      };
+      # ".config/btop/themes" = {
+      #   source = pkgs.fetchFromGitHub {
+      #     owner = "catppuccin";
+      #     repo = "btop";
+      #     rev = "c6469190f2ecf25f017d6120bf4e050e6b1d17af";
+      #     hash = "sha256-jodJl4f2T9ViNqsY9fk8IV62CrpC5hy7WK3aRpu70Cs=";
+      #   };
+      # };
     };
   };
 }
