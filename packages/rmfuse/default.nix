6ef04c98ead6bbd34bba010581b9457aadc4ccb5
Tomas Harkema <tomas@harkema.io>
2024-04-27 17:08:16 +0200

    fixes

--- packages/rmfuse/default.nix
+++ packages/rmfuse/default.nix
@@ -12,13 +12,6 @@ python3Packages.buildPythonPackage rec {
     sha256 = "sha256-lPce8tiqIPyZgbCgAxiADca+JRjUzYLDfA0tf1SQftI=";
   };
 
-  # doCheck = false;
-
-  # nativeBuildInputs = [
-  #   pdm-pep517
-  #   pep517
-  # ];
-
   nativeBuildInputs = with python3Packages; [
     setuptools
     wheel
@@ -30,22 +23,7 @@ python3Packages.buildPythonPackage rec {
     # python-daemon
     bidict
     pyfuse3
+    rmcl
+    rmrl
   ];
 }
-# {
-#   lib,
-#   python310Packages,
-#   fetchFromGitHub,
-# }:
-# python310Packages.buildPythonApplication rec {
-#   pname = "rmfuse";
-#   version = "0.2.3";
-#   pyproject = true;
-#   src = fetchFromGitHub {
-#     owner = "rschroll";
-#     repo = "rmfuse";
-#     rev = "v${version}";
-#     hash = "sha256-Esm2UmvTJk8rOL5LxvBmsLtkbh9h15DaqG8hpAdhk6o=";
-#   };
-# }
-
