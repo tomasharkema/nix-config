ea8d8da71f517db86ab832d677fedd1858aceb71
tomas <tomas@harkema.io>
2024-04-30 20:36:08 +0200

    fix icewm

--- modules/nixos/home/default.nix
+++ modules/nixos/home/default.nix
@@ -7,6 +7,23 @@
   ...
 }:
 with lib; {
+  options.home = {
+    homeFiles = mkOption {
+      description = "Attribute set of files to link into the user home.";
+      default = {};
+      type = types.attrsOf (types.submodule {
+        options = {
+          source = mkOption {
+            type = types.path;
+          };
+          # target = mkOption {
+          #   type = types.str;
+          # };
+        };
+      });
+    };
+  };
+
   config = {
     snowfallorg.user.${config.user.name}.home.config = {
       home.stateVersion = mkDefault "23.11";
