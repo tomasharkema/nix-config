9d510d4218fd63f8980d739fccaa3f75ef6060d4
Tomas Harkema <tomas@harkema.io>
2024-05-01 12:50:17 +0200

    ojoo

--- systems/x86_64-linux/schweizer-bobbahn/default.nix
+++ systems/x86_64-linux/schweizer-bobbahn/default.nix
@@ -9,7 +9,7 @@ with lib; {
   imports = with inputs; [
     ./hardware-configuration.nix
 
-    nixos-hardware.nixosModules.common-cpu-intel
+    # nixos-hardware.nixosModules.common-cpu-intel
     nixos-hardware.nixosModules.common-pc-ssd
 
     nixos-hardware.nixosModules.common-gpu-intel
