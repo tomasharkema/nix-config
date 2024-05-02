17ba605afd12f1bdc2aca7de3f32c7c8d59b84fb
Tomas Harkema <tomas@harkema.io>
2024-05-02 14:46:41 +0200

    ojoo

--- modules/darwin/default/default.nix
+++ modules/darwin/default/default.nix
@@ -105,10 +105,10 @@
         builders-use-substitutes = true
       '';
 
-      trusted-users = ["root" "tomas"];
+      settings.trusted-users = ["root" "tomas"];
       # netrc-file = "/etc/nix/netrc";
 
-      substituters = [
+      settings.substituters = [
         "https://nix-gaming.cachix.org"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org/"
@@ -117,7 +117,7 @@
         "https://tomasharkema.cachix.org"
       ];
 
-      trusted-public-keys = [
+      settings.trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
--- modules/darwin/home/default.nix
+++ modules/darwin/home/default.nix
@@ -8,6 +8,22 @@
 }:
 with lib;
 with lib.custom; {
+  options.home = {
+    homeFiles = mkOption {
+      description = "Attribute set of files to link into the user home.";
+      default = {};
+      type = types.attrsOf (types.submodule {
+        options = {
+          source = mkOption {
+            type = types.path;
+          };
+          # target = mkOption {
+          #   type = types.str;
+          # };
+        };
+      });
+    };
+  };
   config = {
     snowfallorg.user.${config.user.name}.home.config = {
       home.stateVersion = mkDefault "23.11";
