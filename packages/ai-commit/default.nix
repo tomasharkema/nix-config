7df47ec291979e2b60c9ab2d2a41d0487db0ef0f
Tomas Harkema <tomas@harkema.io>
2024-05-02 14:22:13 +0200

    build(ai-commit): Update npm package hash and postPatch action
    Changed the npm package hash and added a postPatch action to copy package-lock.json.
    
    - Update npm package hash
    - Add postPatch action to copy package-lock.json

--- packages/ai-commit/default.nix
+++ packages/ai-commit/default.nix
@@ -23,6 +23,12 @@ buildNpmPackage rec {
     owner = "insulineru";
     repo = pname;
     rev = "76f3f9983bbd435cadf7c854f00f70f165122c59";
-    hash = "sha256-ZsEAE9EDJLREpKjHLbvqAUNM/y9eCH44g3D8NHYHiT4=";
+    hash = "sha256-pqcczObKkeC2sS0Y0XHBZM+oS4x4UUlr7pSutls7GzI=";
   };
+
+  postPatch = ''
+    cp ${./package-lock.json} package-lock.json
+  '';
+
+  npmDepsHash = "";
 }
