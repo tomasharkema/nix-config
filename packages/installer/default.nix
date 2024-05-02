0b7d9e0522bcbe506b828cfb8e2db000812cfdba
Tomas Harkema <tomas@harkema.io>
2024-05-02 22:52:43 +0200

    ✨ Refactor installer script
    Refactor and improve the installer script by organizing the code, adding
    tailscale status check and updating the installation process.

--- packages/installer/default.nix
+++ packages/installer/default.nix
@@ -1,26 +1,91 @@
 {
   inputs,
-  pkgs,
-  self,
   lib,
+  jq,
+  gum,
+  system,
+  writeShellApplication,
+  tailscale,
+  configurations ? {
+    "foo" = {};
+    bar = {};
+  },
+  writeText,
+  stdenv,
 }: let
-  disko = inputs.disko.packages."${pkgs.system}".disko;
+  disko =
+    if stdenv.isLinux
+    then inputs.disko.packages."${system}".disko
+    else "true";
 
-  gum = "${lib.getExe pkgs.gum}";
+  configurationNames = lib.attrNames configurations;
+  configurationNamesString = builtins.trace configurationNames (builtins.toString configurationNames);
+  hosts = builtins.trace configurationNamesString (writeText "hosts.txt" ''
+    ${configurationNamesString}
+  '');
 
-  hosts = builtins.writeFile (builtins.attrNames self.nixosConfigurations);
+  tailscaleStatus = writeShellApplication {
+    name = "tailscaleStatus";
+
+    runtimeInputs = [
+      tailscale
+      jq
+    ];
+
+    text = ''
+      set -ex
+      TAILSCALE_STATUS="$(tailscale status --json | jq -r '.BackendState')"
+      case "$TAILSCALE_STATUS" in
+        "Stopped")
+          exit 1
+          ;;
+
+        *)
+          exit 0
+          ;;
+      esac
+    '';
+  };
 in
-  pkgs.writeShellScriptBin "installer" ''
-    set -e
-    echo "Login with Tailscale..."
+  writeShellApplication {
+    name = "installer";
 
-    ${pkgs.tailscale}/bin/tailscale up --qr --accept-dns
+    runtimeInputs = [
+      tailscaleStatus
+      gum
+      tailscale
+      jq
+    ];
 
-    export HOSTNAME_INST="$(${gum} filter --placeholder Hostname < ${hosts})"
+    text = ''
+      set -ex
 
-    echo "Installing $HOSTNAME_INST..."
+      function tailscaleStatus () {
+        TAILSCALE_STATUS="$(tailscale status --json | jq -r '.BackendState')"
+        case "$TAILSCALE_STATUS" in
+          "Stopped")
+            return 1
+            ;;
 
-    ${disko}/bin/disko --mode mount --flake "github:tomasharkema/nix-config#$HOSTNAME_INST" || {
-      ${gum} confirm "Format disk?" && ${disko}/bin/disko --mode disko --flake "github:tomasharkema/nix-config#$HOSTNAME_INST"
-    }
-  ''
+          *)
+            return 0
+            ;;
+        esac
+      }
+
+
+      tailscaleStatus || {
+        echo "Login with Tailscale..."
+
+        tailscale up --qr --accept-dns
+      }
+
+      HOSTNAME_INST="$(gum filter --placeholder Hostname < ${hosts})"
+
+      echo "Installing $HOSTNAME_INST..."
+      disko --mode mount --flake "github:tomasharkema/nix-config#$HOSTNAME_INST" || {
+        gum confirm "Format disk?" && disko --mode disko --flake "github:tomasharkema/nix-config#$HOSTNAME_INST"
+      }
+
+    '';
+  }
