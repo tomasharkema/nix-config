60c71de0ff836e5f6dcf9300293442126239a583
Tomas Harkema <tomas@harkema.io>
2024-05-02 14:43:37 +0200

    build: Update version to 0.0.1-rc.15 and npm dependencies hash.

--- packages/inshellisense/default.nix
+++ packages/inshellisense/default.nix
@@ -17,21 +17,21 @@
 }:
 buildNpmPackage rec {
   pname = "inshellisense";
-  version = "0.0.1-rc.14";
+  version = "0.0.1-rc.15";
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = pname;
     rev = "${version}";
-    hash = "sha256-ZsEAE9EDJLREpKjHLbvqAUNM/y9eCH44g3D8NHYHiT4=";
+    hash = "sha256-/6pU8ubasONPMe1qnE+Db0nzdHRQTo9fhMr7Xxjgsos="; # "sha256-ZsEAE9EDJLREpKjHLbvqAUNM/y9eCH44g3D8NHYHiT4=";
   };
 
-  passthru.updateScript = gitUpdater {
-    url = "https://github.com/microsoft/inshellisense.git";
-    rev-prefix = "grpc-tools@";
-  };
+  # passthru.updateScript = gitUpdater {
+  #   url = "https://github.com/microsoft/inshellisense.git";
+  #   rev-prefix = "grpc-tools@";
+  # };
 
-  npmDepsHash = "sha256-p0/GnAdWNM/wjB/w+rXbOrh3Hr/smIW0IVQga7uCKYY=";
+  npmDepsHash = "sha256-rOyvFA5X3o1TCgY54XxNSg0+QotA8IUauLngTtJuRj4="; #"sha256-p0/GnAdWNM/wjB/w+rXbOrh3Hr/smIW0IVQga7uCKYY=";
 
   postInstall = ''
     cp -r shell $out/share
