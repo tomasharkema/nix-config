377bc0bc590ae3fb92035a399ffcbbc08dda5af6
Tomas Harkema <tomas@harkema.io>
2024-05-02 15:30:49 +0200

    🔧 Update inshellisense package: adjust dependencies and fix build on Darwin systems.

--- modules/home/packages/inshellisense/default.nix
+++ modules/home/packages/inshellisense/default.nix
@@ -21,17 +21,28 @@ in {
     home.packages = [cfg.package];
 
     programs.zsh = mkIf cfg.enableZshIntegration {
+      # initExtra = ''
+      #   . "${cfg.package}/share/shellIntegration-rc.zsh"
+      # '';
+      # profileExtra = ''
+      #   . "${cfg.package}/share/shellIntegration-profile.zsh"
+      # '';
+      # loginExtra = ''
+      #   . "${cfg.package}/share/shellIntegration-login.zsh"
+      # '';
+      # envExtra = ''
+      #   . "${cfg.package}/share/shellIntegration-env.zsh"
+      # '';
+
       initExtra = ''
-        . "${cfg.package}/share/shellIntegration-rc.zsh"
-      '';
-      profileExtra = ''
-        . "${cfg.package}/share/shellIntegration-profile.zsh"
-      '';
-      loginExtra = ''
-        . "${cfg.package}/share/shellIntegration-login.zsh"
-      '';
-      envExtra = ''
-        . "${cfg.package}/share/shellIntegration-env.zsh"
+        # ---------------- inshellisense shell plugin ----------------
+        if [[ -z "$ISTERM" && $- = *i* && $- != *c* ]]; then
+          if [[ -o login ]]; then
+            is -s zsh --login ; exit
+          else
+            is -s zsh ; exit
+          fi
+        fi
       '';
     };
   };
