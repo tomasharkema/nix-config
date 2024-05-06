e4f42287ab3b8291723678bb5ba11c591464fab6
Tomas Harkema <tomas@harkema.io>
2024-05-06 15:00:37 +0200

    updates

--- modules/nixos/apps/attic/default.nix
+++ modules/nixos/apps/attic/default.nix
@@ -35,40 +35,27 @@ in {
     '';
   in
     mkIf cfg.enable {
-      systemd.user.services.attic-login = {
-        description = "attic-login";
-        script = ''${attic-login}'';
+      # systemd.user.services.attic-login = {
+      #   description = "attic-login";
+      #   script = ''${attic-login}'';
 
-        wants = ["multi-user.target" "network.target"];
-        after = ["multi-user.target" "network.target"];
+      # systemd.services.attic-watch = {
+      #   enable = true;
+      #   description = "attic-watch";
+      #   unitConfig = {
+      #     StartLimitIntervalSec = 500;
+      #     StartLimitBurst = 5;
+      #   };
+      #   serviceConfig = {
+      #     RestartSec = 5;
+      #     MemoryHigh = "4G";
+      #     MemoryMax = "5G";
+      #     Nice = 15;
+      #   };
 
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-        };
-      };
-
-      environment.systemPackages = with pkgs; [attic];
-
-      systemd.services.attic-watch = {
-        enable = true;
-        description = "attic-watch";
-        unitConfig = {
-          StartLimitIntervalSec = 500;
-          StartLimitBurst = 5;
-        };
-        serviceConfig = {
-          RestartSec = 5;
-          MemoryHigh = "4G";
-          MemoryMax = "5G";
-          Nice = 15;
-        };
-        preStart = ''
-          ${attic-login}
-        '';
-        script = "${pkgs.attic}/bin/attic watch-store tomas:tomas -j 1";
-        wants = ["multi-user.target" "network.target"];
-        after = ["multi-user.target" "network.target"];
-      };
+      #   script = "${pkgs.attic}/bin/attic watch-store tomas:tomas -j 1";
+      #   wants = ["multi-user.target" "network.target"];
+      #   after = ["multi-user.target" "network.target"];
+      # };
     };
 }
