3a06d1c0a7b30b9c19f13bb57948d2f21a9f4369
Tomas Harkema <tomas@harkema.io>
2024-05-06 12:12:07 +0200

    ojoo

--- modules/nixos/apps/attic/default.nix
+++ modules/nixos/apps/attic/default.nix
@@ -28,47 +28,26 @@ in {
     };
   };
 
-  config = let
-    attic-login = writeShellScript "attic-script" ''
-      ${pkgs.attic}/bin/attic login tomas https://nix-cache.harke.ma/ $(cat ${config.age.secrets.attic-key.path})
-      ${pkgs.attic}/bin/attic use tomas:tomas
-    '';
-  in
-    mkIf cfg.enable {
-      systemd.user.services.attic-login = {
-        description = "attic-login";
-        script = ''${attic-login}'';
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [attic];
 
-        wants = ["multi-user.target" "network.target"];
-        after = ["multi-user.target" "network.target"];
-
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-        };
+    systemd.services.attic-watch = {
+      enable = true;
+      description = "attic-watch";
+      unitConfig = {
+        StartLimitIntervalSec = 500;
+        StartLimitBurst = 5;
+      };
+      serviceConfig = {
+        RestartSec = 5;
+        MemoryHigh = "4G";
+        MemoryMax = "5G";
+        Nice = 15;
       };
 
-      environment.systemPackages = with pkgs; [attic];
-
-      systemd.services.attic-watch = {
-        enable = true;
-        description = "attic-watch";
-        unitConfig = {
-          StartLimitIntervalSec = 500;
-          StartLimitBurst = 5;
-        };
-        serviceConfig = {
-          RestartSec = 5;
-          MemoryHigh = "4G";
-          MemoryMax = "5G";
-          Nice = 15;
-        };
-        preStart = ''
-          ${attic-login}
-        '';
-        script = "${pkgs.attic}/bin/attic watch-store tomas:tomas -j 1";
-        wants = ["multi-user.target" "network.target"];
-        after = ["multi-user.target" "network.target"];
-      };
+      script = "${pkgs.attic}/bin/attic watch-store tomas:tomas -j 1";
+      wants = ["multi-user.target" "network.target"];
+      after = ["multi-user.target" "network.target"];
     };
+  };
 }
