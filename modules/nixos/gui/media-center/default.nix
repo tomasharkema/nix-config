05f940db0c81755e7b682b23a6051ae9f2613446
tomasharkema <tomas@harkema.io>
2024-05-07 15:55:45 +0200

    ojoo

--- modules/nixos/gui/media-center/default.nix
+++ modules/nixos/gui/media-center/default.nix
@@ -13,22 +13,42 @@ in {
 
   config = mkIf cfg.enable {
     system.nixos.tags = ["media-center"];
-#    xdg.portal.enable = mkForce false;
-    
+    #    xdg.portal.enable = mkForce false;
+
     programs.dconf.enable = true;
-    
+
     sound = {
-      enable = true;
+      enable = false;
       mediaKeys.enable = true;
     };
 
-    hardware.pulseaudio = {
+    security.rtkit.enable = true;
+    services.pipewire = {
       enable = true;
-      support32Bit = true;
-      package = pkgs.pulseaudioFull;
-      #extraConfig = ''
-      #  load-module module-dbus-protocol
-      #'';
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      jack.enable = true;
+    };
+
+    services.pipewire.extraConfig.pipewire-pulse."92-tcp" = {
+      context.modules = [
+        {
+          name = "module-native-protocol-tcp";
+          args = {
+          };
+        }
+        {
+          name = "module-zeroconf-publish";
+          args = {
+          };
+        }
+      ];
+      stream.properties = {
+        node.latency = "32/48000";
+        resample.quality = 1;
+      };
     };
 
     apps.flatpak.enable = mkForce false;
@@ -85,7 +105,6 @@ in {
 
       enable = true;
       extraArguments = ["-d" "-m" "last"];
-      
     };
 
     # services.xserver = {
@@ -132,7 +151,7 @@ in {
       uid = 1100;
       extraGroups = ["data" "video" "audio" "input"];
     };
-    
+
     home-manager = {
       users.media = {
         home.stateVersion = mkDefault "23.11";
