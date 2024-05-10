d7824d0e51f69c2e4ce3bed513b7a8d2527eb987
Tomas Harkema <tomas@harkema.io>
2024-05-10 13:49:19 +0200

    fixes

--- modules/darwin/default/default.nix
+++ modules/darwin/default/default.nix
@@ -100,30 +100,25 @@
         builders-use-substitutes = true
       '';
 
-      settings.trusted-users = ["root" "tomas"];
-      # netrc-file = "/etc/nix/netrc";
+      settings = {
+        trusted-users = ["root" "tomas"];
 
-      settings.substituters = [
-        "https://nix-gaming.cachix.org"
-        "https://nix-community.cachix.org"
-        "https://cache.nixos.org/"
-        "https://nix-cache.harke.ma/tomas/"
-        "https://devenv.cachix.org"
-        "https://tomasharkema.cachix.org"
-      ];
-
-      settings.trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "peerix-tomas-1:OBFTUNI1LIezxoFStcRyCHKi2PHExoIcZA0Mfq/4uJA="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "tomas:hER/5A08v05jH8GnQUZRrh33+HDNbeiJj8z/8JY6ZvI="
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "tomasharkema.cachix.org-1:BV3Sv3qGZ0bcybPFeigwKoxnpj/NBAFYHq9FMO1XgH4="
-      ];
-
-      binaryCaches = ["https://cache.nixos.org"];
+        substituters = [
+          "https://cache.nixos.org/"
+          "https://nix-gaming.cachix.org"
+          "https://nix-community.cachix.org"
+          "https://nix-cache.harke.ma/tomas/"
+          "https://devenv.cachix.org"
+        ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          "peerix-tomas-1:OBFTUNI1LIezxoFStcRyCHKi2PHExoIcZA0Mfq/4uJA="
+          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          "tomas:hER/5A08v05jH8GnQUZRrh33+HDNbeiJj8z/8JY6ZvI="
+        ];
+      }; # netrc-file = "/etc/nix/netrc";
 
       distributedBuilds = true;
 
