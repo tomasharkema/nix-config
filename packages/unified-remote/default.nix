80b1cd68f72aad1a1c5c8cef90895b97518504fb
Tomas Harkema <tomas@harkema.io>
2024-05-13 00:11:00 +0200

    first work with unified route

---
+++ packages/unified-remote/default.nix
@@ -0,0 +1,32 @@
+{
+  dpkg,
+  lib,
+  stdenv,
+  fetchurl,
+  autoPatchelfHook,
+  makeWrapper,
+}:
+stdenv.mkDerivation rec {
+  pname = "unified-remote";
+  version = "0.0.1";
+
+  src = fetchurl {
+    url = "https://www.unifiedremote.com/d/linux-x64-deb";
+    sha256 = "sha256-EHlJDzVH5YcKf6mcGZ85CPYkgFNFU2aCLq6APFcCaTI=";
+    downloadToTemp = true;
+    recursiveHash = true;
+    postFetch = ''
+      mkdir $out
+      cp file $out/urserver.deb
+    '';
+  };
+
+  dontConfigure = true;
+  dontBuild = true;
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    dpkg
+    makeWrapper
+  ];
+}
