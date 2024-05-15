c5080f22f7236907ec6c81c1afca594753b4dd90
Tomas Harkema <tomas@harkema.io>
2024-05-15 02:54:34 +0200

    fixes

--- packages/netbrowse/default.nix
+++ packages/netbrowse/default.nix
@@ -1,20 +1,30 @@
-{
-  rustPlatform,
-  fetchCrate,
-  xorg,
-  libGL,
-  makeWrapper,
+{ rustPlatform, fetchCrate, xorg, libGL, makeWrapper, makeDesktopItem, fetchurl,
 }:
-rustPlatform.buildRustPackage rec {
-  pname = "netbrowse";
-  version = "0.1.0";
 
-  src = fetchCrate {
-    inherit pname version;
-    hash = "sha256-dA8oDeQSPDPXPAgX6jPLLPWXxje+1L80zC8SVaQ2WTs=";
+let
+  icon = fetchurl {
+    url =
+      "https://upload.wikimedia.org/wikipedia/commons/8/80/Gnome-preferences-system-network.svg";
+    hash = "sha256-+MPq3I0F+YDPFfWd74B3pkpipy+BBbAqD+6g34Y1njg=";
+  };
+  desk = makeDesktopItem {
+
+    desktopName = "Netbrowse";
+    name = "Netbrowse";
+    exec = "netbrowse";
+    icon = icon;
+    categories = [ "System" ];
   };
 
-  cargoSha256 = "sha256-FzKDKm3TJNelDRUL3BJp8UJnjrCb++g1osXOT6X55ho=";
+in rustPlatform.buildRustPackage rec {
+  pname = "netbrowse";
+  version = "0.1.1";
+  src = fetchCrate {
+    inherit pname version;
+    hash = "sha256-wUznGsKAZrd6Ut/XpCNIMS9XA0mS1ORVouIpD2nX1JE=";
+  };
+
+  cargoSha256 = "sha256-M7IK9++EY87cv8poa2qFc5iAvMYd2NmPrR8SfH+nqNk=";
 
   buildInputs = [
     xorg.libxcb
@@ -27,8 +37,8 @@ rustPlatform.buildRustPackage rec {
   ];
 
   postInstall = ''
-    mkdir -p $out/share/applications
-    cp ${./netbrowse.desktop} $out/share/applications/netbrowse.desktop
+    # mkdir -p $out/share/applications
+    cp -r ${desk} $out
     wrapProgram $out/bin/netbrowse --prefix LD_LIBRARY_PATH : "${xorg.libX11.out}/lib:${xorg.libXcursor.out}/lib:${xorg.libXrandr.out}/lib:${xorg.libXi.out}/lib:${libGL}/lib"
   '';
 }
