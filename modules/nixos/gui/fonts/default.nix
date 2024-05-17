0a736aa6228c1408c21051ad3b23bac6ea7a8d9e
tomasharkema <tomas@harkema.io>
2024-05-18 01:34:08 +0200

    ojoo

--- modules/nixos/gui/fonts/default.nix
+++ modules/nixos/gui/fonts/default.nix
@@ -42,29 +42,33 @@ with lib; {
         ];
     };
 
-    # system.fsPackages = [pkgs.bindfs];
-    # fileSystems = let
-    #   mkRoSymBind = path: {
-    #     device = path;
-    #     fsType = "fuse.bindfs";
-    #     options = ["ro" "resolve-symlinks" "x-gvfs-hide"];
-    #   };
-    #   aggregatedIcons = pkgs.buildEnv {
-    #     name = "system-icons";
-    #     paths = with pkgs; [
-    #       #libsForQt5.breeze-qt5  # for plasma
-    #       gnome.gnome-themes-extra
-    #     ];
-    #     pathsToLink = ["/share/icons"];
-    #   };
-    #   aggregatedFonts = pkgs.buildEnv {
-    #     name = "system-fonts";
-    #     paths = config.fonts.packages;
-    #     pathsToLink = ["/share/fonts"];
-    #   };
-    # in {
-    #   "/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
-    #   "/usr/local/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
-    # };
+    system.fsPackages = [ pkgs.bindfs ];
+    fileSystems = let
+      mkRoSymBind = path: {
+        device = path;
+        fsType = "fuse.bindfs";
+        options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+      };
+      aggregatedIcons = pkgs.buildEnv {
+        name = "system-icons";
+        paths = with pkgs;
+          [
+            apple-cursor
+            catppuccin-papirus-folders
+
+            #libsForQt5.breeze-qt5  # for plasma
+            gnome.gnome-themes-extra
+          ] ++ config.fonts.packages;
+        pathsToLink = [ "/share/icons" ];
+      };
+      aggregatedFonts = pkgs.buildEnv {
+        name = "system-fonts";
+        paths = config.fonts.packages;
+        pathsToLink = [ "/share/fonts" ];
+      };
+    in {
+      "/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
+      "/usr/local/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
+    };
   };
 }
