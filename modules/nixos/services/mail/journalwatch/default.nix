fc93232f31d8b741c881852865d7b4c2ca8afa3f
tomasharkema <tomas@harkema.io>
2024-05-20 16:22:26 +0200

    fix journalwatch

--- modules/nixos/services/mail/journalwatch/default.nix
+++ modules/nixos/services/mail/journalwatch/default.nix
@@ -19,10 +19,10 @@
 
       # mailcatcher.enable = true;
 
-      # journalwatch = {
-      #   enable = true;
-      #   mailTo = "tomas";
-      # };
+      journalwatch = {
+        enable = true;
+        mailTo = "all@mailrise.xyz";
+      };
     };
   };
 }
