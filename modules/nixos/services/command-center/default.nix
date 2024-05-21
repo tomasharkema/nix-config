3bad7b6a014d032229afcc1895c54a8c47a3aff0
Tomas Harkema <tomas@harkema.io>
2024-05-21 12:20:50 +0200

    disable command center

--- modules/nixos/services/command-center/default.nix
+++ modules/nixos/services/command-center/default.nix
@@ -9,60 +9,60 @@ in {
     enableBot = mkBoolOpt false "";
   };
 
-  config = let
-    command-center-service = {
-      enable = true;
-      description = "command-center";
-      environment = { COMMAND_CENTER_RUN_BOT = mkIf cfg.enableBot "true"; };
-      unitConfig = { Type = "notify"; };
-      serviceConfig = {
-        RestartSec = 5;
-        EnvironmentFile = config.age.secrets."command-center.env".path;
-      };
-      script = "${lib.getExe command-center} -v";
-      wantedBy = [ "multi-user.target" ];
-    };
-  in mkIf cfg.enable {
-    systemd.services = { command-center = command-center-service; };
+  # config = let
+  #   command-center-service = {
+  #     enable = true;
+  #     description = "command-center";
+  #     environment = { COMMAND_CENTER_RUN_BOT = mkIf cfg.enableBot "true"; };
+  #     unitConfig = { Type = "notify"; };
+  #     serviceConfig = {
+  #       RestartSec = 5;
+  #       EnvironmentFile = config.age.secrets."command-center.env".path;
+  #     };
+  #     script = "${lib.getExe command-center} -v";
+  #     wantedBy = [ "multi-user.target" ];
+  #   };
+  # in mkIf cfg.enable {
+  #   systemd.services = { command-center = command-center-service; };
 
-    proxy-services.services = {
-      "/dashboard/" = { proxyPass = "http://localhost:3456/"; };
-    };
+  #   proxy-services.services = {
+  #     "/dashboard/" = { proxyPass = "http://localhost:3456/"; };
+  #   };
 
-    age.secrets."command-center.env" = {
-      file = ../../../../secrets/command-center.env.age;
-      mode = "644";
-    };
+  #   age.secrets."command-center.env" = {
+  #     file = ../../../../secrets/command-center.env.age;
+  #     mode = "644";
+  #   };
 
-    networking.nat = mkIf false {
-      enable = true;
-      internalInterfaces = [ "ve-+" ];
-      # externalInterface = "ens3";
-    };
+  #   networking.nat = mkIf false {
+  #     enable = true;
+  #     internalInterfaces = [ "ve-+" ];
+  #     # externalInterface = "ens3";
+  #   };
 
-    containers.go-nixos-menu = mkIf false {
-      autoStart = true;
-      privateNetwork = true;
-      hostAddress = "192.168.100.10";
-      localAddress = "192.168.100.11";
+  #   containers.go-nixos-menu = mkIf false {
+  #     autoStart = true;
+  #     privateNetwork = true;
+  #     hostAddress = "192.168.100.10";
+  #     localAddress = "192.168.100.11";
 
-      config = { config, pkgs, ... }: {
-        systemd.services = { };
+  #     config = { config, pkgs, ... }: {
+  #       systemd.services = { };
 
-        system.stateVersion = "23.11";
+  #       system.stateVersion = "23.11";
 
-        networking = {
-          firewall = {
-            enable = false;
-            allowedTCPPorts = [ 3456 ];
-          };
-          # Use systemd-resolved inside the container
-          # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-          useHostResolvConf = mkForce false;
-        };
+  #       networking = {
+  #         firewall = {
+  #           enable = false;
+  #           allowedTCPPorts = [ 3456 ];
+  #         };
+  #         # Use systemd-resolved inside the container
+  #         # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+  #         useHostResolvConf = mkForce false;
+  #       };
 
-        services.resolved.enable = true;
-      };
-    };
-  };
+  #       services.resolved.enable = true;
+  #     };
+  #   };
+  # };
 }
