92d6987a0f9872e58a0b14b09df2a1b69cc43e46
Tomas Harkema <tomas@harkema.io>
2024-05-22 23:48:26 +0200

    add usb over ethernet

--- packages/unified-remote/default.nix
+++ packages/unified-remote/default.nix
@@ -1,27 +1,18 @@
-{
-  dpkg,
-  lib,
-  stdenv,
-  fetchurl,
-  autoPatchelfHook,
-  makeWrapper,autoreconfHook, pkg-config,
-     gcc,
-  libgcc,
-  libcxx,
-}:
+{ dpkg, lib, stdenv, fetchurl, autoPatchelfHook, makeWrapper, autoreconfHook
+, pkg-config, gcc, libgcc, libcxx, }:
 stdenv.mkDerivation rec {
   pname = "unified-remote";
   version = "0.0.1";
 
   src = fetchurl {
-    url = if stdenv.isx86_64 then 
-      "https://www.unifiedremote.com/d/linux-x64-deb" 
-      else 
+    url = if stdenv.isx86_64 then
+      "https://www.unifiedremote.com/d/linux-x64-deb"
+    else
       "https://www.unifiedremote.com/download/linux-arm64-deb";
-      #"https://www.unifiedremote.com/download/linux-arm64-portable";
-    sha256 = if stdenv.isx86_64 then 
+    #"https://www.unifiedremote.com/download/linux-arm64-portable";
+    sha256 = if stdenv.isx86_64 then
       "WIWW7SQll7Xp8Jwk+QDucqM6TS9I/W9K+18+SDAK9Cs="
-      else 
+    else
       "kokM7rqXZzLvwhEezD0qcsdivjFFMzp0PhFtwKip1vM=";
 
     #downloadToTemp = true;
@@ -32,26 +23,19 @@ stdenv.mkDerivation rec {
     #'';
   };
 
-	unpackPhase = ''
-    cp $src unified-remote.deb
-    dpkg -x unified-remote.deb unpacked
-    mkdir -p $out
-		cp -r unpacked/* $out/
-    cp -r $out/opt/urserver/ $out/bin/
-    mkdir -p $out/share/applications/
-    cp $out/opt/urserver/urserver-autostart.desktop $out/share/applications/
-	'';
+  unpackPhase = ''
+        cp $src unified-remote.deb
+        dpkg -x unified-remote.deb unpacked
+        mkdir -p $out
+    		cp -r unpacked/* $out/
+        cp -r $out/opt/urserver/ $out/bin/
+        mkdir -p $out/share/applications/
+        cp $out/opt/urserver/urserver-autostart.desktop $out/share/applications/
+    	'';
 
   dontConfigure = true;
   dontBuild = true;
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-    dpkg
-    makeWrapper
- pkg-config
-   gcc
-  libgcc
-  libcxx
-  ];
+  nativeBuildInputs =
+    [ autoPatchelfHook dpkg makeWrapper pkg-config gcc libgcc libcxx ];
 }
