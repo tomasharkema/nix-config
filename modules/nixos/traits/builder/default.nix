31682046e4ccc8151ccd66a38ceb6d12c4e9a45d
Tomas Harkema <tomas@harkema.io>
2024-05-24 01:51:20 +0200

    fix

--- modules/nixos/traits/builder/default.nix
+++ modules/nixos/traits/builder/default.nix
@@ -7,24 +7,26 @@ in {
     builder = { enable = mkBoolOpt false "SnowflakeOS GNOME configuration"; };
   };
 
-  config = let
-    github-default = {
-      enable = true;
-      tokenFile = config.age.secrets.ght-runner.path;
-      url = "https://github.com/tomasharkema/nix-config";
-      ephemeral = true;
-    };
-  in mkIf cfg.enable {
-    age.secrets.ght = {
-      file = ../../../../secrets/ght.age;
-      mode = "0664";
-    };
-    age.secrets."ght-runner" = {
-      file = ../../../../secrets/ght-runner.age;
-      mode = "0664";
-    };
+  config =
+    # let
+    #   github-default = {
+    #     enable = true;
+    #     tokenFile = config.age.secrets.ght-runner.path;
+    #     url = "https://github.com/tomasharkema/nix-config";
+    #     ephemeral = true;
+    #   };
+    # in
+    mkIf cfg.enable {
+      # age.secrets.ght = {
+      #   file = "${../../secrets/ght.age}";
+      #   mode = "0664";
+      # };
+      # age.secrets."ght-runner" = {
+      #   file = ../../../secrets/ght-runner.age;
+      #   mode = "0664";
+      # };
 
-    # services.github-runners."${config.networking.hostName}-runner-1" = github-default;
-    # services.github-runners."${config.networking.hostName}-runner-2" = github-default;
-  };
+      # services.github-runners."${config.networking.hostName}-runner-1" = github-default;
+      # services.github-runners."${config.networking.hostName}-runner-2" = github-default;
+    };
 }
