360bb659852991dac918de884092fc3bfa81d1ae
Tomas Harkema <tomas@harkema.io>
2024-05-27 00:14:34 +0200

    fix hm

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "nixpkgs/nixos-24.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
