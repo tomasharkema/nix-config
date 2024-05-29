d950d54fc0d987c98c8ac3e5c9571ec84eb4c964
Tomas Harkema <tomas@harkema.io>
2024-05-29 23:21:24 +0200

    update

--- overlays/gnome/default.nix
+++ overlays/gnome/default.nix
@@ -1,14 +1,14 @@
 { channels, self, ... }:
 final: prev: {
-  gnome = prev.gnome.overrideScope' (gnomeFinal: gnomePrev: {
-    mutter = gnomePrev.mutter.overrideAttrs (old: {
-      src = fetchTarball {
-        url =
-          "https://gitlab.gnome.org/vanvugt/mutter/-/archive/triple-buffering-v4-46/mutter-triple-buffering-v4-46.tar.gz";
-        sha256 = "sha256:1fqss0837k3sc7hdixcgy6w1j73jdc57cglqxdc644a97v5khnr3";
-      };
-    });
-  });
+  # gnome = prev.gnome.overrideScope' (gnomeFinal: gnomePrev: {
+  #   mutter = gnomePrev.mutter.overrideAttrs (old: {
+  #     src = fetchTarball {
+  #       url =
+  #         "https://gitlab.gnome.org/vanvugt/mutter/-/archive/triple-buffering-v4-46/mutter-triple-buffering-v4-46.tar.gz";
+  #       sha256 = "sha256:1fqss0837k3sc7hdixcgy6w1j73jdc57cglqxdc644a97v5khnr3";
+  #     };
+  #   });
+  # });
   # dconf = channels.unstable.dconf;
   # flatpak = channels.unstable.flatpak;
 
