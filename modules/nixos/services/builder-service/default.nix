5cfa9be6721426aa692d2bba0206e361827c3f93
Tomas Harkema <tomas@harkema.io>
2024-06-04 23:23:04 +0200

    update

--- modules/nixos/services/builder-service/default.nix
+++ modules/nixos/services/builder-service/default.nix
@@ -1,19 +1,12 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ config, pkgs, lib, ... }:
 with lib;
-let
-  cfg = config.services.builder-service;
-in
-{
+let cfg = config.services.builder-service;
+in {
   options.services.builder-service = {
     enable = mkEnableOption "builder-service";
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf (cfg.enable && false) {
     age.secrets."hercules-cli.key" = {
       file = ../../secrets/hercules-cli.key.age;
       # mode = "644";
@@ -25,15 +18,15 @@ in
 
       settings = {
         clusterJoinTokenPath = config.age.secrets."hercules-cli.key".path;
-        binaryCachesPath = pkgs.writeText "binary-caches.json" (
-          builtins.toJSON {
+        binaryCachesPath = pkgs.writeText "binary-caches.json"
+          (builtins.toJSON {
             tomas = {
               kind = "NixCache";
               storeURI = "htpps://nix-cache.harke.ma/tomas/";
-              publicKeys = [ "tomas:hER/5A08v05jH8GnQUZRrh33+HDNbeiJj8z/8JY6ZvI=" ];
+              publicKeys =
+                [ "tomas:hER/5A08v05jH8GnQUZRrh33+HDNbeiJj8z/8JY6ZvI=" ];
             };
-          }
-        );
+          });
       };
     };
   };
