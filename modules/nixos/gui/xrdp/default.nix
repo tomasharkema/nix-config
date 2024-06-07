73523d1dfb198535868199f68dda2c987a589411
Tomas Harkema <tomas@harkema.io>
2024-06-07 12:36:32 +0200

    fixes

--- modules/nixos/gui/xrdp/default.nix
+++ modules/nixos/gui/xrdp/default.nix
@@ -13,8 +13,8 @@ with lib; {
           substituteInPlace $out/etc/xrdp/xrdp.ini \
             --replace "port=3389" "port=tcp://0.0.0.0:3389" \
             --replace "security_layer=negotiate" "security_layer=rdp" \
-            --replace "crypt_level=high" "crypt_level=none" \
-            --replace "bitmap_compression=true" "bitmap_compression=false"
+            --replace "crypt_level=high" "crypt_level=none" #\
+            #--replace "bitmap_compression=true" "bitmap_compression=false"
         '';
         # substituteInPlace $out/etc/xrdp/sesman.ini \
         #   --replace "X11DisplayOffset=10" "X11DisplayOffset=0" 
