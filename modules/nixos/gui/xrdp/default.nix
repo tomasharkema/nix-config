b995251fd6a42394a2441dd7573bcc83fdd673fb
Tomas Harkema <tomas@harkema.io>
2024-06-13 13:25:27 +0200

    update

--- modules/nixos/gui/xrdp/default.nix
+++ modules/nixos/gui/xrdp/default.nix
@@ -2,24 +2,23 @@
 with lib; {
   config = mkIf config.gui.enable {
     services.xrdp = {
-      enable = true;
 
-      # confDir = "/etc/xrdp";
+      enable = true;
+      audio.enable = true;
+
+      extraConfDirCommands = ''
+        substituteInPlace $out/xrdp.ini \
+          --replace "use_vsock=false" "use_vsock=true" \
+          --replace "port=3389" "port=tcp://0.0.0.0:3389" \
+          --replace "security_layer=negotiate" "security_layer=rdp" \
+          --replace "crypt_level=high" "crypt_level=none"
+
+        # substituteInPlace $out/sesman.ini \
+        #   --replace "X11DisplayOffset=10" "X11DisplayOffset=0" 
+      '';
 
       package = pkgs.xrdp.overrideAttrs (oldAttrs: {
         configureFlags = oldAttrs.configureFlags ++ [ "--enable-vsock" ];
-
-        postInstall = oldAttrs.postInstall + ''
-          substituteInPlace $out/etc/xrdp/xrdp.ini \
-            --replace "use_vsock=false" "use_vsock=true" \
-            --replace "port=3389" "port=tcp://0.0.0.0:3389" \
-            --replace "security_layer=negotiate" "security_layer=rdp" \
-            --replace "crypt_level=high" "crypt_level=none"
-        '';
-        # substituteInPlace $out/etc/xrdp/sesman.ini \
-        #   --replace "X11DisplayOffset=10" "X11DisplayOffset=0" 
-
-        # '';
       });
     };
 
@@ -27,8 +26,6 @@ with lib; {
       serviceConfig = {
         ExecStart = lib.mkForce
           "${pkgs.xrdp}/bin/xrdp --nodaemon --config ${config.services.xrdp.confDir}/xrdp.ini";
-        #     # https://github.com/NixOS/nixpkgs/blob/nixos-23.11/nixos/modules/services/networking/xrdp.nix#L158
-        #     # seems the integer port results in ipv6 only
       };
     };
   };
