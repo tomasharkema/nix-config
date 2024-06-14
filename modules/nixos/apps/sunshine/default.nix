c828427f51c65e8ae0847bf779d75af6297eed8c
Tomas Harkema <tomas@harkema.io>
2024-06-14 03:51:27 +0200

    ojoo

--- modules/nixos/apps/sunshine/default.nix
+++ modules/nixos/apps/sunshine/default.nix
@@ -53,34 +53,34 @@ in {
       };
     };
 
-    services.xserver = {
-      # Dummy screen
-      monitorSection = ''
-        VendorName     "Unknown"
-        HorizSync   30-85
-        VertRefresh 48-120
+    # services.xserver = {
+    #   # Dummy screen
+    #   monitorSection = ''
+    #     VendorName     "Unknown"
+    #     HorizSync   30-85
+    #     VertRefresh 48-120
 
-        ModelName      "Unknown"
-        Option         "DPMS"
-      '';
+    #     ModelName      "Unknown"
+    #     Option         "DPMS"
+    #   '';
 
-      deviceSection = ''
-        VendorName "NVIDIA Corporation"
-        Option      "AllowEmptyInitialConfiguration"
-        Option      "ConnectedMonitor" "DFP"
-        Option      "CustomEDID" "DFP-0"
+    #   deviceSection = ''
+    #     VendorName "NVIDIA Corporation"
+    #     Option      "AllowEmptyInitialConfiguration"
+    #     Option      "ConnectedMonitor" "DFP"
+    #     Option      "CustomEDID" "DFP-0"
 
-      '';
+    #   '';
 
-      screenSection = ''
-        DefaultDepth    24
-        Option      "ModeValidation" "AllowNonEdidModes, NoVesaModes"
-        Option      "MetaModes" "1920x1080"
-        SubSection     "Display"
-            Depth       24
-        EndSubSection
-      '';
-    };
+    #   screenSection = ''
+    #     DefaultDepth    24
+    #     Option      "ModeValidation" "AllowNonEdidModes, NoVesaModes"
+    #     Option      "MetaModes" "1920x1080"
+    #     SubSection     "Display"
+    #         Depth       24
+    #     EndSubSection
+    #   '';
+    # };
 
     # Required to simulate input
     boot.kernelModules = [ "uinput" ];
