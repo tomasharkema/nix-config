f591e1e23356f0b7e0cabc6486c0c73d83ae837b
tomas <tomas@harkema.io>
2024-06-23 15:53:40 +0200

    ojoo

--- systems/x86_64-linux/enzian/default.nix
+++ systems/x86_64-linux/enzian/default.nix
@@ -104,18 +104,21 @@ with lib; {
     #   targetUser = "root";
     # };
 
-    # services.factorio = {
-    #   enable = true;
-    #   lan = true;
-    # };
+    services = {
+      factorio = {
+        enable = true;
+        lan = true;
+        requireUserVerification = false;
+      };
 
-    services.pufferpanel = {
-      enable = true;
-      extraPackages = with pkgs; [bash curl gawk gnutar gzip];
-      package = pkgs.buildFHSEnv {
-        name = "pufferpanel-fhs";
-        runScript = lib.getExe pkgs.pufferpanel;
-        targetPkgs = pkgs': with pkgs'; [icu openssl zlib factorio-headless];
+      pufferpanel = {
+        enable = true;
+        extraPackages = with pkgs; [bash curl gawk gnutar gzip];
+        package = pkgs.buildFHSEnv {
+          name = "pufferpanel-fhs";
+          runScript = lib.getExe pkgs.pufferpanel;
+          targetPkgs = pkgs': with pkgs'; [icu openssl zlib factorio-headless];
+        };
       };
     };
 
