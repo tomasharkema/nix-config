9c7b57b475acdc9bed39f545cc4ac419e1f3bd86
Tomas Harkema <tomas@harkema.io>
2024-06-27 01:53:19 +0200

    updates

--- modules/home/packages/catppuccin/default.nix
+++ modules/home/packages/catppuccin/default.nix
@@ -16,15 +16,17 @@ with lib; {
       accent = "blue";
     };
 
-    gtk = mkIf config.gtk.enable {
-      catppuccin = {
-        enable = true;
-        flavor = "mocha";
-        accent = "blue";
-        size = "compact";
-        tweaks = ["black"];
-        gnomeShellTheme = true;
-      };
-    };
+    home.pointerCursor = mkIf pkgs.stdenv.isDarwin (mkForce null);
+
+    # gtk = mkIf config.gtk.enable {
+    #   catppuccin = {
+    #     enable = true;
+    #     flavor = "mocha";
+    #     accent = "blue";
+    #     size = "compact";
+    #     tweaks = ["black"];
+    #     gnomeShellTheme = true;
+    #   };
+    # };
   };
 }
