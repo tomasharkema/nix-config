66ff6cc11f332a07e7f9698b577a3b8c9b0f335d
Tomas Harkema <tomas@harkema.io>
2024-07-04 17:05:42 +0200

    upd

--- modules/home/packages/conky/conky.lua
+++ modules/home/packages/conky/conky.lua
@@ -58,17 +58,17 @@ conky.config = {
 conky.text = [[
 ${color lightgrey}$color$nodename ${color lightgrey}${addr}
 $hr
-${color grey}Info:$color $sysname $nodename $kernel $machine
+${color grey}$color $sysname $nodename $kernel $machine
 $hr
-${color grey}Uptime:$color0 $uptime
-${color grey}Frequency:$color0 $freq_g
-${color grey}RAM Usage:$color0 $mem/$memmax $color${membar 4}
-${color grey}Swap Usage:$color0 $swap/$swapmax $color${swapbar 4}
-${color grey}CPU Usage:$color0 $cpu% $color${cpubar 4}
-${color grey}Processes:$color0 $processes  ${color grey}Running:$color0 $running_processes
+${color grey}UPT$color0 $uptime
+${color grey}FRQ$color0 $freq_g
+${color grey}RAM$color0 $mem/$memmax $color${membar 4}
+${color grey}SWP$color0 $swap/$swapmax $color${swapbar 4}
+${color grey}CPU$color0 $cpu% $color${cpubar 4}
+${color grey}PRC$color0 $processes  ${color grey}Running:$color0 $running_processes
 $color$hr
-${color grey}File systems: $color0${fs_used /}/${fs_size /} $color${fs_bar 6 /}
-${color grey}Networking: Up:$color0 ${upspeed} ${color grey} - Down:$color0 ${downspeed}
+${color grey}FSS: $color0${fs_used /}/${fs_size /} $color${fs_bar 6 /}
+${color grey}NET UP$color0 ${upspeed} ${color grey} - DWN$color0 ${downspeed}
 ]]
 
 -- $hr
