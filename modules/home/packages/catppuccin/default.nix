3bded99bbbb3bc2d98ac033baa537f82baaf820c
Tomas Harkema <tomas@harkema.io>
2024-07-04 18:37:29 +0200

    undo flat

--- modules/home/packages/catppuccin/default.nix
+++ modules/home/packages/catppuccin/default.nix
@@ -19,14 +19,15 @@ with lib; {
     home.pointerCursor = mkIf pkgs.stdenv.isDarwin (mkForce null);
 
     gtk = mkIf config.gtk.enable {
-      # catppuccin = {
-      #   enable = true;
-      #   flavor = "mocha";
-      #   accent = "blue";
-      #   size = "compact";
-      #   tweaks = ["black"];
-      #   gnomeShellTheme = true;
-      # };
+      catppuccin = {
+        enable = true;
+        flavor = "mocha";
+        accent = "blue";
+        size = "compact";
+        tweaks = ["black"];
+        gnomeShellTheme = true;
+        icon.enable = true;
+      };
     };
   };
 }
