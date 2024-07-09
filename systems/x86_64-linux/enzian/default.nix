75bc9bd9d03e1b0f48d35bee529892e868278a5a
tomasharkema <tomas@harkema.io>
2024-07-09 14:14:52 +0200

    update

--- systems/x86_64-linux/enzian/default.nix
+++ systems/x86_64-linux/enzian/default.nix
@@ -119,7 +119,6 @@ with lib; {
     boot = {
       binfmt.emulatedSystems = ["aarch64-linux"];
 
-      kernel.sysctl."kernel.sysrq" = 1;
       initrd = {
         availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
         kernelModules = ["kvm-intel" "uinput" "nvme"];
