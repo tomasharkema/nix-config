94c7d607d0efa2d1951ff4d2a647927898220832
tomasharkema <tomas@harkema.io>
2024-07-09 20:36:14 +0200

    update authorized

--- packages/authorized-keys/default.nix
+++ packages/authorized-keys/default.nix
@@ -1,11 +1,15 @@
-{ fetchurl, lib, stdenvNoCC, }:
+{
+  fetchurl,
+  lib,
+  stdenvNoCC,
+}:
 stdenvNoCC.mkDerivation rec {
   pname = "authorized-keys";
-  version = "0.0.3";
+  version = "0.0.4";
 
   src = fetchurl {
     url = "https://github.com/tomasharkema.keys";
-    sha256 = "0cbbbcy0qqshp82hx67k8g7cpcjhq7k5mk0095jjf58b71h18z1x";
+    sha256 = "sha256-2aiRs0VDBe2Ksn7uPHSM2SnHOkFgnq2SDo1nOfySCpU=";
   };
 
   dontUnpack = true;
