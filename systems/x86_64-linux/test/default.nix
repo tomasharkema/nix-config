f2e53f678fab60825401e04a6c35bc00b821dbaf
Tomas Harkema <tomas@harkema.io>
2024-07-13 19:37:11 +0200

    rekey

--- systems/x86_64-linux/test/default.nix
+++ systems/x86_64-linux/test/default.nix
@@ -8,7 +8,7 @@
 with lib; {
   config = {
     age.rekey = {
-      hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK0wmN/Cr3JXqmLW7u+g9pTh+wyqDHpSQEIQczXkVx9q root@test";
+      hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDC7tNpEvVh4rd5ChYh2uTRK/cMRes21NW8HiZQc0vo5 root@test";
     };
   };
 }
