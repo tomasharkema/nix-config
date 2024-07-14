17757529ac6d39f916300f887fa57a6378633f8e
Tomas Harkema <tomas@harkema.io>
2024-07-14 23:57:22 +0200

    fix entoll

--- modules/nixos/traits/hardware/secure-boot/default.nix
+++ modules/nixos/traits/hardware/secure-boot/default.nix
@@ -19,7 +19,7 @@ in {
     boot = {
       lanzaboote = {
         enable = true;
-        enrollKeys = true;
+        # enrollKeys = true;
         pkiBundle = "/etc/secureboot";
         # settings = {
         #   options = "vt.default_red=30,243,166,249,137,245,148,186,88,243,166,249,137,245,148,166 vt.default_grn=30,139,227,226,180,194,226,194,91,139,227,226,180,194,226,173 vt.default_blu=46,168,161,175,250,231,213,222,112,168,161,175,250,231,213,200";
