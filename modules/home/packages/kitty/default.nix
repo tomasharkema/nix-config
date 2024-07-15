db405ba5a9b664668ead4fee95fccde677c436a9
Tomas Harkema <tomas@harkema.io>
2024-07-15 17:26:14 +0200

    fix font for kitty

--- modules/home/packages/kitty/default.nix
+++ modules/home/packages/kitty/default.nix
@@ -1,19 +1,25 @@
 # background              #1e1e2e
-{ config, osConfig, ... }: {
+{
+  config,
+  osConfig,
+  ...
+}: {
   config = {
     programs = {
       kitty = {
         enable = true;
         theme = "Catppuccin-Mocha";
 
+        font = {name = "JetBrainsMono Nerd Font Mono";};
+
         settings = {
-          font_family = "JetBrainsMono Nerd Font Mono Regular";
-          font_size = "13.0";
+          # font_family = "JetBrainsMono Nerd Font Mono Regular";
+          # font_size = "13.0";
           background = "#000000";
           #osConfig.variables.theme.withHashtag.base00; #"#000000";
         };
 
-        shellIntegration = { enableZshIntegration = true; };
+        shellIntegration = {enableZshIntegration = true;};
       };
     };
   };
