a6030ea31c773d9d2d56779a5894315237aaa912
Tomas Harkema <tomas@harkema.io>
2024-07-16 00:39:33 +0200

    oops

--- modules/nixos/apps/btrbk/default.nix
+++ modules/nixos/apps/btrbk/default.nix
@@ -32,7 +32,6 @@ with lib; {
           raw_target_split = "100M";
 
           transaction_syslog = "daemon";
-          lockfile = "/run/btrbk.lock";
 
           ssh_identity = "${config.age.secrets.btrbk.path}";
           stream_buffer = "100M";
@@ -48,6 +47,8 @@ with lib; {
                 #   snapshot_name = "${config.networking.hostName}-rootfs";
                 # };
               };
+
+              # target = "/mnt/servers/nfs/silver-star-backup/btrbk/${config.networking.hostName}";
 
               target = {
                 "raw ssh://silver-star/mnt/user0/backup/btrbk/${config.networking.hostName}" = {
