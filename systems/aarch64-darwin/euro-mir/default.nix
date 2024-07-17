eaf1ceb0be6c10e43d5c5f664669a5363e63042b
Tomas Harkema <tomas@harkema.io>
2024-07-17 21:00:33 +0200

    fixes

--- systems/aarch64-darwin/euro-mir/default.nix
+++ systems/aarch64-darwin/euro-mir/default.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
   config = {
-    age.rekey = {hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIICb4X+GYjcvvpjiRPhNdyqMyda6kFTkD9N4ZB2oOsEQ root@euro-mir-2";};
+    age.rekey = {hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILgD7me/mlDG89ZE/tLTJeNhbo3L+pi7eahB2rUneSR4";};
   };
 }
