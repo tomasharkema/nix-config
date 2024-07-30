7d67af48182cee832e508fb7e86bcc59cf229b05
tomasharkema <tomas@harkema.io>
2024-07-30 06:34:15 +0200

    remove and fix key

--- modules/nixos/apps/shellhub/default.nix
+++ modules/nixos/apps/shellhub/default.nix
@@ -6,8 +6,6 @@
 }:
 with lib; {
   config = {
-    # curl -sSf "https://cloud.shellhub.io/install.sh?tenant_id=2d2799eb-43c0-4478-b080-ddcf22f49a29" | sh
-
     age.secrets.shellhub.rekeyFile = ./shellhub.age;
 
     services.shellhub-agent = {
