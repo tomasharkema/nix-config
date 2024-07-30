220b516c3643ef452ab0ea46175eeae6f3546531
tomasharkema <tomas@harkema.io>
2024-07-30 07:03:49 +0200

    fixes

--- modules/nixos/apps/shellhub/default.nix
+++ modules/nixos/apps/shellhub/default.nix
@@ -15,6 +15,11 @@ with lib; {
       enable = true;
       tenantId = "2d2799eb-43c0-4478-b080-ddcf22f49a29";
       privateKey = config.age.secrets.shellhub.path;
+      preferredHostname = config.networking.hostName;
     };
+
+    systemd.tmpfiles.rules = [
+      "L+  /var/lib/shellhub-agent/private.key.pub  -  -  -  -  ${pkgs.writeText "private.key.pub" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCswh/1hPkXtoqsdkYvqf2KYm3WcMvflpc7F1jQPpo2 tomas@voltron"}"
+    ];
   };
 }
