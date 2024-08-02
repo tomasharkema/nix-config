90e2adcf19e16f341583a03622c04d60e69f61d3
tomasharkema <tomas@harkema.io>
2024-08-02 21:51:23 +0200

    update

--- modules/nixos/gui/desktop/default.nix
+++ modules/nixos/gui/desktop/default.nix
@@ -102,82 +102,75 @@ in {
 
     programs.virt-manager.enable = true;
 
-    environment.systemPackages = with pkgs;
-      [
-        fractal
-        ventoy-full
-        unstable.zed-editor
-        spot
-        ulauncher
-        yelp
-        # firefox
-        # gnome.gnome-boxes
-        # keybase
-        # libmx
-        # mattermost-desktop
-        # nixos-conf-editor
-        # pcmanfm
-        # plymouth
-        # polkit
-        # transmission
-        # waybar
-        # caffeine-ng
-        unstable.ktailctl
-        clutter
-        effitask
-        filezilla
-        font-manager
-        gamehub
-        github-desktop
-        gotop
-        gparted
-        grsync
-        gtk-engine-murrine
-        libGL
-        libGLU
-        meteo
-        mission-center
-        nix-software-center
-        # notify-client
-        partition-manager
-        pavucontrol
-        pkgs.custom.netbrowse
-        powertop
-        pwvucontrol
-        qdirstat
-        qjournalctl
-        rtfm
-        sublime-merge
-        unstable.systemdgenie
-        unstable.transmission-remote-gtk
-        unstable.trayscale
-        unstable.tremotesf
-        vsce
-        unstable.vscode
-        # unstable.vscodium
-        vte-gtk4
-        wezterm
-        xdg-utils
-        xdgmenumaker
-        xdiskusage
-        xdotool
-      ]
-      ++ optionals pkgs.stdenv.isx86_64 [
-        devdocs-desktop
-        discord
-        # spotify
-        ipmiview
-        jetbrains-toolbox
-        gitkraken
-        unstable.xpipe
-        angryipscanner
-        telegram-desktop
-        # pkgs.custom.git-butler
-        unstable.bottles
-
-        handbrake
-      ]
-      ++ (with pkgs.custom; [zerotier-ui]);
+    environment.systemPackages =
+      (with pkgs; [nix-software-center])
+      ++ (with pkgs.unstable;
+        [
+          sqlitebrowser
+          # notify-client
+          # unstable.vscodium
+          clutter
+          effitask
+          filezilla
+          font-manager
+          fractal
+          gamehub
+          github-desktop
+          gotop
+          gparted
+          grsync
+          gtk-engine-murrine
+          ktailctl
+          libGL
+          libGLU
+          meteo
+          mission-center
+          partition-manager
+          pavucontrol
+          powertop
+          pwvucontrol
+          qdirstat
+          qjournalctl
+          rtfm
+          spot
+          sublime-merge
+          systemdgenie
+          transmission-remote-gtk
+          trayscale
+          tremotesf
+          ulauncher
+          ventoy-full
+          vsce
+          vscode
+          vte-gtk4
+          wezterm
+          xdg-utils
+          xdgmenumaker
+          xdiskusage
+          xdotool
+          yelp
+          zed-editor
+        ]
+        ++ optionals pkgs.stdenv.isx86_64 [
+          # pkgs.custom.git-butler
+          # pkgs.wolfram-engine
+          # spotify
+          angryipscanner
+          bottles
+          devdocs-desktop
+          discord
+          dmidecode
+          gitkraken
+          handbrake
+          ipmiview
+          jetbrains-toolbox
+          libsmbios
+          plex-media-player
+          plexamp
+          telegram-desktop
+          xpipe
+        ]
+        ++ (with pkgs.custom; [zerotier-ui netbrowse ztui]));
 
     programs = {
       ssh = {
