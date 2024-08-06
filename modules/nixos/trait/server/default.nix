8be23e47cc8b96a08108bcf13c912ec184a05095
tomasharkema <tomas@harkema.io>
2024-08-06 18:27:28 +0200

    fixes

--- modules/nixos/trait/server/default.nix
+++ modules/nixos/trait/server/default.nix
@@ -9,14 +9,22 @@ with lib; let
 in {
   options.trait.server = {
     enable = mkEnableOption "server";
+
+    headless.enable = mkEnableOption "server headless";
   };
 
   config = mkIf cfg.enable {
     system.nixos.tags = ["server"];
     services = {
       hypervisor = {
+        enable = true;
         webservices.enable = true;
       };
+    };
+
+    virtualisation.virtualbox.host = {
+      headless = true;
+      enableWebService = true;
     };
 
     apps.podman.enable = true;
