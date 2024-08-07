4b05f4c1fc5bf8781aab217f1b7d32b452fc11f6
tomasharkema <tomas@harkema.io>
2024-08-07 17:07:37 +0200

    battery

--- modules/nixos/trait/hardware/laptop/default.nix
+++ modules/nixos/trait/hardware/laptop/default.nix
@@ -29,14 +29,44 @@ in {
       extraModulePackages = with config.boot.kernelPackages; [acpi_call];
     };
 
-    systemd.services."beesd@root".unitConfig.ConditionACPower = true;
-    systemd.services."nh-clean".unitConfig.ConditionACPower = true;
-    systemd.services."nix-optimise".unitConfig.ConditionACPower = true;
+    systemd.services."beesd@root" = {
+      unitConfig = {
+        ConditionACPower = true;
+      };
+      wantedBy = ["acpower.target"];
+      partOf = ["acpower.target"];
+    };
+    systemd.services."nh-clean" = {
+      unitConfig = {
+        ConditionACPower = true;
+      };
+      wantedBy = ["acpower.target"];
+      partOf = ["acpower.target"];
+    };
+    systemd.services."nix-optimise" = {
+      unitConfig = {
+        ConditionACPower = true;
+      };
+      wantedBy = ["acpower.target"];
+      partOf = ["acpower.target"];
+    };
+
+    boot.kernelParams = [
+      "ahci.mobile_lpm_policy=3"
+      "rtc_cmos.use_acpi_alarm=1"
+    ];
 
     services = {
       # synergy.server = {
       #   enable = true;
       # };
+
+      udev.extraRules = ''
+        SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="0", RUN+="${pkgs.systemd}/bin/systemctl start battery.target"
+        SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="1", RUN+="${pkgs.systemd}/bin/systemctl stop battery.target"
+        SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="1", RUN+="${pkgs.systemd}/bin/systemctl start acpower.target"
+        SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="0", RUN+="${pkgs.systemd}/bin/systemctl stop acpower.target"
+      '';
 
       thermald.enable = true;
 
@@ -68,6 +98,20 @@ in {
       suspend.enable = true;
       hibernate.enable = true;
       hybrid-sleep.enable = true;
+      battery = {
+        enable = true;
+        unitConfig = {
+          DefaultDependencies = "no";
+          StopWhenUnneeded = "yes";
+        };
+      };
+      acpower = {
+        enable = true;
+        unitConfig = {
+          DefaultDependencies = "no";
+          StopWhenUnneeded = "yes";
+        };
+      };
     };
   };
 }
