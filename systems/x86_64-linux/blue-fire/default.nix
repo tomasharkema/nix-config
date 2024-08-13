c9e1f2db132aa7dedf1d569e94e4283946d1a9d8
Tomas Harkema <tomas@harkema.io>
2024-08-13 18:17:04 +0200

    add kernelmodues

--- systems/x86_64-linux/blue-fire/default.nix
+++ systems/x86_64-linux/blue-fire/default.nix
@@ -402,6 +402,15 @@ in {
           "ipmi_si"
           "ipmi_devintf"
           "ipmi_msghandler"
+          "vfio_pci"
+          "vfio"
+          "vfio_iommu_type1"
+          "vfio_virqfd"
+
+          "nvidia"
+          "nvidia_modeset"
+          "nvidia_uvm"
+          "nvidia_drm"
         ];
       };
       kernelModules = [
