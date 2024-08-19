e6d9ecb4eb3376e2cf9199fe8ee65857fa51d16b
Tomas Harkema <tomas@harkema.io>
2024-08-19 22:44:28 +0200

    fix sha

--- modules/nixos/trait/hardware/nvidia/default.nix
+++ modules/nixos/trait/hardware/nvidia/default.nix
@@ -107,7 +107,7 @@ in {
           version = "560.31.02";
           sha256_64bit = "sha256-0cwgejoFsefl2M6jdWZC+CKc58CqOXDjSi4saVPNKY0=";
           sha256_aarch64 = "";
-          openSha256 = "";
+          openSha256 = "sha256-X5UzbIkILvo0QZlsTl9PisosgPj/XRmuuMH+cDohdZQ=";
           settingsSha256 = "sha256-A3SzGAW4vR2uxT1Cv+Pn+Sbm9lLF5a/DGzlnPhxVvmE=";
           persistencedSha256 = "";
         };
