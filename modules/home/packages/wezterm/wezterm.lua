d9e8e7396b751a50db2abead5ea3280683405b32
Tomas Harkema <tomas@harkema.io>
2024-09-02 02:30:10 +0200

    fix

--- modules/home/packages/wezterm/wezterm.lua
+++ modules/home/packages/wezterm/wezterm.lua
@@ -9,18 +9,21 @@ config.color_scheme = "Catppuccin Mocha"
 -- config.font = wezterm.font_with_fallback {
 --   {family="JetBrainsMono Nerd Font Mono", weight="Light"},
 -- }
--- config.font = wezterm.font_with_fallback({ -- <built-in>, BuiltIn
---   "JetBrainsMono Nerd Font Mono",
+config.font = wezterm.font_with_fallback({ -- <built-in>, BuiltIn,
+  {
+    family = 'JetBrains Mono',
+    weight = 'Light',
+    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+  },
+  -- /nix/store/664gs7n95z32zpg9mxngrs7r9p85sm5p-noto-fonts-emoji-2.038/share/fonts/noto/NotoColorEmoji.ttf, FontConfig
+  -- Assumed to have Emoji Presentation
+  -- Pixel sizes: [128]
+  "Noto Color Emoji", -- <built-in>, BuiltIn
+  "Symbols Nerd Font Mono",
+})
 
---   -- /nix/store/664gs7n95z32zpg9mxngrs7r9p85sm5p-noto-fonts-emoji-2.038/share/fonts/noto/NotoColorEmoji.ttf, FontConfig
---   -- Assumed to have Emoji Presentation
---   -- Pixel sizes: [128]
---   "Noto Color Emoji", -- <built-in>, BuiltIn
---   "Symbols Nerd Font Mono",
--- })
 
-
-config.font = wezterm.font 'JetBrains Mono'
+-- config.font = wezterm.font 'JetBrains Mono'
 
 -- config.window_background_opacity = 0.8
 config.automatically_reload_config = true
