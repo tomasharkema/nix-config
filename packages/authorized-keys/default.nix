c452e8605ef540802f623e13826d2cf6f3822f95
Tomas Harkema <tomas@harkema.io>
2024-09-04 23:24:45 +0200

    update

--- packages/authorized-keys/default.nix
+++ packages/authorized-keys/default.nix
@@ -5,11 +5,11 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "authorized-keys";
-  version = "9";
+  version = "10";
 
   src = fetchurl {
     url = "https://github.com/tomasharkema.keys";
-    sha256 = "04w6vdkd6hiz1l2l59scy7kn0zw5iwfd2wag8fn13zpwccrkdqig";
+    sha256 = "sha256-rdBwW0NFTakOjNe5voGLc6xVrNYZvVaWHaN54/Hw4aA=";
   };
 
   dontUnpack = true;
