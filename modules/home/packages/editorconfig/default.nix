3c52480cf30b20b52c38f5a9fe27d05faf388e19
tomasharkema <tomas@harkema.io>
2024-10-01 11:48:42 +0200

    updates

--- modules/home/packages/editorconfig/default.nix
+++ modules/home/packages/editorconfig/default.nix
@@ -8,6 +8,11 @@
           indent_size = "2";
           insert_final_newline = true;
         };
+        "*.swift" = {
+          indent_style = "space";
+          indent_size = "4";
+          insert_final_newline = true;
+        };
       };
     };
   };
