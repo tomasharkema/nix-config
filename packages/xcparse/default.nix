744e62b81c4227bdc702fc35a3c0bad01b4fb275
Tomas Harkema <tomas@harkema.io>
2024-10-19 02:23:14 +0200

    fix plugin

--- packages/xcparse/default.nix
+++ packages/xcparse/default.nix
@@ -2,24 +2,34 @@
   lib,
   stdenv,
   fetchFromGitHub,
-}:
-stdenv.mkDerivation rec {
-  pname = "xcparse";
-  version = "2.3.1";
+  swiftPackages,
+  # swiftpm2nix,
+  # swift,
+  # swiftpm,
+}: let
+  generated = swiftPackages.swiftpm2nix.helpers ./swift.nix;
+in
+  stdenv.mkDerivation rec {
+    pname = "xcparse";
+    version = "2.3.1";
 
-  src = fetchFromGitHub {
-    owner = "ChargePoint";
-    repo = "xcparse";
-    rev = version;
-    hash = "sha256-4uOBqZ5R9UY5u8HJUAcxuX+o4AuM/wJRlrySYqfww64=";
-  };
+    src = fetchFromGitHub {
+      owner = "ChargePoint";
+      repo = "xcparse";
+      rev = version;
+      hash = "sha256-4uOBqZ5R9UY5u8HJUAcxuX+o4AuM/wJRlrySYqfww64=";
+    };
 
-  meta = {
-    description = "Command line tool & Swift framework for parsing Xcode 11+ xcresult";
-    homepage = "https://github.com/ChargePoint/xcparse";
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [];
-    mainProgram = "xcparse";
-    platforms = lib.platforms.all;
-  };
-}
+    nativeBuildInputs = [swiftPackages.swift swiftPackages.swiftpm];
+
+    configurePhase = generated.configure;
+
+    meta = {
+      description = "Command line tool & Swift framework for parsing Xcode 11+ xcresult";
+      homepage = "https://github.com/ChargePoint/xcparse";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [];
+      mainProgram = "xcparse";
+      platforms = lib.platforms.all;
+    };
+  }
