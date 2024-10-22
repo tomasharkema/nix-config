d4c696f0c9d8e151a48cb7eb18f27f6eb1d83fc2
Tomas Harkema <tomas@harkema.io>
2024-10-23 00:50:34 +0200

    fix mackarel plugins

--- modules/nixos/apps/mackerel/default.nix
+++ modules/nixos/apps/mackerel/default.nix
@@ -9,24 +9,27 @@
       build.releaseJson = pkgs.writers.writeJSON "nixos.json" config.system.nixos;
     };
 
-    environment.etc."nixos.json".source = config.system.build.releaseJson;
+    environment.etc = {
+      "nixos.json".source = config.system.build.releaseJson;
+      "mackarel-agent/conf.d/plugins.conf".text = ''
+        [plugin.metrics.nvidia-smi]
+        command = "${pkgs.custom.mackerel-plugin-nvidia-smi}/bin/mackerel-plugin-nvidia-smi"
+
+        [plugin.checks.check_cron]
+        command = [ "${pkgs.custom.mackerel-check-plugins}/bin/check-procs", "-p", "crond" ]
+
+        [plugin.checks.check_sssd]
+        command = [ "${pkgs.custom.mackerel-check-plugins}/bin/check-procs", "-p", "sssd" ]
+
+        [plugin.metadata.nixos]
+        command = "cat /etc/nixos.json";
+      '';
+    };
 
     services.mackerel-agent = {
       enable = true;
       apiKeyFile = config.age.secrets.mak.path;
       settings = {
-        "plugin.metrics.nvidia-smi" = {
-          command = "[\"${pkgs.custom.mackerel-plugin-nvidia-smi}/bin/mackerel-plugin-nvidia-smi\"]";
-        };
-        "plugin.checks.check_cron" = {
-          command = "[ \"${pkgs.custom.mackerel-check-plugins}/bin/check-procs\", \"-p\", \"crond\" ]";
-        };
-        "plugin.checks.check_sssd" = {
-          command = "[ \"${pkgs.custom.mackerel-check-plugins}/bin/check-procs\", \"-p\", \"sssd\" ]";
-        };
-        "plugin.metadata.nixos" = {
-          command = "cat /etc/nixos.json";
-        };
       };
     };
   };
