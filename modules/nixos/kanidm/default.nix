6b31fd821e5bbb6d0cf2727e070f9430c3d0ae5b
tomasharkema <tomas@harkema.io>
2024-11-02 01:57:34 +0100

    update kandim

--- modules/nixos/kanidm/default.nix
+++ modules/nixos/kanidm/default.nix
@@ -4,11 +4,7 @@
   pkgs,
   ...
 }: {
-  config = lib.mkIf false {
-    environment.systemPackages = with pkgs; [
-      kanidm-provision
-    ];
-
+  config = {
     system.nssDatabases = {
       group = lib.mkForce [
         "kanidm"
@@ -22,12 +18,47 @@
       ];
     };
 
+    environment = {
+      systemPackages = with pkgs; [
+        kanidm-provision
+      ];
+
+      etc."ssh/authorized_keys_command" = {
+        mode = "0755";
+        text = ''
+          #!/bin/sh
+          exec ${pkgs.kanidm}/bin/kanidm_ssh_authorizedkeys "$@"
+        '';
+      };
+    };
+
     services = {
+      openssh = {
+        authorizedKeysCommand = "/etc/ssh/authorized_keys_command";
+        authorizedKeysCommandUser = "nobody";
+      };
+
       sssd.enable = false;
 
       kanidm = {
         enableClient = true;
         enablePam = true;
+
+        package = pkgs.kanidm.overrideAttrs (old: rec {
+          version = "1.4.0";
+          src = pkgs.fetchFromGitHub {
+            owner = old.pname;
+            repo = old.pname;
+            rev = "refs/tags/v${version}";
+            hash = "sha256-hRYHr4r3+LRiaZoJgs3MA5YtDEoKyeg/ohPAIw3OMyo=";
+          };
+
+          cargoDeps = old.cargoDeps.overrideAttrs (_: {
+            inherit src; # You need to pass "src" here again,
+            # otherwise the old "src" will be used.
+            outputHash = "sha256-DfTalKTOiReQCreAzbkSjbhMSW5cdOGGg04i/QKPonE=";
+          });
+        });
 
         clientSettings = {
           uri = "https://idm.harke.ma";
@@ -37,7 +68,7 @@
           pam_allowed_login_groups = ["allusers"];
           home_attr = "name";
           home_alias = "name";
-          # allow_local_account_override = ["tomas"];
+          allow_local_account_override = ["tomas"];
           selinux = false;
           default_shell = "${pkgs.zsh}/bin/zsh";
           uid_attr_map = "name";
