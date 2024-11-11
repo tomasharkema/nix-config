ddc883e456b7382ff15fd1215d9b6dec484dbe30
tomasharkema <tomas@harkema.io>
2024-11-11 22:46:27 +0100

    fix more abrt

--- packages/abrt/default.nix
+++ packages/abrt/default.nix
@@ -10,51 +10,61 @@
   asciidoc,
   sphinx,
   xmlto,
-  sphinx-autobuild,
-}: let
-  py = python3.withPackages (ps: [sphinx]);
-in
-  stdenv.mkDerivation rec {
-    pname = "abrt";
-    version = "2.17.6";
+  glib,
+  gtk3,
+  systemd,
+  libxml2,
+  libreport,
+}:
+stdenv.mkDerivation rec {
+  pname = "abrt";
+  version = "2.17.6";
 
-    src = fetchFromGitHub {
-      owner = "abrt";
-      repo = "abrt";
-      rev = version;
-      hash = "sha256-F8KO7/wdmrkvZTuVO9UKd99fSXduthIeigl3ShTYaqI=";
-    };
+  src = fetchFromGitHub {
+    owner = "abrt";
+    repo = "abrt";
+    rev = version;
+    hash = "sha256-F8KO7/wdmrkvZTuVO9UKd99fSXduthIeigl3ShTYaqI=";
+  };
 
-    nativeBuildInputs = [
-      autoreconfHook
-      pkg-config
-    ];
-    buildInputs = [
-      gettext
-      intltool
-      py
-      py.pkgs.sphinx
-      asciidoc
-      xmlto
-      sphinx
-      sphinx-autobuild
-    ];
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+  ];
 
-    # autoreconfFlags = [ "--without--pythondoc" ];
+  buildInputs = [
+    gettext
+    intltool
+    # py
+    # py.pkgs.sphinx
+    python3
+    python3.pkgs.pytest
+    asciidoc
+    xmlto
+    sphinx
+    glib
+    gtk3
+    systemd
+    libxml2
+    libreport
+  ];
 
-    postPatch = ''
-        pwd 
-        ls
+  configureFlags = [
+    "PYTHON_SPHINX=${sphinx}/bin/sphinx"
+    "PYTEST=${python3.pkgs.pytest}/bin/pytest"
+  ];
 
-      sh gen-version'';
+  postPatch = ''
+    sh gen-version
+  '';
 
-    meta = with lib; {
-      description = "Automatic bug detection and reporting tool";
-      homepage = "https://github.com/abrt/abrt";
-      changelog = "https://github.com/abrt/abrt/blob/${src.rev}/CHANGELOG.md";
-      license = licenses.gpl2Only;
-      maintainers = with maintainers; [];
-      mainProgram = "abrt";
-      platforms = platforms.all;
-    };
-  }
+  meta = with lib; {
+    description = "Automatic bug detection and reporting tool";
+    homepage = "https://github.com/abrt/abrt";
+    changelog = "https://github.com/abrt/abrt/blob/${src.rev}/CHANGELOG.md";
+    license = licenses.gpl2Only;
+    maintainers = with maintainers; [];
+    mainProgram = "abrt";
+    platforms = platforms.all;
+  };
+}
