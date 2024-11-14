ba0a84b75e723e6c693a686e42db92136f6d6a4f
tomasharkema <tomas@harkema.io>
2024-11-14 14:44:56 +0100

    fix immich

--- modules/nixos/apps/podman/default.nix
+++ modules/nixos/apps/podman/default.nix
@@ -19,6 +19,26 @@ in {
       # pods
     ];
 
+    systemd.services = {
+      "docker-compose@" = {
+        wantedBy = ["multi-user.target"];
+
+        description = "%i service with docker compose";
+        partOf = ["podman.service"];
+        after = ["podman.service"];
+        unitConfig = {
+          ConditionPathExists = ["/etc/docker/compose/%i"];
+        };
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+          WorkingDirectory = "/etc/docker/compose/%i";
+          ExecStart = "${pkgs.docker-compose}/bin/docker-compose up -d --remove-orphans";
+          ExecStop = "${pkgs.docker-compose}/bin/docker-compose down";
+        };
+      };
+    };
+
     networking = {
       firewall = {
         interfaces."podman+" = {
