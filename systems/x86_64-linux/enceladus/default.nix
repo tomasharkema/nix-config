26a8454281b978a5d86915803dbb22b505c9cb41
tomasharkema <tomas@harkema.io>
2024-12-09 02:02:44 +0100

    add attic

--- systems/x86_64-linux/enceladus/default.nix
+++ systems/x86_64-linux/enceladus/default.nix
@@ -64,6 +64,66 @@ in {
       # remote-builders.server.enable = true;
       blueman.enable = true;
 
+      atticd = {
+        enable = true;
+        environmentFile = "/srv/atticd/.env";
+        settings = {
+          listen = "[::]:7124";
+
+          api-endpoint = "https://nix.harke.ma/";
+
+          #
+          # Warning: If you change any of the values here, it will be
+          # difficult to reuse existing chunks for newly-uploaded NARs
+          # since the cutpoints will be different. As a result, the
+          # deduplication ratio will suffer for a while after the change.
+          chunking = {
+            # The minimum NAR size to trigger chunking
+            #
+            # If 0, chunking is disabled entirely for newly-uploaded NARs.
+            # If 1, all NARs are chunked.
+            nar-size-threshold = 65536; # chunk files that are 64 KiB or larger
+
+            # The preferred minimum size of a chunk, in bytes
+            min-size = 16384; # 16 KiB
+
+            # The preferred average size of a chunk, in bytes
+            avg-size = 65536; # 64 KiB
+
+            # The preferred maximum size of a chunk, in bytes
+            max-size = 262144; # 256 KiB
+          };
+          # Compression
+          compression = {
+            # Compression type
+            #
+            # Can be "none", "brotli", "zstd", or "xz"
+            type = "zstd";
+
+            # Compression level
+            level = 9;
+          };
+
+          # Garbage collection
+          garbage-collection = {
+            # The frequency to run garbage collection at
+            #
+            # By default it's 12 hours. You can use natural language
+            # to specify the interval, like "1 day".
+            #
+            # If zero, automatic garbage collection is disabled, but
+            # it can still be run manually with `atticd --mode garbage-collector-once`.
+            interval = "12 hours";
+
+            # Default retention period
+            #
+            # Zero (default) means time-based garbage-collection is
+            # disabled by default. You can enable it on a per-cache basis.
+            #default-retention-period = "6 months"
+          };
+        };
+      };
+
       beesd.filesystems = {
         root = {
           spec = "UUID=7227b9fb-8619-403a-8944-4cc3f615ad6f";
