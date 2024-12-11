606256c0be90f0ef356ab6c8971299e21547cf42
tomasharkema <tomas@harkema.io>
2024-12-11 22:35:24 +0100

    fix secure boot

--- modules/nixos/traits/hardware/secure-boot/default.nix
+++ modules/nixos/traits/hardware/secure-boot/default.nix
@@ -15,13 +15,9 @@ in {
     boot = {
       lanzaboote = {
         enable = true;
-        # enrollKeys = true;
-        pkiBundle = "/etc/secureboot";
-        # settings = {
-        #   options = "vt.default_red=30,243,166,249,137,245,148,186,88,243,166,249,137,245,148,166 vt.default_grn=30,139,227,226,180,194,226,194,91,139,227,226,180,194,226,173 vt.default_blu=46,168,161,175,250,231,213,222,112,168,161,175,250,231,213,200";
-        # };
-        # enrollKeys = true;
-        # configuratoin
+        enrollKeys = true;
+        pkiBundle = "/var/lib/sbctl";
+
         configurationLimit = 10;
       };
       loader = {
