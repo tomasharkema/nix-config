59b6d7b0bf7e3c55b1f5e2496079e03edb24754f
Tomas Harkema <tomas@harkema.io>
2025-02-09 14:42:49 +0100

    fix tty

--- systems/x86_64-linux/wodan/default.nix
+++ systems/x86_64-linux/wodan/default.nix
@@ -289,8 +289,8 @@
 
       # KMS will load the module, regardless of blacklisting
       kernelParams = [
-        "console=tty1"
-        "console=ttyUSB0,115200n8"
+        # "console=tty1"
+        # "console=ttyUSB0,115200n8"
         "intel_iommu=on"
         "iommu=pt"
         "preempt=full"
