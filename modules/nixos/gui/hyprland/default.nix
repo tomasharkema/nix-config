1fc86380ee0fc28cf80c7a87ed83c52028ef115d
Tomas Harkema <tomas@harkema.io>
2025-02-15 19:30:30 +0100

    fixes

--- modules/nixos/gui/hyprland/default.nix
+++ modules/nixos/gui/hyprland/default.nix
@@ -5,16 +5,21 @@
   ...
 }: {
   config = {
-    programs.hyprland = {
-      enable = true;
+    programs = {
+      hyprland = {
+        enable = true;
+      };
+      hyprlock.enable = true;
     };
-
+    services = {
+      hypridle.enable = true;
+    };
     environment.systemPackages = with pkgs; [
       pyprland
       hyprpicker
       hyprcursor
-      hyprlock
-      hypridle
+      # hyprlock
+      # hypridle
       hyprpaper
 
       hyprsunset
@@ -53,7 +58,9 @@
           pass.enable = true;
           terminal = "kitty";
         };
+
         wlogout.enable = true;
+
         hyprlock = {
           enable = true;
         };
@@ -62,262 +69,32 @@
           enable = true;
           systemd.enable = true;
           style = builtins.readFile ./waybar.css;
-          settings = [
-            {
-              # layer = "top";
-              # position = "top";
-              height = 30;
-              spacing = 0; # 2; # 10
-              # output = [
-              #   "eDP-1"
-              #   "HDMI-A-1"
-              # ];
-              modules-left = [
-                "hyprland/workspaces"
-                "hyprland/mode"
-                "hyprland/scratchpad"
-                "wlr/taskbar"
-              ];
-
-              modules-center = [
-                "hyprland/window"
-                #"custom/hello-from-waybar"
-              ];
-              modules-right = [
-                # "mpd"
-                # "idle_inhibitor"
-                # "network"
-                "power-profiles-daemon"
-                # "cpu"
-                # "memory"
-                # "temperature"
-
-                # "keyboard-state"
-                # "sway/language"
-                "battery"
-                "battery#bat2"
-                "backlight"
-
-                # "pulseaudio"
-                "clock"
-                "tray"
-                "custom/lock"
-                "custom/power"
-              ];
-              "hyprland/workspaces" = {
-                disable-scroll = true;
-                all-outputs = true;
-              };
-              clock = {
-                interval = 1;
-                format = "{:%H:%M:%S}";
-              };
-              "keyboard-state" = {
-                "numlock" = true;
-
-                "capslock" = true;
-                "format" = "{name} {icon}";
-                "format-icons" = {
-                  "locked" = "";
-                  "unlocked" = "";
-                };
-              };
-              "sway/mode" = {
-                "format" = "<span style=\"italic\">{}</span>";
-              };
-              "sway/scratchpad" = {
-                "format" = "{icon} {count}";
-                "show-empty" = false;
-                "format-icons" = [
-                  ""
-                  ""
-                ];
-                "tooltip" = true;
-                "tooltip-format" = "{app}: {title}";
-              };
-              "mpd" = {
-                "format" = "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime:%M:%S}/{totalTime:%M:%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ";
-                "format-disconnected" = "Disconnected ";
-                "format-stopped" = "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ";
-                "unknown-tag" = "N/A";
-                "interval" = 5;
-                "consume-icons" = {
-                  "on" = " ";
-                };
-                "random-icons" = {
-                  "off" = "<span color=\"#f53c3c\"></span> ";
-                  "on" = " ";
-                };
-                "repeat-icons" = {
-                  "on" = " ";
-                };
-                "single-icons" = {
-                  "on" = "1 ";
-                };
-                "state-icons" = {
-                  "paused" = "";
-                  "playing" = "";
-                };
-                "tooltip-format" = "MPD (connected)";
-                "tooltip-format-disconnected" = "MPD (disconnected)";
-              };
-              "idle_inhibitor" = {
-                "format" = "{icon}";
-                "format-icons" = {
-                  "activated" = "";
-                  "deactivated" = "";
-                };
-              };
-              "tray" = {
-                "icon-size" = 21;
-                "spacing" = 10;
-              };
-              # "clock"= {
-              #     // "timezone"= "America/New_York";        "tooltip-format"= "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";        "format-alt"= "{:%Y-%m-%d}"
-              # };
-              "cpu" = {
-                "format" = "{usage}% ";
-                "tooltip" = false;
-              };
-              "memory" = {
-                "format" = "{}% ";
-              };
-              "temperature" = {
-                "thermal-zone" = 2;
-                "hwmon-path" = "/sys/class/hwmon/hwmon2/temp1_input";
-                "critical-threshold" = 80;
-                "format-critical" = "{temperatureC}°C {icon}";
-                "format" = "{temperatureC}°C {icon}";
-                "format-icons" = ["" "" ""];
-              };
-              "backlight" = {
-                "device" = "acpi_video1";
-                "format" = "{percent}% {icon}";
-                "format-icons" = ["" "" "" "" "" "" "" "" ""];
-              };
-              "battery" = {
-                # "states"= {
-                #     // "good"= 95;            "warning"= 30;            "critical"= 15
-                # };
-                "format" = "{capacity}% {icon}";
-                "format-full" = "{capacity}% {icon}";
-                "format-charging" = "{capacity}% ";
-                "format-plugged" = "{capacity}% ";
-                "format-alt" = "{time} {icon}"; # // "format-good"= ""; #// An empty format will hide the module
-                # // "format-full"= "";        "format-icons"= ["", "", "", "", ""]
-              };
-              "battery#bat2" = {
-                "bat" = "BAT2";
-              };
-              "power-profiles-daemon" = {
-                "format" = "{icon}";
-                "tooltip-format" = "Power profile: {profile}\nDriver: {driver}";
-                "tooltip" = true;
-                "format-icons" = {
-                  "default" = "";
-                  "performance" = "";
-                  "balanced" = "";
-                  "power-saver" = "";
-                };
-              };
-              "network" = {
-                # // "interface"= "wlp2*", // (Optional) To force the use of this interface
-                "format-wifi" = "{essid} ";
-                "format-ethernet" = "{ipaddr}/{cidr} ";
-                "tooltip-format" = "{ifname} via {gwaddr} ";
-                "format-linked" = "{ifname} (No IP) ";
-                "format-disconnected" = "Disconnected ⚠";
-                "format-alt" = "{ifname}: {ipaddr}/{cidr}";
-              };
-              "pulseaudio" = {
-                # // "scroll-step"= 1;// %, can be a float
-                "format" = "{volume}% {icon} {format_source}";
-                "format-bluetooth" = "{volume}% {icon} {format_source}";
-                "format-bluetooth-muted" = " {icon} {format_source}";
-                "format-muted" = " {format_source}";
-                "format-source" = "{volume}% ";
-                "format-source-muted" = "";
-                "format-icons" = {
-                  "headphone" = "";
-                  "hands-free" = "";
-                  "headset" = "";
-                  "phone" = "";
-                  "portable" = "";
-                  "car" = "";
-                  "default" = [
-                    ""
-                    ""
-                    ""
-                  ];
-                };
-                "on-click" = "pavucontrol";
-              };
-              "custom/media" = {
-                "format" = "{icon} {}";
-                "return-type" = "json";
-                "max-length" = 40;
-                "format-icons" = {
-                  "spotify" = "";
-                  "default" = "🎜";
-                };
-                "escape" = true;
-                "exec" = "$HOME/.config/waybar/mediaplayer.py 2> /dev/null"; # // Script in resources folder
-                # // "exec"= "$HOME/.config/waybar/mediaplayer.py --player spotify 2> /dev/null" // Filter player based on name
-              };
-
-              "custom/music" = {
-                "format" = " {}";
-                "escape" = true;
-                "interval" = 5;
-                "tooltip" = false;
-                "exec" = "playerctl metadata --format='{{ title }}'";
-                "on-click" = "playerctl play-pause";
-                "max-length" = 50;
-              };
-
-              "custom/power" = {
-                "format" = "⏻ ";
-                "tooltip" = false;
-                "menu" = "on-click";
-                "menu-file" = "$HOME/.config/waybar/power_menu.xml"; # // Menu file in resources folder
-                "menu-actions" = {
-                  "shutdown" = "shutdown";
-                  "reboot" = "reboot";
-                  "suspend" = "systemctl suspend";
-                  "hibernate" = "systemctl hibernate";
-                };
-              };
-              "custom/lock" = {
-                "tooltip" = false;
-                "on-click" = "hyprlock &";
-                "format" = "";
-              };
-              # "backlight" = {
-              #   # "device" = "nvidia_0";
-              #   "format" = "{icon}";
-              #   "on-scroll-up" = "brightnessctl s '+10%'";
-              #   "on-scroll-down" = "brightnessctl s '10%-'";
-              #   "on-click" = "((( $(brightnessctl g) == 100 )) && brightnessctl s '0') || (brightnessctl s '+10%')";
-              #   "format-icons" = [
-              #     ""
-              #     ""
-              #     ""
-              #     ""
-              #     ""
-              #     ""
-              #     ""
-              #     ""
-              #     ""
-              #   ];
-              # };
-            }
-          ];
+          settings = import ./waybar.nix;
         };
       };
 
       services = {
         swaync = {
           enable = true;
+        };
+        hypridle = {
+          enable = true;
+          settings = {
+            general = {
+              lock_cmd = ''${lib.getExe pkgs.notify-send} "lock!"''; # dbus/sysd lock command (loginctl lock-session)
+              unlock_cmd = ''${lib.getExe pkgs.notify-send} "unlock!"''; # same as above, but unlock
+              before_sleep_cmd = ''${lib.getExe pkgs.notify-send} "Zzz"''; # command ran before sleep
+              after_sleep_cmd = ''${lib.getExe pkgs.notify-send} "Awake!"''; # command ran after sleep
+              ignore_dbus_inhibit = false; # whether to ignore dbus-sent idle-inhibit requests (used by e.g. firefox or steam)
+              ignore_systemd_inhibit = false; # whether to ignore systemd-inhibit --what=idle inhibitors
+            };
+
+            listener = {
+              timeout = 500; # in seconds
+              on-timeout = ''${lib.getExe pkgs.notify-send} "You are idle!"''; # command to run when timeout has passed
+              on-resume = ''${lib.getExe pkgs.notify-send} "Welcome back!"''; # command to run when activity is detected after timeout has fired.
+            };
+          };
         };
       };
 
