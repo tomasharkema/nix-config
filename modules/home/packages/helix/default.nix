4215ae8e0eb3d79cbb9b6e45d7cce67b25b5f787
tomasharkema <tomas@harkema.io>
2025-02-16 23:10:04 +0100

    add helix

---
+++ modules/home/packages/helix/default.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  config = {
+    programs.helix = {
+      enable = true;
+      settings = {
+        # theme = "autumn_night_transparent";
+        editor = {
+          cursor-shape = {
+            normal = "block";
+            insert = "bar";
+            select = "underline";
+          };
+          true-color = true;
+        };
+      };
+      languages.language = [
+        {
+          name = "nix";
+          auto-format = true;
+          formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+        }
+      ];
+      # themes = {
+      #   autumn_night_transparent = {
+      #     "inherits" = "autumn_night";
+      #     "ui.background" = {};
+      #   };
+      # };
+    };
+  };
+}
