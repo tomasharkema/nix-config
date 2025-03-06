f6ad1ca145821bbd0aac8fba4f8f8cd873649f07
Tomas Harkema <tomas@harkema.io>
2025-03-06 10:53:51 +0100

    fixes

--- packages/racadm/default.nix
+++ packages/racadm/default.nix
@@ -4,37 +4,72 @@
   tree,
   dpkg,
   autoPatchelfHook,
-}:
-stdenv.mkDerivation rec {
-  pname = "racadm";
-  version = "10.3.0.0-4945";
+  fetchFromGitHub,
+  cmake,
+  ninja,
+  pkg-config,
+}: let
+  argtable = stdenv.mkDerivation rec {
+    pname = "argtable";
+    version = "2.13";
 
-  src = fetchurl {
-    url = "https://dl.dell.com/FOLDER08637461M/1/DellEMC-iDRACTools-Web-LX-${version}_A00.tar.gz";
-    sha256 = "sha256-TbrMpVdBQ6lE+K23v6m4EkL55V9PzHXPcx7+vh1MaMY=";
-    curlOptsList = ["-A" "Mozilla"];
+    src = fetchFromGitHub {
+      owner = "jonathanmarvens";
+      repo = "argtable2";
+      rev = "v2.13";
+      sha256 = "sha256-K6++QVvpcPR+BYxbDRZ24sY0+PgIaQ3t1ktt3zZGh6Q=";
+    };
+
+    nativeBuildInputs = [
+      # cmake
+      pkg-config
+      # ninja
+    ];
+
+    buildInputs = [];
+
+    patches = [./ctype.patch];
   };
-  nativeBuildInputs = [dpkg autoPatchelfHook];
+in
+  stdenv.mkDerivation rec {
+    pname = "racadm";
+    version = "10.3.0.0-4945";
 
-  postUnpack = ''
-    ${tree}/bin/tree .
-    dpkg-deb --fsys-tarfile racadm/UBUNTU20/x86_64/srvadmin-hapi_11.0.1.0_amd64.deb | \
-      tar -x --no-same-owner
+    src = fetchurl {
+      url = "https://dl.dell.com/FOLDER08637461M/1/DellEMC-iDRACTools-Web-LX-${version}_A00.tar.gz";
+      sha256 = "sha256-TbrMpVdBQ6lE+K23v6m4EkL55V9PzHXPcx7+vh1MaMY=";
+      curlOptsList = ["-A" "Mozilla"];
+    };
 
-    dpkg-deb --fsys-tarfile racadm/UBUNTU20/x86_64/srvadmin-idracadm8_11.0.1.0_amd64.deb | \
-      tar -x --no-same-owner
+    nativeBuildInputs = [dpkg autoPatchelfHook];
 
-    dpkg-deb --fsys-tarfile racadm/UBUNTU20/x86_64/srvadmin-idracadm7_11.0.1.0_amd64.deb | \
-      tar -x --no-same-owner
-  '';
-  installPhase = ''
-    ${tree}/bin/tree .
+    buildInputs = [argtable];
 
-    # racadm/UBUNTU20/x86_64/srvadmin-hapi_11.0.1.0_amd64.deb
-    # racadm/UBUNTU20/x86_64/srvadmin-idracadm7_11.0.1.0_all.deb
-    # racadm/UBUNTU20/x86_64/srvadmin-idracadm8_11.0.1.0_amd64.deb
+    postUnpack = ''
+      ${tree}/bin/tree .
+      dpkg-deb --fsys-tarfile iDRACTools/racadm/UBUNTU20/x86_64/srvadmin-hapi_11.0.1.0_amd64.deb | \
+        tar -vx --no-same-owner -C iDRACTools
 
+      dpkg-deb --fsys-tarfile iDRACTools/racadm/UBUNTU20/x86_64/srvadmin-idracadm8_11.0.1.0_amd64.deb | \
+        tar -vx --no-same-owner -C iDRACTools
 
-    # install -D racadm $out/bin/racadm
-  '';
-}
+      dpkg-deb --fsys-tarfile iDRACTools/racadm/UBUNTU20/x86_64/srvadmin-idracadm7_11.0.1.0_all.deb | \
+        tar -vx --no-same-owner -C iDRACTools
+
+      ${tree}/bin/tree .
+    '';
+
+    installPhase = ''
+      ${tree}/bin/tree .
+      mkdir -p $out/{bin,lib}
+      cp -r usr/libexec $out
+      cp -r opt/dell/srvadmin/* $out
+      cp -r etc $out
+      cp -r etc/systemd $out/lib
+      rm -rf $out/var
+
+      substituteInPlace $out/lib/systemd/system/instsvcdrv.service --replace-fail "/usr" "$out"
+      substituteInPlace $out/libexec/instsvcdrv-helper --replace-fail "/opt/dell/srvadmin\"" "$out\"" \
+        --replace-fail "/opt/dell/srvadmin/" "/"
+    '';
+  }
