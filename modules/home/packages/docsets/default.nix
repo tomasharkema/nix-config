ebac541941104a26dbec9130f198bfa81b77ab64
Tomas Harkema <tomas@harkema.io>
2025-03-24 19:57:09 +0100

    meh

--- modules/home/packages/docsets/default.nix
+++ modules/home/packages/docsets/default.nix
@@ -20,13 +20,13 @@
   #   ${pkgs.custom.doc2dash}/bin/doc2dash ${gtk4'.devdoc}/share/doc/gtk4
   # '';
 in {
-  config = lib.mkIf (pkgs.stdenv.isLinux && osConfig.gui.enable) {
+  config = lib.mkIf (pkgs.stdenv.isLinux && osConfig.gui.enable && false) {
     home = {
       packages = with pkgs; [zeal];
       file = {
         # "${docsRoot}/nixos-${pkgs.docset.version}.docset".source = "${pkgs.docset}/share/nixos-${pkgs.docset.version}.docset";
         # TODO: generate with doc2dash
-        "${docsRoot}/Gtk.docset".source = "${pkgs.rtfm}/share/rtfm/docsets/Gtk.docset";
+        # "${docsRoot}/Gtk.docset".source = "${pkgs.rtfm}/share/rtfm/docsets/Gtk.docset";
       };
     };
   };
