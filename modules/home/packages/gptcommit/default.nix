0a2a213033a5d4c32ee855bf49b01df5e9a1eeab
Tomas Harkema <tomas@harkema.io>
2025-05-09 12:14:45 +0200

    updates

--- modules/home/packages/gptcommit/default.nix
+++ modules/home/packages/gptcommit/default.nix
@@ -13,7 +13,7 @@ in {
       #  ${package}/bin/gptcommit config set openai.api_key "$(cat ${osConfig.age.secrets.openai.path})"
       activation = {
         gptcommit = ''
-          ${pkg}/bin/gptcommit config set openai.api_base "http://wodan:11434"
+          ${pkg}/bin/gptcommit config set openai.api_base "http://silver_star:11434"
           ${pkg}/bin/gptcommit config set allow-amend true
         '';
       };
