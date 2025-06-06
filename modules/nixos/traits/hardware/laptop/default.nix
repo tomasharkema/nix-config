1bc72cd65c5f9beb19226de1aa3c82f06ff3907f
tomasharkema <tomas@harkema.io>
2025-06-06 17:43:01 +0200

    fixes

--- modules/nixos/traits/hardware/laptop/default.nix
+++ modules/nixos/traits/hardware/laptop/default.nix
@@ -64,6 +64,8 @@ in {
           #Optional helps save long term battery health
           START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
           STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+          START_CHARGE_THRESH_BAT1 = 40; # 40 and below it starts to charge
+          STOP_CHARGE_THRESH_BAT1 = 80; # 80 and above it stops charging
         };
       };
 
--- packages/tlpui/default.nix
+++ packages/tlpui/default.nix
@@ -3,30 +3,51 @@
   python3,
   fetchFromGitHub,
   tlp,
+  gtk3,
+  gobject-introspection,
+  wrapGAppsHook,
 }:
 python3.pkgs.buildPythonApplication rec {
   pname = "tlpui";
-  version = "1.6.5";
+  version = "1.8.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "d4nj1";
     repo = "TLPUI";
     rev = "tlpui-${version}";
-    hash = "sha256-pgzGhf2WDRNQ2z0hPapUJA5MLTKq92UlgjC+G78T/4s=";
+    hash = "sha256-JRchkgH8efFWpLbYWwgrWo7bMXwvGP0oHyXG4h65Y+A=";
   };
 
-  nativeBuildInputs = [
-    python3.pkgs.poetry-core
+  build-system = [python3.pkgs.poetry-core];
+
+  nativeBuildInputs = with python3.pkgs; [
+    gobject-introspection
+    wrapGAppsHook
   ];
 
   propagatedBuildInputs = with python3.pkgs; [
     pygobject3
     pyyaml
     tlp
+    gtk3
   ];
 
-  # pythonImportsCheck = ["tlp_ui"];
+  runtimeDependencies = with python3.pkgs; [
+    gtk3
+  ];
+
+  pythonImportsCheck = ["tlpui"];
+  postPatch = ''
+    substituteInPlace tlpui/settings.py \
+      --replace-fail "FOLDER_PREFIX = \"/var/run/host\" if IS_FLATPAK else \"\"" "FOLDER_PREFIX = \"/run/current-system/sw\""
+  '';
+
+  dontWrapGApps = true;
+  makeWrapperArgs = [
+    "--prefix 'PATH' ':' 'lib.makeBinPath [tlp]}'"
+    ''"''${gappsWrapperArgs[@]}"''
+  ];
 
   meta = with lib; {
     description = "A GTK user interface for TLP written in Python";
