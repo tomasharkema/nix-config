4172f27e329006563a7620b7e9631cad06677def
tomasharkema <tomas@harkema.io>
2025-06-22 00:49:03 +0200

    update tailscale ui

--- packages/tailscale-tui/default.nix
+++ packages/tailscale-tui/default.nix
@@ -9,9 +9,9 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "bilguun0203";
     repo = pname;
-    rev = "9be253f5a75829fbf64472a67dcccb9730fa9c69";
-    sha256 = "sha256-j5azNUtsI6aNkg9tkpawLPxiwNBp/tP5V28VijtHrns=";
+    rev = "db90a604ac40c6222947d6c6f97f481641b80d48";
+    sha256 = "sha256-P1tu56DkwZTxavFRrNM/Wx1N86M5k/XQRfDrv+VK1uE=";
   };
 
-  vendorHash = "sha256-r05aVSwIORiJtKvS8fJFUoYRMB5272M+G9YhobwA6c8=";
+  vendorHash = "sha256-w6agPfHGYL5DCe3GbgdFsojQ3GEAs37ZeNvy2nL/kq0=";
 }
