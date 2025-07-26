3e3ffdbdc19f3dfd6f7e5e175548d4a5117b3a19
Tomas Harkema <tomas@harkema.io>
2025-07-26 05:26:07 +0200

    Update default.nix

--- modules/home/packages/git/default.nix
+++ modules/home/packages/git/default.nix
@@ -73,9 +73,10 @@
           rerere = {enable = true;};
           pull = {rebase = true;};
           branch = {autosetupmerge = true;};
-
+push.autoSetupRemote = true;
           commit.gpgsign = true;
           init.defaultBranch = "main";
+maintenance.auto=true;
 
           credential.helper = ["${pkgs.custom.git-credential-1password}/bin/git-credential-1password"];
 
