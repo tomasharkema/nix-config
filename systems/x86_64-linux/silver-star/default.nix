971dc76e7cc5ee5f25c9a8b0704df984537bd541
tomasharkema <tomas@harkema.io>
2025-09-26 15:44:16 +0200

    openmanage fixes

--- systems/x86_64-linux/silver-star/default.nix
+++ systems/x86_64-linux/silver-star/default.nix
@@ -432,14 +432,14 @@
           image = "teumaauss/srvadmin:latest";
 
           volumes = let
-            kernelVersion = config.boot.kernelPackages.kernel.version;
+            kernelVersion = config.boot.kernelPackages.kernel.modDirVersion;
           in [
             "/run/current-system/sw/lib/modules/${kernelVersion}:/lib/modules/${kernelVersion}:ro"
             "/sys:/sys:ro"
             "/srv/openmanage/shared:/data"
             "/nix/store:/nix/store:ro"
             "/etc/os-release:/etc/os-release:ro"
-            "/usr/libexec/dell_dup:/usr/libexec/dell_dup"
+            "/usr/libexec/dell_dup:/usr/libexec/dell_dup:rw"
             "/run/systemd/system:/run/systemd/system"
             "/var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket"
           ];
