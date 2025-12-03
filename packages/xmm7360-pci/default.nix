ddf5dc9f0d0c1247184e8fd075d35010e28ec0c1
tomasharkema <tomas@harkema.io>
2025-12-03 22:10:58 +0100

    try to fix xmm7360

--- packages/xmm7360-pci/default.nix
+++ packages/xmm7360-pci/default.nix
@@ -7,7 +7,12 @@
   python3,
   fetchpatch,
 }: let
-  py = python3.withPackages (ps: [ps.configargparse ps.pyroute2 ps.dbus-python]);
+  py = python3.withPackages (ps: [
+    ps.configargparse
+    ps.pyroute2
+    ps.dbus-python
+    ps.pytap2
+  ]);
 in
   stdenv.mkDerivation rec {
     pname = "xmm7360-pci";
@@ -40,15 +45,28 @@ in
       #   url = "https://raw.githubusercontent.com/chaotic-aur/pkgbuild-xmm7360-pci-git/refs/heads/master/dns-priority.patch";
       #   sha256 = "sha256-/oSuUn6INq0uo2wmVieRo5CWgjTLYO51mqinBEbcanA=";
       # })
+      #
+      # adds netstats!
+      (fetchpatch {
+        url = "https://patch-diff.githubusercontent.com/raw/xmm7360/xmm7360-pci/pull/165.patch";
+        sha256 = "sha256-VmCqfyxGAiuJq9WIDF5NFIKkEnPHJlsjyaYXfv39oV8=";
+      })
       ./linux-6.15.patch
     ];
 
     prePatch = ''
       substituteInPlace rpc/open_xdatachannel.py \
         --replace-fail "#!/usr/bin/env python3" "#!${py}/bin/python3"
+
+      substituteInPlace rpc/mux.py \
+        --replace-fail "#!/usr/bin/env python" "#!${py}/bin/python3"
     '';
 
     makeFlags = ["KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"];
+
+    postBuild = ''
+      make -C rpc
+    '';
 
     installPhase = ''
       runHook preInstall
