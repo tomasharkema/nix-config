1c094876b30a33b8bae60a6f6e7274108527925c
Tomas Harkema <tomas@harkema.io>
2026-01-07 15:45:14 +0100

    remove git-butler

--- overlays/nix/default.nix
+++ overlays/nix/default.nix
@@ -1,13 +1,12 @@
 {
   agenix,
   nixos-anywhere,
-  nix-software-center,
   channels,
   ...
 }: final: prev: {
   agenix = agenix.packages."${prev.system}".default;
   nixos-anywhere = nixos-anywhere.packages."${prev.system}".nixos-anywhere;
-  nix-software-center = nix-software-center.packages."${prev.system}".nix-software-center;
+  # nix-software-center = nix-software-center.packages."${prev.system}".nix-software-center;
 
   # nerdfonts = prev.nerdfonts.override {
   #   fonts = [
