8613e6bce7d01b9be299b385233c505282a56327
Tomas Harkema <tomas@harkema.io>
2026-02-05 19:32:03 +0100

    cleanups

--- configs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-NAMES="$(nix eval '.#nodes' --apply 'builtins.attrNames' --json | jq -r -c '.[]')"
-
-for name in $NAMES; do
-
-  echo "$name ojoo"
-  nix eval ".#nixosConfigurations.${name}.config.age.rekey.storageMode" --json
-done
