cd6c335eab0ae07493a383a94115bb57056d3eb4
Tomas Harkema <tomas@harkema.io>
2026-02-13 01:02:28 +0100

    implement fail 2 ban

--- modules/nixos/traits/server/default.nix
+++ modules/nixos/traits/server/default.nix
@@ -35,6 +35,34 @@ in {
           "$HOME/.vscode-server-insiders"
         ];
       };
+
+      fail2ban = {
+        enable = true;
+
+        maxretry = 10;
+        ignoreIP = [
+          "10.0.0.0/8"
+          "172.16.0.0/12"
+          "192.168.0.0/16"
+          "100.0.0.0/8"
+        ];
+        bantime = "24h"; # Ban IPs for one day on the first ban
+        bantime-increment = {
+          enable = true; # Enable increment of bantime after each violation
+          formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+          multipliers = "1 2 4 8 16 32 64";
+          maxtime = "168h"; # Do not ban for more than 1 week
+          overalljails = true; # Calculate the bantime based on all the violations
+        };
+      };
+    };
+
+    environment.etc = {
+      "fail2ban/action.d/ntfy.local".text = pkgs.lib.mkDefault (pkgs.lib.mkAfter ''
+        [Definition]
+        norestored = true # Needed to avoid receiving a new notification after every restart
+        actionban = curl -H "Title: <ip> has been banned" -d "<name> jail has banned <ip> from accessing $(hostname) after <failures> attempts of hacking the system." https://ntfy.sh/tomasharkema-nixos
+      '');
     };
 
     disks.btrfs.swap.resume.enable = false;
