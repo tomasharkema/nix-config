28260f519d2d371447fd0bc1a845381bfd7e01ac
tomasharkema <tomas@harkema.io>
2026-02-17 02:57:26 +0100

    add kdeconnect

--- modules/nixos/apps/kdeconnect/default.nix
+++ modules/nixos/apps/kdeconnect/default.nix
@@ -7,7 +7,16 @@
   config = lib.mkIf config.gui.gnome.enable {
     programs.kdeconnect = {
       enable = true;
-      package = pkgs.gnomeExtensions.gsconnect;
+      package = pkgs.valent;
+    };
+    networking.firewall = rec {
+      allowedTCPPortRanges = [
+        {
+          from = 1714;
+          to = 1764;
+        }
+      ];
+      allowedUDPPortRanges = allowedTCPPortRanges;
     };
   };
 }
