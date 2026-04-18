11e81fe1b5f9c2f0bc66c87e250f0be86152695a
Tomas Harkema <tomas@harkema.io>
2026-04-18 19:22:47 +0200

    fix idsdm

--- systems/x86_64-linux/silver-star/default.nix
+++ systems/x86_64-linux/silver-star/default.nix
@@ -35,7 +35,7 @@
     disks.btrfs = {
       enable = true;
       main = "/dev/disk/by-id/nvme-Samsung_SSD_990_PRO_2TB_S7PJNJ0Y411286E_1-part1";
-      boot = "/dev/disk/by-id/usb-Kingston_DataTraveler_3.0_0017318221C6B03019AFE5EA-0:0-part1";
+      boot = "/dev/disk/by-id/usb-DELL_IDSDM_012345678901-0:0";
       snapper.enable = false;
       # btrbk.enable = true;
       swap.enable = false;
