0c18404d688becf0b4bd4ede8f6290a69d5fbfbc
Tomas Harkema <tomas@harkema.io>
2026-04-18 18:18:44 +0200

    Update flake-compat URL to point to NixOS repository

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@
       lock = builtins.fromJSON (builtins.readFile ./flake.lock);
     in
       fetchTarball {
-        url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+        url = lock.nodes.flake-compat.locked.url or "https://github.com/NixOS/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
         sha256 = lock.nodes.flake-compat.locked.narHash;
       }
   )
