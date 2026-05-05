b42aea39bf0f9b2ec1333942ae226d5c2ff5ee11
Tomas Harkema <tomas@harkema.io>
2026-05-05 02:03:36 +0200

    fixes

--- packages/computer-modern/default.nix
+++ packages/computer-modern/default.nix
@@ -9,7 +9,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   src = fetchzip {
     url = "https://sourceforge.net/projects/cm-unicode/files/cm-unicode/${finalAttrs.version}/cm-unicode-${finalAttrs.version}-ttf.tar.xz/download";
-    sha256 = "sha256-JNf188eftIS9E6fWXJGnVFj91bXnPwlPkHh7sk6TBno=";
+    sha256 = "sha256-apH5oSwBugVZtcbc/r1kK/GeqfQDrHyMFYvw/YOdqUc=";
     extension = "tar.xz";
   };
 
@@ -17,8 +17,8 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     runHook preInstall
 
     mkdir -p $out/share/fonts/ttf
-
-    install -Dm444 cm-unicode-${finalAttrs.version}/*.ttf $out/share/fonts/ttf
+    ls -la
+    install -Dm444 *.ttf $out/share/fonts/ttf
 
     runHook postInstall
   '';
