98e94a3c6b3334fd01b0b45012685aaa0a57d1e5
tomasharkema <tomas@harkema.io>
2026-05-26 17:30:05 +0200

    dont pin nixpkgs

--- flake.nix
+++ flake.nix
@@ -512,9 +512,9 @@
     nixvim = {
       url = "github:nix-community/nixvim"; # /nixos-24.05";
 
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      # inputs = {
+      #   nixpkgs.follows = "nixpkgs";
+      # };
     };
 
     agenix = {
