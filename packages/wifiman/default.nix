a1b72cf538feebd70da11926fd3df2dc298ec956
tomasharkema <tomas@harkema.io>
2026-08-13 22:00:30 +0200

    fixes

--- packages/wifiman/default.nix
+++ packages/wifiman/default.nix
@@ -15,54 +15,48 @@
   libayatana-appindicator,
   lib,
 }:
-stdenv.mkDerivation {
-  pname = "wifiman-desktop";
-  version = "1.2.8";
-  src = fetchurl {
-    url = "https://desktop.wifiman.com/wifiman-desktop-1.2.8-amd64.deb";
-    sha256 = "sha256-R+MbwxfnBV9VcYWeM1NM08LX1Mz9+fy4r6uZILydlks=";
-  };
+stdenv.mkDerivation (
+  finalAttrs: {
+    pname = "wifiman-desktop";
+    version = "1.2.8";
 
-  nativeBuildInputs = [
-    dpkg
-    autoPatchelfHook
-    wrapGAppsHook3
-    copyDesktopItems
-    pkg-config
-  ];
+    src = fetchurl {
+      url = "https://desktop.wifiman.com/wifiman-desktop-${finalAttrs.version}-amd64.deb";
+      sha256 = "sha256-R+MbwxfnBV9VcYWeM1NM08LX1Mz9+fy4r6uZILydlks=";
+    };
 
-  buildInputs = [
-    gtk3
-    glibc
-    libgcc
-    webkitgtk_4_1
-    glib
-    gobject-introspection
-    libayatana-appindicator
-  ];
+    nativeBuildInputs = [
+      dpkg
+      autoPatchelfHook
+      wrapGAppsHook3
+      copyDesktopItems
+      pkg-config
+    ];
 
-  # postFixup = ''
+    buildInputs = [
+      gtk3
+      glibc
+      libgcc
+      webkitgtk_4_1
+      glib
+      gobject-introspection
+      libayatana-appindicator
+    ];
 
-  # '';
+    postInstall = ''
+      mkdir $out
+      cp -va usr/* $out
 
-  postInstall = ''
-    mkdir $out
-    cp -a usr/* $out
+      substituteInPlace $out/lib/wifiman-desktop/wifiman-desktop.service \
+        --replace-fail "/usr/lib/wifiman-desktop/wifiman-desktopd" "$out/lib/wifiman-desktop/wifiman-desktopd"
 
-    mv $out/lib/wifiman-desktop/* $out/lib
-    mv $out/lib/wifiman-desktop/.* $out/lib
+      mkdir -p $out/lib/systemd/system
+      mv $out/lib/wifiman-desktop/wifiman-desktop.service $out/lib/systemd/system/wifiman-desktop.service
 
-    mv $out/lib/wifiman-desktopd $out/bin/wifiman-desktopd
+      substituteInPlace $out/share/applications/wifiman-desktop.desktop \
+        --replace-fail "Exec=wifiman-desktop" "Exec=$out/bin/wifiman-desktop"
 
-    substituteInPlace $out/lib/wifiman-desktop.service \
-      --replace-fail "/usr/lib/wifiman-desktop/wifiman-desktopd" "$out/bin/wifiman-desktopd"
-
-    mkdir -p $out/lib/systemd/system
-    mv $out/lib/wifiman-desktop.service $out/lib/systemd/system
-
-    substituteInPlace $out/share/applications/wifiman-desktop.desktop \
-      --replace-fail "Exec=wifiman-desktop" "Exec=$out/bin/wifiman-desktop"
-
-    wrapProgram "$out/bin/wifiman-desktop" --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [libayatana-appindicator]}"
-  '';
-}
+      wrapProgram "$out/bin/wifiman-desktop" --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [libayatana-appindicator]}"
+    '';
+  }
+)
